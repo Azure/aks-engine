eac43a0848a8109b5fb5ccd5b68321a5c43ff808
Cecile Robert-Michon <cerobert@microsoft.com>
2018-11-15 13:51:25 -0800

    fix vendor path

--- scripts/copyright-header.sh
+++ scripts/copyright-header.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(find .. -type f -iname '*.go' ! -path './vendor/*') # do not run on the vendor dir
+for i in $(find .. -type f -iname '*.go' ! -path '../vendor/*') # do not run on the vendor dir
 do
   if ! grep -q Copyright $i
   then
