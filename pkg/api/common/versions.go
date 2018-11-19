469188c1c4e5d6892290d71b657bbcab3b601040
Matt Boersma <Matt.Boersma@microsoft.com>
2018-11-19 09:34:01 -0700

    Add support for Kubernetes v1.13.0
    https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.13.md#v1130

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -92,8 +92,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.12.0":         false,
 	"1.12.1":         true,
 	"1.12.2":         true,
-	"1.13.0-alpha.1": true,
-	"1.13.0-alpha.2": true,
+	"1.13.0":         true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
