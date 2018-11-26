9b995ba419a386e569821b94fdca596b3e3a65ba
Matt Boersma <Matt.Boersma@microsoft.com>
2018-11-26 09:29:56 -0700

    Bump to 1.13.0-beta.2

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -94,7 +94,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.12.2":         true,
 	"1.13.0-alpha.1": true,
 	"1.13.0-alpha.2": true,
-	"1.13.0-beta.1":  true,
+	"1.13.0-beta.2":  true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
