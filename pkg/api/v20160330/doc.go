f4f9637a6a0fec75295201dc02236d8227f6d103
Matt Boersma <Matt.Boersma@microsoft.com>
2018-12-07 16:20:19 -0700

    Add some Go package deprecation notices (#110)

--- pkg/api/v20160330/doc.go
+++ pkg/api/v20160330/doc.go
@@ -1,4 +1,8 @@
-// Package v20160330 stores the api model for version "2016-03-30"
+// Package v20160330 stores the API model for version "2016-03-30."
+//
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
+//
+// Deprecated: Use the vlabs package instead. This code remains only to
+// support the legacy ACS service.
 package v20160330
