258ed5db822f883a5e1b42c9662407b49b5f7902
Cecile Robert-Michon <cerobert@microsoft.com>
2018-12-14 15:34:08 -0800

    use $(system.defaultWorkingDirectory)

--- scripts/get-kubectl.sh
+++ scripts/get-kubectl.sh
@@ -4,8 +4,5 @@
 
 echo "==> Downloading kubectl version ${KUBECTL_VERSION} <=="
 
-apt-get install sudo -y
-apt-get update
-apt-get install sudo -y
-sudo curl -L https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+curl -L https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
