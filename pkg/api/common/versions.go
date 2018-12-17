d6eb154491e7c0a25b3ff934d09830049fa82c51
Matt Boersma <Matt.Boersma@microsoft.com>
2018-12-17 10:05:22 -0700

    Bump to k8s 1.12.4
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md#v1124

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -93,6 +93,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.12.0":         false,
 	"1.12.1":         false,
 	"1.12.2":         true,
+	"1.12.4":         true,
 	"1.13.0-alpha.1": false,
 	"1.13.0-alpha.2": false,
 	"1.13.1":         true,
