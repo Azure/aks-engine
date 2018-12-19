aaa22b64208842f08e015dff2b0a3ad8684274f8
Jack Francis <jackfrancis@gmail.com>
2018-12-19 12:21:15 -0800

    use latest VHD image (#180)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -143,7 +143,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-201812",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.12.12",
+		ImageVersion:   "2018.12.18",
 	}
 
 	// DefaultAKSDockerEngineOSImageConfig is the AKS image based on Ubuntu 16.04.
@@ -151,7 +151,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-docker-engine",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.12.12",
+		ImageVersion:   "2018.12.18",
 	}
 
 	//AzureCloudSpec is the default configurations for global azure.
