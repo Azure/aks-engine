001eb12ec6890d2750e58fbf92f4d95916487575
Matt Boersma <Matt.Boersma@microsoft.com>
2019-01-04 16:35:40 -0700

    fix: echo commit message in case of failure

--- scripts/validate-commit-msg.sh
+++ scripts/validate-commit-msg.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 echo "==> Checking commit message <=="
 
-git log -1 --pretty=%B | grep -E '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|test)(\(.+\))?: .+' \
-  || (echo "Commits must follow https://www.conventionalcommits.org/en/v1.0.0-beta.2/#specification with one of these types:
-  build, ci, chore, docs, feat, fix, perf, refactor, revert, style, test" && exit 1)
+msg=$(git log -1 --pretty=%B)
+echo "$msg" | grep -E '^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(.+\))?: .+' \
+  || (echo -e "Commit message \"$msg\" failed validation.\n" \
+      "Commits must follow https://www.conventionalcommits.org/en/v1.0.0-beta.2/#specification with one of these types:\n" \
+      "  build, ci, chore, docs, feat, fix, perf, refactor, revert, style, test" \
+      && exit 1)
