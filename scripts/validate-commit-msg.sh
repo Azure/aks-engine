42407373c56c1398d627915913cf098c1229ddfc
Matt Boersma <Matt.Boersma@microsoft.com>
2019-01-07 12:27:24 -0700

    ci: check that last commit message is conformant (#237)
    * ci: check that last commit message is conformant
    
    * fix: echo commit message in case of failure
    
    * fix: add --no-merges flag so commit check works

---
+++ scripts/validate-commit-msg.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "==> Checking commit message <=="
+
+msg=$(git log --no-merges -1 --pretty=%B)
+echo "$msg" | grep -E '^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(.+\))?: .+' \
+  || (echo -e "Commit message \"$msg\" failed validation.\n" \
+      "Commits must follow https://www.conventionalcommits.org/en/v1.0.0-beta.2/#specification with one of these types:\n" \
+      "  build, ci, chore, docs, feat, fix, perf, refactor, revert, style, test" \
+      && exit 1)
