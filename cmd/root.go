e8e95c58bffab49ac42002e2d6bc46fdc36840d3
Matthew Fisher <matt.fisher@microsoft.com>
2019-01-08 13:05:51 -0800

    feat: use the Azure CLI as an auth method
    This PR allows a user to authenticate using the Azure CLI 2.0 for development purposes.
    
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- cmd/root.go
+++ cmd/root.go
@@ -109,7 +109,7 @@ type authArgs struct {
 func addAuthFlags(authArgs *authArgs, f *flag.FlagSet) {
 	f.StringVar(&authArgs.RawAzureEnvironment, "azure-env", "AzurePublicCloud", "the target Azure cloud")
 	f.StringVarP(&authArgs.rawSubscriptionID, "subscription-id", "s", "", "azure subscription id (required)")
-	f.StringVar(&authArgs.AuthMethod, "auth-method", "client_secret", "auth method (default:`client_secret`, `device`, `client_certificate`)")
+	f.StringVar(&authArgs.AuthMethod, "auth-method", "client_secret", "auth method (default:`client_secret`, `cli`, `client_certificate`, `device`)")
 	f.StringVar(&authArgs.rawClientID, "client-id", "", "client id (used with --auth-method=[client_secret|client_certificate])")
 	f.StringVar(&authArgs.ClientSecret, "client-secret", "", "client secret (used with --auth-mode=client_secret)")
 	f.StringVar(&authArgs.CertificatePath, "certificate-path", "", "path to client certificate (used with --auth-method=client_certificate)")
@@ -199,6 +199,8 @@ func (authArgs *authArgs) getClient() (armhelpers.AKSEngineClient, error) {
 		return nil, err
 	}
 	switch authArgs.AuthMethod {
+	case "cli":
+		client, err = armhelpers.NewAzureClientWithCLI(env, authArgs.SubscriptionID.String())
 	case "device":
 		client, err = armhelpers.NewAzureClientWithDeviceAuth(env, authArgs.SubscriptionID.String())
 	case "client_secret":
