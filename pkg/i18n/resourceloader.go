940ac93b28fc13a927410df955a21058bec7b2c5
Matt Boersma <Matt.Boersma@microsoft.com>
2019-02-07 10:59:03 -0700

    build: ignore generated files in code coverage

--- pkg/i18n/resourceloader.go
+++ pkg/i18n/resourceloader.go
@@ -3,7 +3,7 @@
 
 package i18n
 
-//go:generate go-bindata -nometadata -pkg $GOPACKAGE -prefix ../../ -o translations.go ../../translations/...
-//go:generate gofmt -s -l -w translations.go
+//go:generate go-bindata -nometadata -pkg $GOPACKAGE -prefix ../../ -o translations_generated.go ../../translations/...
+//go:generate gofmt -s -l -w translations_generated.go
 // resourceloader use go-bindata (https://github.com/go-bindata/go-bindata)
 // go-bindata is the way we handle embedded files, like binary, template, etc.
