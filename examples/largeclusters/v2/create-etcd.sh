b71aedbe999d60adeb841718911cfafd0af8879f
Khaled Henidak(Kal) <khnidk@microsoft.com>
2019-02-13 23:00:07 +0000

    etcd bootstrap

--- examples/largeclusters/v2/create-etcd.sh
+++ examples/largeclusters/v2/create-etcd.sh
@@ -9,16 +9,26 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #RG									resource_group 									mandatory
 #VNET_NAME					vnet name												mandatory
 #SUBNET_NAME				subnet name											mandatory
-#BASE_CLUSTER_NAME	basename for cluster resourcs		optional(default random)
+#BASE_CLUSTER_NAME	basename for cluster resources	optional(default random)
 #CLUSTER_SIZE				etcd cluster size								optional(default 3)
 #VM_SIZE						etcd vm size										optional(default: Standard_DS2_v2)
-#OUTPUT_DIR					locatio of generated certs			optional(default /tmp/${BASE_CLUSTER_NAME}
-
+#OUTPUT_DIR					location of generated certs			optional(default /tmp/${BASE_CLUSTER_NAME}
+#ETCD_VERSION       etcd version										optional, defaults to 3.3.10
 
 # Global Vars
 AV_SET_NAME=""
 ADMIN_USER=${ADMIN_USER:-azureuser}
 SSH_KEY_VALUE=${SSH_KEY_VALUE:-$(cat ~/.ssh/id_rsa.pub)}
+
+CLOUD_INIT_FILE="cloudinit.yaml"
+
+ETCD_DISCOVERY_FILE="etcd.discovery"
+ETCD_SERVER_CERT_DIR="etcd.server"
+ETCD_PEER_CERT_DIR="etcd.peer"
+ETCD_CLIENT_CERT_DIR="etcd.client"
+
+ETCD_ENV="DAEMON_ARGS=--name <<SERVER_NAME>> --peer-client-cert-auth --peer-trusted-ca-file=/etc/kubernetes/certs/peer.ca.crt --peer-cert-file=/etc/kubernetes/certs/peer.crt --peer-key-file=/etc/kubernetes/certs/peer.key --initial-advertise-peer-urls https://0.0.0.0:2380 --client-cert-auth --trusted-ca-file=/etc/kubernetes/certs/client.ca.crt --cert-file=/etc/kubernetes/certs/server.crt --key-file=/etc/kubernetes/certs/server.key --advertise-client-urls https://0.0.0.0:2379 --listen-client-urls https://0.0.0.0:2379 --discovery <<DISCOVERY>> --data-dir /var/lib/etcddisk --initial-cluster-state new"
+
 
 
 # Colors
@@ -48,7 +58,7 @@ err(){
 #creates random string (starts with letter)
 get_random(){
 	local length="$1"
-	if [[ -z "${length}" ]] || [[ "0" -eq "$lenght" ]]; then
+	if [[ -z "${length}" ]] || [[ "0" -eq "${length}" ]]; then
 		length="8" #default
 	fi
 	length=$((length - 1))
@@ -107,6 +117,11 @@ validate_and_default(){
 		CLUSTER_SIZE=3
 	fi
 
+	if [[ -z "${ETCD_VERSION}" ]]; then
+		ETCD_VERSION="3.3.10"
+		wrn "etcd version [ETCD_VERSION] was not set, defaulting to ${ETCD_VERSION}"
+	fi
+
 	AV_SET_NAME="${BASE_CLUSTER_NAME}_avset"
 	inf "Availability set name: ${AV_SET_NAME}"
 }
@@ -136,13 +151,154 @@ create_vms(){
 								 --storage-sku Premium_LRS \
 								 --admin-username "${ADMIN_USER}" \
 								 --ssh-key-value "${SSH_KEY_VALUE}" \
-								 --image ubuntults
+								 --image ubuntults \
+                 --custom-data "${OUTPUT_DIR}/${CLOUD_INIT_FILE}"
 		let current=current+1 
 	done
 }
 
+
 generate_certs(){
-	inf "Generating certs"
+	local server_cert_dir="${OUTPUT_DIR}/${ETCD_SERVER_CERT_DIR}"
+	local peer_cert_dir="${OUTPUT_DIR}/${ETCD_PEER_CERT_DIR}"
+	local client_cert_dir="${OUTPUT_DIR}/${ETCD_CLIENT_CERT_DIR}"
+
+	mkdir -p "${server_cert_dir}"
+	mkdir -p "${peer_cert_dir}"
+	mkdir -p "${client_cert_dir}"
+
+	inf "Generating etcd certs for server"
+	#CA
+	openssl genrsa -out "${server_cert_dir}/server.ca.key"  2048 2>/dev/null
+  #CA crt
+	openssl req -x509 \
+							-new \
+							-nodes \
+							-key "${server_cert_dir}/server.ca.key" \
+							-days $((5 * 365)) \
+							-out "${server_cert_dir}/server.ca.crt" \
+							-subj "/C=US" 2>/dev/null
+	# key
+	openssl genrsa \
+					-out "${server_cert_dir}/server.key" 4096 2>/dev/null
+
+  # csr
+	openssl req -new \
+							-key "${server_cert_dir}/server.key" \
+							-out "${server_cert_dir}/server.csr" \
+							-subj "/C=US" 2>/dev/null
+
+	# sign
+	openssl x509 -req \
+							-in "${server_cert_dir}/server.csr" \
+							-CA "${server_cert_dir}/server.ca.crt" \
+							-CAkey "${server_cert_dir}/server.ca.key" \
+							-CAcreateserial -out "${server_cert_dir}/server.crt" \
+							-days $((5 * 365))  2>/dev/null
+
+	inf "etcd server certs are in ${server_cert_dir}"
+	
+	inf "Generating etcd certs for peer"
+	#CA
+	openssl genrsa -out "${peer_cert_dir}/peer.ca.key"  2048 2>/dev/null
+  #CA crt
+	openssl req -x509 \
+							-new \
+							-nodes \
+							-key "${peer_cert_dir}/peer.ca.key" \
+							-days $((5 * 365)) \
+							-out "${peer_cert_dir}/peer.ca.crt" \
+							-subj "/C=US" 2>/dev/null
+	# key
+	openssl genrsa \
+					-out "${peer_cert_dir}/peer.key" 4096 2>/dev/null
+
+  # csr
+	openssl req -new \
+							-key "${peer_cert_dir}/peer.key" \
+							-out "${peer_cert_dir}/peer.csr" \
+							-subj "/C=US" 2>/dev/null
+
+	# sign
+	openssl x509 -req \
+							-in "${peer_cert_dir}/peer.csr" \
+							-CA "${peer_cert_dir}/peer.ca.crt" \
+							-CAkey "${peer_cert_dir}/peer.ca.key" \
+							-CAcreateserial -out "${peer_cert_dir}/peer.crt" \
+							-days $((5 * 365))  2>/dev/null
+
+ 	inf "etcd peer certs are in ${peer_cert_dir}"	
+ 	inf "Generating etcd certs for client"
+	#CA
+	openssl genrsa -out "${client_cert_dir}/client.ca.key"  2048 2>/dev/null
+  #CA crt
+	openssl req -x509 \
+							-new \
+							-nodes \
+							-key "${client_cert_dir}/client.ca.key" \
+							-days $((5 * 365)) \
+							-out "${client_cert_dir}/client.ca.crt" \
+							-subj "/C=US" 2>/dev/null
+	# key
+	openssl genrsa \
+					-out "${client_cert_dir}/client.key" 4096 2>/dev/null
+
+  # csr
+	openssl req -new \
+							-key "${client_cert_dir}/client.key" \
+							-out "${client_cert_dir}/client.csr" \
+							-subj "/C=US" 2>/dev/null
+
+	# sign
+	openssl x509 -req \
+							-in "${client_cert_dir}/client.csr" \
+							-CA "${client_cert_dir}/client.ca.crt" \
+							-CAkey "${client_cert_dir}/client.ca.key" \
+							-CAcreateserial -out "${client_cert_dir}/client.crt" \
+							-days $((5 * 365))  2>/dev/null
+
+ inf "etcd client certs are in ${client_cert_dir}"	
+
+}
+
+# all offline work
+# 1- Get cluster discovery token
+# 2- Generates the certs
+# 3- Put vars in cloud init 
+pre_work(){
+	inf "Generating etcd discovery token"
+	curl -s "https://discovery.etcd.io/new?size=${CLUSTER_SIZE}" --output "${OUTPUT_DIR}/${ETCD_DISCOVERY_FILE}"
+	inf "etcd token generated in ${OUTPUT_DIR}/${ETCD_DISCOVERY_FILE}"
+
+	generate_certs
+
+	# Modify cloud init
+	inf "Creating cloudinit file"
+	cloudinit_src="${CURRENT_DIR}/${CLOUD_INIT_FILE}"
+	cloudinit_dst="${OUTPUT_DIR}/${CLOUD_INIT_FILE}"
+	cp "${cloudinit_src}" "${cloudinit_dst}"
+
+	local server_cert_dir="${OUTPUT_DIR}/${ETCD_SERVER_CERT_DIR}"
+	local peer_cert_dir="${OUTPUT_DIR}/${ETCD_PEER_CERT_DIR}"
+	local client_cert_dir="${OUTPUT_DIR}/${ETCD_CLIENT_CERT_DIR}"
+	
+	# certs, keys et al
+	sed -i "s/<PEER_CA>/$(cat ${peer_cert_dir}/peer.ca.crt | base64 -w 0)/g" ${cloudinit_dst}
+	sed -i "s/<PEER_CRT>/$(cat ${peer_cert_dir}/peer.crt | base64 -w 0)/g" ${cloudinit_dst}
+	sed -i "s/<PEER_KEY>/$(cat ${peer_cert_dir}/peer.key | base64 -w 0)/g" ${cloudinit_dst}
+
+	sed -i "s/<SERVER_CRT>/$(cat ${server_cert_dir}/server.crt | base64 -w 0)/g" ${cloudinit_dst}
+	sed -i "s/<SERVER_KEY>/$(cat ${server_cert_dir}/server.key | base64 -w 0)/g" ${cloudinit_dst}
+
+	sed -i "s/<CLIENT_CA>/$(cat ${client_cert_dir}/client.ca.crt | base64 -w 0)/g" ${cloudinit_dst}
+
+	# etcd env file 
+	sed -i "s/<ETCD_ENV>/$(echo -n ${ENV} | base64 -w 0)/g" ${cloudinit_dst}
+
+	# etcd version
+	sed -i "s/<ETCD_VERSION>/${ETCD_VERSION}/g" ${cloudinit_dst}
+
+	inf "cloudinit file is in ${cloudinit_dst}"
 }
 
 main(){
@@ -150,6 +306,7 @@ main(){
 	inf "Script dir is [${CURRENT_DIR}]"
 
 	validate_and_default
+	pre_work
 	create_vms
 }
 
