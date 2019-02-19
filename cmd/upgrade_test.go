25fa2ab21e70b676b1ae37902fa25428470f249d
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-02-19 14:20:11 -0800

    test: using go test instead of ginkgo

--- cmd/upgrade_test.go
+++ cmd/upgrade_test.go
@@ -4,104 +4,102 @@
 package cmd
 
 import (
-	. "github.com/onsi/ginkgo"
+	"testing"
+
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
-var _ = Describe("the upgrade command", func() {
+func TestUpgradeCommandShouldBeValidated(t *testing.T) {
+	g := NewGomegaWithT(t)
+	r := &cobra.Command{}
 
-	It("should create an upgrade command", func() {
-		output := newUpgradeCmd()
+	cases := []struct {
+		uc          *upgradeCmd
+		expectedErr error
+	}{
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "",
+				deploymentDirectory: "_output/test",
+				upgradeVersion:      "1.8.9",
+				location:            "centralus",
+				timeoutInMinutes:    60,
+			},
+			expectedErr: errors.New("--resource-group must be specified"),
+		},
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "test",
+				deploymentDirectory: "_output/test",
+				upgradeVersion:      "1.8.9",
+				location:            "",
+				timeoutInMinutes:    60,
+			},
+			expectedErr: errors.New("--location must be specified"),
+		},
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "test",
+				deploymentDirectory: "_output/test",
+				upgradeVersion:      "",
+				location:            "southcentralus",
+				timeoutInMinutes:    60,
+			},
+			expectedErr: errors.New("--upgrade-version must be specified"),
+		},
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "test",
+				deploymentDirectory: "",
+				upgradeVersion:      "1.9.0",
+				location:            "southcentralus",
+				timeoutInMinutes:    60,
+			},
+			expectedErr: errors.New("--deployment-dir must be specified"),
+		},
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "test",
+				deploymentDirectory: "",
+				upgradeVersion:      "1.9.0",
+				location:            "southcentralus",
+				timeoutInMinutes:    60,
+			},
+			expectedErr: errors.New("--deployment-dir must be specified"),
+		},
+		{
+			uc: &upgradeCmd{
+				resourceGroupName:   "test",
+				deploymentDirectory: "_output/mydir",
+				upgradeVersion:      "1.9.0",
+				location:            "southcentralus",
+			},
+			expectedErr: nil,
+		},
+	}
 
-		Expect(output.Use).Should(Equal(upgradeName))
-		Expect(output.Short).Should(Equal(upgradeShortDescription))
-		Expect(output.Long).Should(Equal(upgradeLongDescription))
-		Expect(output.Flags().Lookup("location")).NotTo(BeNil())
-		Expect(output.Flags().Lookup("resource-group")).NotTo(BeNil())
-		Expect(output.Flags().Lookup("deployment-dir")).NotTo(BeNil())
-		Expect(output.Flags().Lookup("upgrade-version")).NotTo(BeNil())
-	})
-
-	It("should validate an upgrade command", func() {
-		r := &cobra.Command{}
-
-		cases := []struct {
-			uc          *upgradeCmd
-			expectedErr error
-		}{
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "",
-					deploymentDirectory: "_output/test",
-					upgradeVersion:      "1.8.9",
-					location:            "centralus",
-					timeoutInMinutes:    60,
-				},
-				expectedErr: errors.New("--resource-group must be specified"),
-			},
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "test",
-					deploymentDirectory: "_output/test",
-					upgradeVersion:      "1.8.9",
-					location:            "",
-					timeoutInMinutes:    60,
-				},
-				expectedErr: errors.New("--location must be specified"),
-			},
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "test",
-					deploymentDirectory: "_output/test",
-					upgradeVersion:      "",
-					location:            "southcentralus",
-					timeoutInMinutes:    60,
-				},
-				expectedErr: errors.New("--upgrade-version must be specified"),
-			},
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "test",
-					deploymentDirectory: "",
-					upgradeVersion:      "1.9.0",
-					location:            "southcentralus",
-					timeoutInMinutes:    60,
-				},
-				expectedErr: errors.New("--deployment-dir must be specified"),
-			},
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "test",
-					deploymentDirectory: "",
-					upgradeVersion:      "1.9.0",
-					location:            "southcentralus",
-					timeoutInMinutes:    60,
-				},
-				expectedErr: errors.New("--deployment-dir must be specified"),
-			},
-			{
-				uc: &upgradeCmd{
-					resourceGroupName:   "test",
-					deploymentDirectory: "_output/mydir",
-					upgradeVersion:      "1.9.0",
-					location:            "southcentralus",
-				},
-				expectedErr: nil,
-			},
+	for _, c := range cases {
+		err := c.uc.validate(r)
+		if c.expectedErr != nil && err != nil {
+			g.Expect(err.Error()).To(Equal(c.expectedErr.Error()))
+		} else {
+			g.Expect(err).To(BeNil())
+			g.Expect(c.expectedErr).To(BeNil())
 		}
+	}
+}
 
-		for _, c := range cases {
-			err := c.uc.validate(r)
-			if c.expectedErr != nil && err != nil {
-				Expect(err.Error()).To(Equal(c.expectedErr.Error()))
-			} else {
-				Expect(err).To(BeNil())
-				Expect(c.expectedErr).To(BeNil())
-			}
-		}
+func TestCreateUpgradeCommandSuccesfully(t *testing.T) {
+	g := NewGomegaWithT(t)
+	output := newUpgradeCmd()
 
-	})
-
-})
+	g.Expect(output.Use).Should(Equal(upgradeName))
+	g.Expect(output.Short).Should(Equal(upgradeShortDescription))
+	g.Expect(output.Long).Should(Equal(upgradeLongDescription))
+	g.Expect(output.Flags().Lookup("location")).NotTo(BeNil())
+	g.Expect(output.Flags().Lookup("resource-group")).NotTo(BeNil())
+	g.Expect(output.Flags().Lookup("deployment-dir")).NotTo(BeNil())
+	g.Expect(output.Flags().Lookup("upgrade-version")).NotTo(BeNil())
+}
