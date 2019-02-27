346f8eef85834f204195322648e76410067cf5f8
Cecile Robert-Michon <cerobert@microsoft.com>
2019-02-27 09:39:59 -0800

    ci: add storage account cleanup to VHD pipeline (#589)

--- packer.mk
+++ packer.mk
@@ -8,10 +8,13 @@ az-login:
 	az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
 
 run-packer: az-login
-	@packer version && $(MAKE) init-packer && ($(MAKE) build-packer | tee packer-output)
+	@packer version && $(MAKE) init-packer | tee packer-output && ($(MAKE) build-packer | tee -a packer-output)
 
 az-copy: az-login
 	azcopy --source "${OS_DISK_SAS}" --destination "${CLASSIC_BLOB}/${VHD_NAME}" --dest-sas "${CLASSIC_SAS_TOKEN}"
+
+delete-sa: az-login
+	az storage account delete -n ${SA_NAME} -g ${AZURE_RESOURCE_GROUP_NAME} --yes
 
 generate-sas: az-login
 	az storage container generate-sas --name vhds --permissions lr --connection-string "${CLASSIC_SA_CONNECTION_STRING}" --start ${START_DATE} --expiry ${EXPIRY_DATE} | tr -d '"' | tee -a vhd-sas && cat vhd-sas
