4064a43617466229fa1ba5dc19d75d6d2a1f74d0
Jack Francis <jackfrancis@gmail.com>
2019-03-04 17:11:47 -0800

    chore: install -proposed systemd package (#630)
    * chore: install -proposed systemd package
    
    * chore: use standard apt_get_install func
    
    * chore: add systemd install message

--- parts/k8s/kubernetesprovisionsource.sh
+++ parts/k8s/kubernetesprovisionsource.sh
@@ -31,6 +31,7 @@ ERR_CNI_DOWNLOAD_TIMEOUT=41 # Timeout waiting for CNI download(s)
 ERR_MS_PROD_DEB_DOWNLOAD_TIMEOUT=42 # Timeout waiting for https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 ERR_MS_PROD_DEB_PKG_ADD_FAIL=43 # Failed to add repo pkg file
 #ERR_FLEXVOLUME_DOWNLOAD_TIMEOUT=44 # Failed to add repo pkg file -- DEPRECATED
+ERR_SYSTEMD_INSTALL_FAIL=48 # Unable to install required systemd version
 ERR_MODPROBE_FAIL=49 # Unable to load a kernel module using modprobe
 ERR_OUTBOUND_CONN_FAIL=50 # Unable to establish outbound connection
 ERR_KATA_KEY_DOWNLOAD_TIMEOUT=60 # Timeout waiting to download kata repo key
