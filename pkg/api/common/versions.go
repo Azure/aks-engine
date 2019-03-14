3d7f2b1dc08f28887ce802063e022f2671e4c0fe
Matt Boersma <Matt.Boersma@microsoft.com>
2019-03-14 11:01:36 -0600

    chore: enable Kubernetes v1.14.0-beta.2 (#756)
    * chore: enable Kubernetes v1.14.0-beta.2
    
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md#changelog-since-v1140-beta1
    
    * chore: disable k8s 1.14.0-alpha.2 support

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -107,8 +107,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.13.3":         true,
 	"1.13.4":         true,
 	"1.14.0-alpha.1": false,
-	"1.14.0-alpha.2": true,
+	"1.14.0-alpha.2": false,
 	"1.14.0-beta.1":  true,
+	"1.14.0-beta.2":  true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
