3e23f6bd7b38be39e0d979215fa870db19573e3a
Tariq Ibrahim <tariq181290@gmail.com>
2019-03-14 18:29:28 -0700

    style: add godoc to the armtype structs used in the template refactor (#772)

--- pkg/engine/armtype.go
+++ pkg/engine/armtype.go
@@ -19,6 +19,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-10-01/compute"
 )
 
+// ARMResource contains the fields that are common to all Azure Resource Manager objects.
 type ARMResource struct {
 	APIVersion string            `json:"apiVersion,omitempty"`
 	Copy       map[string]string `json:"copy,omitempty"`
@@ -29,86 +30,103 @@ func (arm ARMResource) MarshalJSON() ([]byte, error) {
 	return json.Marshal(arm)
 }
 
+// VirtualMachineARM embeds the ARMResource type in compute.VirtualMachine.
 type VirtualMachineARM struct {
 	ARMResource
 	compute.VirtualMachine
 }
 
+// VirtualMachineScaleSetARM embeds the ARMResource type in compute.VirtualMachineScaleSet.
 type VirtualMachineScaleSetARM struct {
 	ARMResource
 	compute.VirtualMachineScaleSet
 }
 
+// VirtualMachineExtensionARM embeds the ARMResource type in compute.VirtualMachineExtension.
 type VirtualMachineExtensionARM struct {
 	ARMResource
 	compute.VirtualMachineExtension
 }
 
+// RoleAssignmentARM embeds the ARMResource type in authorization.RoleAssignment.
 type RoleAssignmentARM struct {
 	ARMResource
 	authorization.RoleAssignment
 }
 
+// AvailabilitySetARM embeds the ARMResource type in compute.AvailabilitySet.
 type AvailabilitySetARM struct {
 	ARMResource
 	compute.AvailabilitySet
 }
 
+// StorageAccountARM embeds the ARMResource type in storage.Account.
 type StorageAccountARM struct {
 	ARMResource
 	storage.Account
 }
 
+// SystemRoleAssignmentARM embeds the ARMResource type in authorization.SystemRoleAssignment(2018-01-01-preview).
 type SystemRoleAssignmentARM struct {
 	ARMResource
 	sysauth.RoleAssignment
 }
 
+// VirtualNetworkARM embeds the ARMResource type in network.VirtualNetwork.
 type VirtualNetworkARM struct {
 	ARMResource
 	network.VirtualNetwork
 }
 
+// NetworkSecurityGroupARM embeds the ARMResource type in network.SecurityGroup.
 type NetworkSecurityGroupARM struct {
 	ARMResource
 	network.SecurityGroup
 }
 
+// RouteTableARM embeds the ARMResource type in network.RouteTable.
 type RouteTableARM struct {
 	ARMResource
 	network.RouteTable
 }
 
+// PublicIPAddressARM embeds the ARMResource type in network.PublicIPAddress.
 type PublicIPAddressARM struct {
 	ARMResource
 	network.PublicIPAddress
 }
 
+// LoadBalancerARM embeds the ARMResource type in network.LoadBalancer.
 type LoadBalancerARM struct {
 	ARMResource
 	network.LoadBalancer
 }
 
+// NetworkInterfaceARM embeds the ARMResource type in network.Interface.
 type NetworkInterfaceARM struct {
 	ARMResource
 	network.Interface
 }
 
+// DocumentDBAccountARM embeds the ARMResource type in documentdb.DatabaseAccountCreateUpdateParameters.
 type DocumentDBAccountARM struct {
 	ARMResource
 	documentdb.DatabaseAccountCreateUpdateParameters
 }
 
+// KeyVaultARM embeds the ARMResource type in keyvault.Vault.
 type KeyVaultARM struct {
 	ARMResource
 	keyvault.Vault
 }
 
+// UserAssignedIdentitiesARM embeds the ARMResource type in msi.Identity.
 type UserAssignedIdentitiesARM struct {
 	ARMResource
 	msi.Identity
 }
 
+// ImageARM embeds the ARMResource type in compute.Image.
 type ImageARM struct {
 	ARMResource
 	compute.Image
