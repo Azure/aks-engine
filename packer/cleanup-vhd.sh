c075b62e075d00dcf6e4247ee040375dff831871
Cecile Robert-Michon <cerobert@microsoft.com>
2019-03-19 15:48:23 -0700

    fix: move /var/log/ during VHD build to a vhd specific directory (#777)

--- packer/cleanup-vhd.sh
+++ packer/cleanup-vhd.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eux
 
-## Cleanup packer SSH key and machine ID generated for this boot
+# Cleanup packer SSH key and machine ID generated for this boot
 rm -f /root/.ssh/authorized_keys
 rm -f /home/packer/.ssh/authorized_keys
 rm -f /etc/machine-id
 touch /etc/machine-id
+
+# Move logs from VHD creation out of /var/log 
+sudo mv /var/log /var/log.vhd
+sudo mkdir /var/log
