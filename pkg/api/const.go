cfaf0088ac70d8e5847d9107a8ad4bbd698e8783
Gerardo Saca <gsacavdm@hotmail.com>
2019-03-18 20:32:16 -0700

    Fixing lint issue

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -421,8 +421,8 @@ const (
 	// AzurePublicCloud is a const string reference identifier for public cloud
 	AzurePublicCloud = "AzurePublicCloud"
 	// AzureChinaCloud is a const string reference identifier for china cloud
-	AzureChinaCloud        = "AzureChinaCloud"
-	azureGermanCloud       = "AzureGermanCloud"
+	AzureChinaCloud  = "AzureChinaCloud"
+	azureGermanCloud = "AzureGermanCloud"
 	// AzureUSGovernmentCloud is a const string reference identifier for us government cloud
 	AzureUSGovernmentCloud = "AzureUSGovernmentCloud"
 	// AzureStackCloud is a const string reference identifier for Azure Stack cloud
