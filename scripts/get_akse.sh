0d2c0f56aab1507e567c60258108a232df1c70a8
Jorge Palma <jpalma@microsoft.com>
2019-03-22 10:12:38 -0700

    fix to license header of Linux install script

--- scripts/get_akse.sh
+++ scripts/get_akse.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-# Copyright Jorge Palma.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# The install script is based off of the MIT-licensed script from glide,
-# the package manager for Go: https://github.com/Masterminds/glide.sh/blob/master/get
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
 
 PROJECT_NAME="aks-engine"
 
