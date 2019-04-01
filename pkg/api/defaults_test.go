57e4a413b811e704878aa10e2eaec54c6fba0c65
tamilmani1989 <tamilmani1989@gmail.com>
2019-04-01 13:29:14 -0700

    perf: Network Monitor v0.0.6 integration (#912)

--- pkg/api/defaults_test.go
+++ pkg/api/defaults_test.go
@@ -199,7 +199,7 @@ func TestAssignDefaultAddonImages(t *testing.T) {
 		NVIDIADevicePluginAddonName:        "nvidia/k8s-device-plugin:1.10",
 		ContainerMonitoringAddonName:       "microsoft/oms:ciprod01092019",
 		IPMASQAgentAddonName:               "k8s.gcr.io/ip-masq-agent-amd64:v2.0.0",
-		AzureCNINetworkMonitoringAddonName: "containernetworking/networkmonitor:v0.0.5",
+		AzureCNINetworkMonitoringAddonName: "containernetworking/networkmonitor:v0.0.6",
 		DefaultDNSAutoscalerAddonName:      "k8s.gcr.io/cluster-proportional-autoscaler-amd64:1.1.1",
 		DefaultHeapsterAddonName:           "k8s.gcr.io/heapster-amd64:v1.5.4",
 	}
