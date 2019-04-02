cbfdcb625e8a7ceaff493b2e1b4a555a8c608647
Hongbin Cao <honcao@microsoft.com>
2019-04-02 15:39:33 -0700

    test: unit test for azure stack AzureClient

---
+++ pkg/armhelpers/azurestack/network_test.go
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+package azurestack
+
+import (
+	"context"
+	"testing"
+)
+
+func TestDeleteNetworkInterface(t *testing.T) {
+	mc := NewHTTPMockClient()
+	mc.Activate()
+	defer mc.DeactivateAndReset()
+	mc.RegisterLogin()
+	env := mc.GetEnvironment()
+	azureClient, err := NewAzureClientWithClientSecret(env, subscriptionID, "clientID", "secret")
+	if err != nil {
+		t.Fatalf("can not get client %s", err)
+	}
+
+	mc.RegisterDeleteNetworkInterface()
+	err = azureClient.DeleteNetworkInterface(context.Background(), resourceGroup, virtualNicName)
+	if err != nil {
+		t.Error(err)
+	}
+}
