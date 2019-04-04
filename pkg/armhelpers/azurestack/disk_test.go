6abc0ab42ca2023732147d3dd8f70c7ad457def4
Hongbin <honcao@microsoft.com>
2019-04-03 17:03:43 -0700

    test: unit test for azure stack AzureClient (#934)

---
+++ pkg/armhelpers/azurestack/disk_test.go
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+package azurestack
+
+import (
+	"context"
+	"testing"
+)
+
+func TestDeleteManagedDisk(t *testing.T) {
+	mc := NewHTTPMockClient()
+	mc.Activate()
+	defer mc.DeactivateAndReset()
+	mc.RegisterLogin()
+	env := mc.GetEnvironment()
+
+	azureClient, err := NewAzureClientWithClientSecret(env, subscriptionID, "clientID", "secret")
+	if err != nil {
+		t.Fatalf("can not get client %s", err)
+	}
+
+	mc.RegisterDeleteManagedDisk()
+
+	err = azureClient.DeleteManagedDisk(context.Background(), resourceGroup, virutalDiskName)
+	if err != nil {
+		t.Error(err)
+	}
+}
