dec18fc11b93d9a864993ee24962b01407c4a056
Jack Francis <jackfrancis@gmail.com>
2019-04-05 13:30:36 -0700

    chore: only use new calico CSE config for AKS Engine (#952)

--- parts/k8s/kubernetesconfigs.sh
+++ parts/k8s/kubernetesconfigs.sh
@@ -190,6 +190,19 @@ configureCNI() {
     # needed for the iptables rules to work on bridges
     retrycmd_if_failure 120 5 25 modprobe br_netfilter || exit $ERR_MODPROBE_FAIL
     echo -n "br_netfilter" > /etc/modules-load.d/br_netfilter.conf
+    if [ "$IS_HOSTED_MASTER" = "true" ]; then
+        configureCNIIPTablesAKS
+    else
+        configureCNIIPTables
+    fi
+    if [[ "${NETWORK_PLUGIN}" = "cilium" ]]; then
+        systemctl enable sys-fs-bpf.mount
+        systemctl restart sys-fs-bpf.mount
+        REBOOTREQUIRED=true
+    fi
+}
+
+configureCNIIPTables() {
     if [[ "${NETWORK_PLUGIN}" = "azure" ]]; then
         mv $CNI_BIN_DIR/10-azure.conflist $CNI_CONFIG_DIR/
         chmod 600 $CNI_CONFIG_DIR/10-azure.conflist
@@ -198,10 +211,15 @@ configureCNI() {
         fi
         /sbin/ebtables -t nat --list
     fi
-    if [[ "${NETWORK_PLUGIN}" = "cilium" ]]; then
-        systemctl enable sys-fs-bpf.mount
-        systemctl restart sys-fs-bpf.mount
-        REBOOTREQUIRED=true
+}
+
+configureCNIIPTablesAKS() {
+    if [[ "${NETWORK_PLUGIN}" = "azure" ]]; then
+        if [[ "${NETWORK_POLICY}" != "calico" ]]; then
+            mv $CNI_BIN_DIR/10-azure.conflist $CNI_CONFIG_DIR/
+            chmod 600 $CNI_CONFIG_DIR/10-azure.conflist
+        fi
+        /sbin/ebtables -t nat --list
     fi
 }
 
