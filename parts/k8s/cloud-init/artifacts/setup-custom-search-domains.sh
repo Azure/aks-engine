15ebd7394e8c6ad059367194ca453a32406819c8
Matt Boersma <Matt.Boersma@microsoft.com>
2019-04-10 19:03:23 -0600

    style: sudo doesn't affect redirects
    See https://github.com/koalaman/shellcheck/wiki/SC2024

--- parts/k8s/cloud-init/artifacts/setup-custom-search-domains.sh
+++ parts/k8s/cloud-init/artifacts/setup-custom-search-domains.sh
@@ -2,7 +2,7 @@
 set -x
 source /opt/azure/containers/provision_source.sh
 
-sudo echo "  dns-search <searchDomainName>" >> /etc/network/interfaces.d/50-cloud-init.cfg
+echo "  dns-search <searchDomainName>" | sudo tee -a /etc/network/interfaces.d/50-cloud-init.cfg > /dev/null
 systemctl_restart 20 5 10 restart networking
 wait_for_apt_locks
 retrycmd_if_failure 10 5 120 apt-get -y install realmd sssd sssd-tools samba-common samba samba-common python2.7 samba-libs packagekit
