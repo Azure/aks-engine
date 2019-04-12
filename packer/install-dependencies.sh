ad4dd59ac8192d869b64b522f9d42b8f82da9029
Jack Francis <jack.francis@microsoft.com>
2019-04-12 15:07:44 -0700

    chore: fix packer config

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -4,10 +4,13 @@ source /home/packer/provision_source.sh
 source /home/packer/cis.sh
 
 RELEASE_NOTES_FILEPATH=/var/log/azure/golden-image-install.complete
+SYSCTL_CONFIG_SRC=/home/packer/sysctl-d-60-CIS.conf
+SYSCTL_CONFIG_DEST=/etc/sysctl.d/60-CIS.conf
 
 echo "Starting build on " `date` > ${RELEASE_NOTES_FILEPATH}
 echo "Using kernel:" >> ${RELEASE_NOTES_FILEPATH}
 cat /proc/version | tee -a ${RELEASE_NOTES_FILEPATH}
+cp $SYSCTL_CONFIG_SRC $SYSCTL_CONFIG_DEST
 sysctl_reload 20 5 10
 
 echo ""
