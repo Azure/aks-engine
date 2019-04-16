f277204383525b6dcce48e7e526041236e9a9e97
Jack Francis <jack.francis@microsoft.com>
2019-04-16 12:03:45 -0700

    test: check for ubuntu at test runtime

--- test/e2e/kubernetes/scripts/installed-packages-validate.sh
+++ test/e2e/kubernetes/scripts/installed-packages-validate.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
-ENSURE_NOT_INSTALLED="
-postfix
-"
-for PACKAGE in ${ENSURE_NOT_INSTALLED}; do
-    apt list --installed | grep $PACKAGE && exit 1
-done
+OS=$(cat /etc/*-release | grep ^ID= | tr -d 'ID="' | awk '{print toupper($0)}')
+UBUNTU_OS_NAME="UBUNTU"
 
-ENSURE_INSTALLED="
-apt-transport-https
-blobfuse
-ca-certificates
-ceph-common
-cgroup-lite
-cifs-utils
-conntrack
-ebtables
-ethtool
-fuse
-git
-glusterfs-client
-init-system-helpers
-iproute2
-ipset
-iptables
-jq
-mount
-nfs-common
-pigz
-socat
-util-linux
-xz-utils
-zip
-htop
-iotop
-iftop
-sysstat
-"
-for PACKAGE in ${ENSURE_INSTALLED}; do
-    apt list --installed | grep $PACKAGE || exit 1
-done
+if [[ $OS == $UBUNTU_OS_NAME ]]; then
+    ENSURE_NOT_INSTALLED="
+    postfix
+    "
+    for PACKAGE in ${ENSURE_NOT_INSTALLED}; do
+        apt list --installed | grep $PACKAGE && exit 1
+    done
+
+    ENSURE_INSTALLED="
+    apt-transport-https
+    blobfuse
+    ca-certificates
+    ceph-common
+    cgroup-lite
+    cifs-utils
+    conntrack
+    ebtables
+    ethtool
+    fuse
+    git
+    glusterfs-client
+    init-system-helpers
+    iproute2
+    ipset
+    iptables
+    jq
+    mount
+    nfs-common
+    pigz
+    socat
+    util-linux
+    xz-utils
+    zip
+    htop
+    iotop
+    iftop
+    sysstat
+    "
+    for PACKAGE in ${ENSURE_INSTALLED}; do
+        apt list --installed | grep $PACKAGE || exit 1
+    done
+fi
