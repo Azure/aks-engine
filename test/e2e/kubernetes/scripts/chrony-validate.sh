d8d5f93bfa6901f25ee9894bd906abf1f94aac49
Jack Francis <jack.francis@microsoft.com>
2019-04-16 14:25:30 -0700

    chore: use smaller adjustment, use more ntp sources

--- test/e2e/kubernetes/scripts/chrony-validate.sh
+++ test/e2e/kubernetes/scripts/chrony-validate.sh
@@ -3,4 +3,4 @@
 set -x
 timedatectl status | egrep 'NTP synchronized: yes' || exit 1
 systemctl status chrony || exit 3
-chronyc sources -v | egrep 'Number of sources = 1' || exit 1
+chronyc sources -v | egrep 'Number of sources = 2' || exit 1
