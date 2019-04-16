263f97cfb1de3886de5ec91d4eff7a99e4327419
Jack Francis <jack.francis@microsoft.com>
2019-04-16 16:33:18 -0700

    chore: update pool config and validate at least 3 sources

--- parts/k8s/cloud-init/artifacts/cse_helpers.sh
+++ parts/k8s/cloud-init/artifacts/cse_helpers.sh
@@ -152,6 +152,7 @@ apt_get_update() {
     apt_update_output=/tmp/apt-get-update.out
     for i in $(seq 1 $retries); do
         wait_for_apt_locks
+        export DEBIAN_FRONTEND=noninteractive
         dpkg --configure -a
         apt-get -f -y install
         apt-get update 2>&1 | tee $apt_update_output | grep -E "^([WE]:.*)|([eE]rr.*)$"
@@ -188,6 +189,7 @@ apt_get_dist_upgrade() {
   apt_dist_upgrade_output=/tmp/apt-get-dist-upgrade.out
   for i in $(seq 1 $retries); do
     wait_for_apt_locks
+    export DEBIAN_FRONTEND=noninteractive
     dpkg --configure -a
     apt-get -f -y install
     apt-get dist-upgrade -y 2>&1 | tee $apt_dist_upgrade_output | grep -E "^([WE]:.*)|([eE]rr.*)$"
--- test/e2e/kubernetes/scripts/chrony-validate.sh
+++ test/e2e/kubernetes/scripts/chrony-validate.sh
@@ -3,4 +3,4 @@
 set -x
 timedatectl status | egrep 'NTP synchronized: yes' || exit 1
 systemctl status chrony || exit 3
-chronyc sources -v | egrep 'Number of sources = 2' || exit 1
+[ -s $(chronyc sources -v | grep 'Number of sources =' | awk '0+$6 <3 {print}') ] || exit 1
