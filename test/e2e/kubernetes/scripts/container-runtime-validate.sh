70a30f3e11bd9eee23a0d4b8e4b4a4a0a426d10f
Jack Francis <jack.francis@microsoft.com>
2019-04-17 16:01:42 -0700

    test: a working test

--- test/e2e/kubernetes/scripts/container-runtime-validate.sh
+++ test/e2e/kubernetes/scripts/container-runtime-validate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # should have the expected java MaxHeapSize configuration
+MEMORY_LIMIT=100000000
 set -x
-docker run -m 100m adoptopenjdk/openjdk11:jdk-11.0.2.9-alpine java -XX:+UnlockDiagnosticVMOptions -XX:+PrintCommandLineFlags -version | grep 'MaxHeapSize=52428800' || exit 1
+docker run -m $MEMORY_LIMIT openjdk java -XX:+UnlockDiagnosticVMOptions -XX:+PrintCommandLineFlags -version > /tmp/java-runtime.out || exit 1
+[ -s `cat /tmp/java-runtime.out | awk '{print $2}' | sed 's,.*=,,' | awk -v limit="$MEMORY_LIMIT" '0+$1 >= limit {print}'` ]
+if [ $? -ne 0  ]; then
+  echo "java should not have a MaxHeapSize greater than its container runtime allows"
+  echo "showing the MaxHeapSize settings for a java runtime inside a container with a memory limit of ${MEMORY_LIMIT} bytes"
+  cat /tmp/java-runtime.out
+  exit 1
+fi
