23192d0b18af38c1a1491221f29aafe7fb2dbd7c
Jack Francis <jack.francis@microsoft.com>
2019-04-18 08:54:36 -0700

    chore: adoptopenjdk/openjdk11:jdk-11.0.2.9-alpine is smaller

--- test/e2e/kubernetes/scripts/container-runtime-validate.sh
+++ test/e2e/kubernetes/scripts/container-runtime-validate.sh
@@ -3,7 +3,7 @@
 # should have the expected java MaxHeapSize configuration
 MEMORY_LIMIT=100000000
 set -x
-docker run -m $MEMORY_LIMIT openjdk java -XX:+UnlockDiagnosticVMOptions -XX:+PrintCommandLineFlags -version > /tmp/java-runtime.out || exit 1
+docker run -m $MEMORY_LIMIT adoptopenjdk/openjdk11:jdk-11.0.2.9-alpine java -XX:+UnlockDiagnosticVMOptions -XX:+PrintCommandLineFlags -version > /tmp/java-runtime.out || exit 1
 [ -s `cat /tmp/java-runtime.out | awk '{print $2}' | sed 's,.*=,,' | awk -v limit="$MEMORY_LIMIT" '0+$1 >= limit {print}'` ]
 if [ $? -ne 0  ]; then
   echo "java should not have a MaxHeapSize greater than its container runtime allows"
