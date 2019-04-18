c367e48d75792444cdbf5a53fb23dc029d8048b6
Jack Francis <jackfrancis@gmail.com>
2019-04-18 15:01:33 -0700

    test: add ubuntu time sync E2E validation (#1080)
    * test: add time sync E2E validation
    
    * test: make this test work for 18.04
    
    * test: prefer GetReady

---
+++ test/e2e/kubernetes/scripts/time-sync-validate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RELEASE=$(cat /etc/lsb-release | grep ^DISTRIB_RELEASE= | tr -d 'DISTRIB_RELEASE="' | awk '{print toupper($0)}')
+# verify that timesyncd configuration is healthy
+set -x
+if [[ $RELEASE == "16.04" ]]; then
+  sudo timedatectl status | grep 'Network time on: yes' || exit 1
+  sudo timedatectl status | grep 'NTP synchronized: yes' || exit 1
+elif [[ $RELEASE == "18.04" ]]; then
+  sudo timedatectl status | grep 'systemd-timesyncd.service active: yes' || exit 1
+  sudo timedatectl status | grep 'System clock synchronized: yes' || exit 1
+  
+fi
+sudo timedatectl status | grep 'RTC in local TZ: no' || exit 1
+sudo systemctl status systemd-timesyncd | grep 'Active: active' || exit 1
+sudo systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || exit 1
