9dbcb6eb8e2e798d509f628d7393eae1f1012880
Jack Francis <jackfrancis@gmail.com>
2019-04-19 16:03:45 -0700

    chore(CIS): kernel module hardening for non-essential filesystem types (#1105)

--- test/e2e/kubernetes/scripts/modprobe-config-validate.sh
+++ test/e2e/kubernetes/scripts/modprobe-config-validate.sh
@@ -9,3 +9,15 @@ modprobe -n -v sctp | egrep 'install /bin/true' || exit 1
 [ -s `lsmod | grep sctp` ] || exit 1
 modprobe -n -v rds | egrep 'install /bin/true' || exit 1
 [ -s `lsmod | grep rds` ] || exit 1
+modprobe -n -v cramfs | egrep 'install /bin/true' || exit 1
+[ -s `lsmod | grep cramfs` ] || exit 1
+modprobe -n -v freevxfs | egrep 'install /bin/true' || exit 1
+[ -s `lsmod | grep freevxfs` ] || exit 1
+modprobe -n -v jffs2 | egrep 'install /bin/true' || exit 1
+[ -s `lsmod | grep jffs2` ] || exit 1
+modprobe -n -v hfs | egrep 'install /bin/true' || exit 1
+[ -s `lsmod | grep hfs` ] || exit 1
+modprobe -n -v hfsplus | egrep 'install /bin/true' || exit 1
+[ -s `lsmod | grep hfsplus` ] || exit 1
+[ -s `lsmod | grep squashfs` ] || exit 1
+[ -s `lsmod | grep vfat` ] || exit 1
