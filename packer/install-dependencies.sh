b27553f18e76ac71f5d2a86943d811d047d56c6b
Javier Darsie <javier.darsie@microsoft.com>
2019-04-18 22:56:36 -0700

    cleanUpContainerImages

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -372,16 +372,14 @@ K8S_VERSIONS="
 for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
     if [[ $KUBERNETES_VERSION == *"azs"* ]]; then
       HYPERKUBE_URL="msazurestackdocker/hyperkube-amd64:v${KUBERNETES_VERSION}"
-      extractHyperkube "docker"
-      echo "  - ${HYPERKUBE_URL}" >> ${RELEASE_NOTES_FILEPATH}
     else
       HYPERKUBE_URL="k8s.gcr.io/hyperkube-amd64:v${KUBERNETES_VERSION}"
-      extractHyperkube "docker"
       CONTAINER_IMAGE="k8s.gcr.io/cloud-controller-manager-amd64:v${KUBERNETES_VERSION}"
       pullContainerImage "docker" ${CONTAINER_IMAGE}
-      echo "  - ${HYPERKUBE_URL}" >> ${RELEASE_NOTES_FILEPATH}
       echo "  - ${CONTAINER_IMAGE}" >> ${RELEASE_NOTES_FILEPATH}
     fi
+    extractHyperkube "docker"
+    echo "  - ${HYPERKUBE_URL}" >> ${RELEASE_NOTES_FILEPATH}
 done
 
 df -h
