1d3cf04419ab2ffafe8f81d037fa0ba28dbf9c92
Jack Francis <jack.francis@microsoft.com>
2019-04-23 10:29:10 -0700

    test: shell out in test to ensure we pick up /etc/profile.d changes

--- test/e2e/kubernetes/scripts/cloud-init-files-validate.sh
+++ test/e2e/kubernetes/scripts/cloud-init-files-validate.sh
@@ -23,6 +23,6 @@ sudo grep "^\s*linux" /boot/grub/grub.cfg | grep 'audit=1' || exit 1
 # validate su configuration
 sudo grep "auth required pam_wheel.so use_uid" /etc/pam.d/su
 # validate umask configuration
-touch test-umask
+sh -c "touch test-umask"
 stat test-umask | grep 'Access: (0640' || exit 1
 rm -f test-umask
