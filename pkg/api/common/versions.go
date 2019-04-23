f86b2f120a30106d7b60578a8575d756b696e2b5
Matt Boersma <Matt.Boersma@microsoft.com>
2019-04-23 12:40:04 -0600

    feat: add support for Kubernetes v1.15.0-alpha.1
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.15.md#v1150-alpha1

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -117,6 +117,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.14.0-rc.1":    false,
 	"1.14.0":         true,
 	"1.14.1":         true,
+	"1.15.0-alpha.1": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
