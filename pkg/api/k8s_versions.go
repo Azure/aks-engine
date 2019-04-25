76bb47b64f1cc76b40e1d466228a41e5e1580d4f
Jack Francis <jack.francis@microsoft.com>
2019-04-25 11:35:10 -0700

    chore: rev coredns to 1.2.6 for k8s 1.12

--- pkg/api/k8s_versions.go
+++ pkg/api/k8s_versions.go
@@ -86,7 +86,7 @@ var k8sComponentVersions = map[string]map[string]string{
 		"exechealthz":                      "exechealthz-amd64:1.2",
 		"heapster":                         "heapster-amd64:v1.5.4",
 		"metrics-server":                   "metrics-server-amd64:v0.2.1",
-		"coredns":                          "coredns:1.2.2",
+		"coredns":                          "coredns:1.2.6",
 		"kube-dns":                         "k8s-dns-kube-dns-amd64:1.15.0",
 		"addon-manager":                    "kube-addon-manager-amd64:v8.7",
 		"dnsmasq":                          "k8s-dns-dnsmasq-nanny-amd64:1.15.0",
