7ebc685057ccf24fcbff6a835a4775ea193c2e93
Cecile Robert-Michon <cerobert@microsoft.com>
2019-05-02 13:08:38 -0700

    chore: update VHD version to 04.30.2019 (#1184)
    * chore: update VHD version to 04.20.2019
    
    * remove ubuntu distro E2E gates for CIS
    
    * Revert "remove ubuntu distro E2E gates for CIS"
    
    This reverts commit 7d583aef269e893e9671b8aaac19ac3dc7beb961.
    
    * remove ubuntu distro E2E gates for CIS with rebase
    
    * re-add gate for cloud-init files
    
    * test: only validate password enforcement for ubuntu

--- test/e2e/kubernetes/kubernetes_test.go
+++ test/e2e/kubernetes/kubernetes_test.go
@@ -345,22 +345,22 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 		})
 
 		It("should validate password enforcement configuration", func() {
-			if eng.ExpandedDefinition.Properties.IsUbuntuDistroForAllNodes() {
-				kubeConfig, err := GetConfig()
-				Expect(err).NotTo(HaveOccurred())
-				master := fmt.Sprintf("%s@%s", eng.ExpandedDefinition.Properties.LinuxProfile.AdminUsername, kubeConfig.GetServerName())
-				nodeList, err := node.GetReady()
-				Expect(err).NotTo(HaveOccurred())
-				pwQualityValidateScript := "pwquality-validate.sh"
-				cmd := exec.Command("scp", "-i", masterSSHPrivateKeyFilepath, "-P", masterSSHPort, "-o", "StrictHostKeyChecking=no", filepath.Join(ScriptsDir, pwQualityValidateScript), master+":/tmp/"+pwQualityValidateScript)
-				util.PrintCommand(cmd)
-				out, err := cmd.CombinedOutput()
-				log.Printf("%s\n", out)
-				Expect(err).NotTo(HaveOccurred())
-				var conn *remote.Connection
-				conn, err = remote.NewConnection(kubeConfig.GetServerName(), masterSSHPort, eng.ExpandedDefinition.Properties.LinuxProfile.AdminUsername, masterSSHPrivateKeyFilepath)
-				Expect(err).NotTo(HaveOccurred())
-				for _, node := range nodeList.Nodes {
+			kubeConfig, err := GetConfig()
+			Expect(err).NotTo(HaveOccurred())
+			master := fmt.Sprintf("%s@%s", eng.ExpandedDefinition.Properties.LinuxProfile.AdminUsername, kubeConfig.GetServerName())
+			nodeList, err := node.GetReady()
+			Expect(err).NotTo(HaveOccurred())
+			pwQualityValidateScript := "pwquality-validate.sh"
+			cmd := exec.Command("scp", "-i", masterSSHPrivateKeyFilepath, "-P", masterSSHPort, "-o", "StrictHostKeyChecking=no", filepath.Join(ScriptsDir, pwQualityValidateScript), master+":/tmp/"+pwQualityValidateScript)
+			util.PrintCommand(cmd)
+			out, err := cmd.CombinedOutput()
+			log.Printf("%s\n", out)
+			Expect(err).NotTo(HaveOccurred())
+			var conn *remote.Connection
+			conn, err = remote.NewConnection(kubeConfig.GetServerName(), masterSSHPort, eng.ExpandedDefinition.Properties.LinuxProfile.AdminUsername, masterSSHPrivateKeyFilepath)
+			Expect(err).NotTo(HaveOccurred())
+			for _, node := range nodeList.Nodes {
+				if node.IsUbuntu() {
 					err := conn.CopyToRemote(node.Metadata.Name, "/tmp/"+pwQualityValidateScript)
 					Expect(err).NotTo(HaveOccurred())
 					pwQualityValidationCommand := fmt.Sprintf("\"/tmp/%s\"", pwQualityValidateScript)
@@ -369,8 +369,6 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 					_, err = cmd.CombinedOutput()
 					Expect(err).NotTo(HaveOccurred())
 				}
-			} else {
-				Skip("pwquality only works on ubuntu distro until this lands in a VHD")
 			}
 		})
 
