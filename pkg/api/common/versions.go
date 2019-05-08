5f9bc25a04173c03fb788990335ea0dc6b497d0a
Matt Boersma <Matt.Boersma@microsoft.com>
2019-05-07 22:44:39 -0600

    feat: add support for Kubernetes 1.15.0-alpha.3 (#1247)
    * feat: add support for Kubernetes 1.15.0-alpha.3
    
    See https://github.com/kubernetes/kubernetes/releases/tag/v1.15.0-alpha.3
    
    * chore: bump addon-manager to v9.0.1
    
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.15.md#changelog-since-v1150-alpha2
    
    * test: add missing unit test for k8s 1.15 component versions
    
    * chore: add addon-manager v9.0.1 to VHD script

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -120,6 +120,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.14.1":         true,
 	"1.15.0-alpha.1": true,
 	"1.15.0-alpha.2": true,
+	"1.15.0-alpha.3": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
