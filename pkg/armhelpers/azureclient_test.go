e88f868219e53c64caef24c30f1bfca466a51e3e
Matt Boersma <Matt.Boersma@microsoft.com>
2019-05-10 11:36:20 -0600

    revert: unneeded unit test change

--- pkg/armhelpers/azureclient_test.go
+++ pkg/armhelpers/azureclient_test.go
@@ -21,33 +21,20 @@ func TestAzureClient(t *testing.T) {
 	RunSpecsWithReporters(t, "AzureClient Tests", "Server Suite")
 }
 
-var _ = Describe("AzureClient", func() {
-	Context("in public cloud", func() {
-		It("should set auxiliary token", func() {
-			env, err := azure.EnvironmentFromName("AZUREPUBLICCLOUD")
-			Expect(err).To(BeNil())
+var _ = Describe("AzureClient Aux token tests", func() {
+	It("Should set Aux token", func() {
+		env, err := azure.EnvironmentFromName("AZUREPUBLICCLOUD")
+		Expect(err).To(BeNil())
 
-			token := "eyJ0eXAiOiJKV1QiL"
-			azureClient, err := NewAzureClientWithClientSecretExternalTenant(env, "subID", "d1a3-4ea4", "clientID", "secret")
-			Expect(err).To(BeNil())
-			azureClient.AddAuxiliaryTokens([]string{token})
-			request, err := azureClient.deploymentsClient.GetPreparer(context.Background(), "testRG", "testDeployment")
-			Expect(err).To(BeNil())
-			Expect(request).To(Not(BeNil()))
-			request, err = autorest.Prepare(request, azureClient.deploymentsClient.WithInspection())
-			Expect(err).To(BeNil())
-			Expect(request.Header.Get("x-ms-authorization-auxiliary")).To(Equal(fmt.Sprintf("Bearer %s", token)))
-		})
-		Context("with mock client", func() {
-			It("should find the platform fault domain count for a cluster", func() {
-				azureClient := MockAKSEngineClient{}
-				vmas, err := azureClient.GetAvailabilitySet(nil, "resourceGroup", "vmasname")
-				Expect(err).NotTo(HaveOccurred())
-				Expect(vmas).NotTo(BeNil())
-				count, err := azureClient.GetAvailabilitySetFaultDomainCount(nil, "resourceGroup", []string{"ID1", "ID2"})
-				Expect(err).NotTo(HaveOccurred())
-				Expect(count).To(Equal(3))
-			})
-		})
+		token := "eyJ0eXAiOiJKV1QiL"
+		azureClient, _ := NewAzureClientWithClientSecretExternalTenant(env, "subID", "d1a3-4ea4", "clientID", "secret")
+		Expect(err).To(BeNil())
+		azureClient.AddAuxiliaryTokens([]string{token})
+		request, err := azureClient.deploymentsClient.GetPreparer(context.Background(), "testRG", "testDeployment")
+		Expect(err).To(BeNil())
+		Expect(request).To(Not(BeNil()))
+		request, err = autorest.Prepare(request, azureClient.deploymentsClient.WithInspection())
+		Expect(err).To(BeNil())
+		Expect(request.Header.Get("x-ms-authorization-auxiliary")).To(Equal(fmt.Sprintf("Bearer %s", token)))
 	})
 })
