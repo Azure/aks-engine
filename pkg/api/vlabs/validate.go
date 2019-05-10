3a5985cd0affac42ca7fea5d6d643fd4981b01ce
vyta <vvyy.ta@outlook.com>
2019-05-10 12:40:40 -0700

    fix: add validation for non-support of prometheus extension for Windows (#1259)
    * fix: short term fix for non-support of prometheus extension for Windows
    
    * adding unit test to validation
    
    * Updating test
    
    * Fix test and refactor
    
    * fixing lint errors
    
    * fix lint

--- pkg/api/vlabs/validate.go
+++ pkg/api/vlabs/validate.go
@@ -596,6 +596,14 @@ func (a *Properties) validateExtensions() error {
 		if len(agentPool.Extensions) != 0 && (len(agentPool.AvailabilityProfile) == 0 || agentPool.IsVirtualMachineScaleSets()) {
 			return errors.Errorf("Extensions are currently not supported with VirtualMachineScaleSets. Please specify \"availabilityProfile\": \"%s\"", AvailabilitySet)
 		}
+
+		if agentPool.OSType == Windows && len(agentPool.Extensions) != 0 {
+			for _, e := range agentPool.Extensions {
+				if e.Name == "prometheus-grafana-k8s" {
+					return errors.Errorf("prometheus-grafana-k8s extension is currently not supported for Windows agents")
+				}
+			}
+		}
 	}
 
 	for _, extension := range a.ExtensionProfiles {
