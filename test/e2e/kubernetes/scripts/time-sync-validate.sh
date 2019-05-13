e9e4ecbfd64d2ec96eb039032d521b00fd374e67
Cecile Robert-Michon <cerobert@microsoft.com>
2019-05-13 10:27:46 -0700

    add retries to timesync test

--- test/e2e/kubernetes/scripts/time-sync-validate.sh
+++ test/e2e/kubernetes/scripts/time-sync-validate.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+retrycmd_if_failure() {
+    retries=$1; wait_sleep=$2; timeout=$3; shift && shift && shift
+    for i in $(seq 1 $retries); do
+        timeout $timeout ${@}
+        [ $? -eq 0  ] && break || \
+        if [ $i -eq $retries ]; then
+            echo Executed \"$@\" $i times;
+            return 1
+        else
+            sleep $wait_sleep
+        fi
+    done
+    echo Executed \"$@\" $i times;
+}
+
 RELEASE=$(grep ^DISTRIB_RELEASE= /etc/lsb-release | tr -d 'DISTRIB_RELEASE="' | awk '{print toupper($0)}')
 # verify that timesyncd configuration is healthy
 set -x
@@ -12,5 +27,6 @@ elif [[ $RELEASE == "18.04" ]]; then
 
 fi
 sudo timedatectl status | grep 'RTC in local TZ: no' || exit 1
-sudo systemctl status systemd-timesyncd | grep 'Active: active' || exit 1
-sudo systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || exit 1
+retrycmd_if_failure 60 10 5 systemctl status systemd-timesyncd | grep 'Active: active' || exit 1
+retrycmd_if_failure 120 10 5 systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || exit 1
+
