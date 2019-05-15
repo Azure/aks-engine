7197fb2c789f085a33e52cd4d15680abcb42d5c2
Jeremy Wood <36464809+JeremyWx@users.noreply.github.com>
2019-05-15 12:41:05 -0400

    Update Setup-gMSA.ps1

--- extensions/gmsa-dc/v1/Setup-gMSA.ps1
+++ extensions/gmsa-dc/v1/Setup-gMSA.ps1
@@ -1,22 +1,25 @@
 ﻿##################################################################################
 # This script will automate the creation of a Group Managed Service Account,
-# install the account locally, and produce the JSON file with gMSA info.
+# install the account locally, produce the JSON file with gMSA info, and convert
+# it to the Yaml file needed.
 #
 # This is only for automated e2e testing.  DO NOT use this for production.
 # Jeremy Wood (JeremyWx)
-# Version: 2019041400
+# Version: 1.0.0.0
 ##################################################################################
 
 # Change to working directory
 Set-Location -Path C:\gmsa
 
-# Uncomment line below for troubleshooting
+# Script Logging - Feel free to comment out if you like.
 Start-Transcript -Path "C:\gmsa\Setup-gmsa.txt" -Append
+# Making sure the AD services are up and running
 Start-Sleep -Seconds 60
 Import-Module ActiveDirectory
 
 if (Get-AdGroupMember -Identity "Enterprise Admins" | Select-String -Pattern "gmsa-admin" -Quiet) {
-
+    
+	# Add the KDS Root Key
     $KdsRootKey = Get-KdsRootKey
     if ($null -eq $KdsRootKey.KeyId) {
         Add-KdsRootKey –EffectiveTime ((get-date).addhours(-10))
@@ -37,6 +40,7 @@ if (Get-AdGroupMember -Identity "Enterprise Admins" | Select-String -Pattern "gm
     C:\gmsa\GenerateCredentialSpecResource.ps1 gmsa-e2e gmsa-e2e
 
 } else {
+    # Add new admin account to needed groups
     Add-ADGroupMember -Identity "Enterprise Admins" -Members gmsa-admin
     Add-ADGroupMember -Identity "Domain Admins" -Members gmsa-admin
     $RunOnce = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
