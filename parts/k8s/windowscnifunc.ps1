c2c400fd488d0fc0958c8417e8f4fe9db39e8884
Patrick Lang <plang@microsoft.com>
2019-05-16 16:33:42 -0700

    feat: Experimental support for Windows+ContainerD

--- parts/k8s/windowscnifunc.ps1
+++ parts/k8s/windowscnifunc.ps1
@@ -22,4 +22,19 @@ function Update-WinCNI
     DownloadFileOverHttp -Url $WinCniUrl -DestinationPath $wincniFile
 }
 
+function Install-SdnBridge
+{
+    Param(
+        [Parameter(Mandatory=$true)][string]
+        $Url,
+        [Parameter(Mandatory=$true)][string]
+        $CNIPath
+    )
+
+    $cnizip = [Io.path]::Combine($CNIPath, "cni.zip")
+    DownloadFileOverHttp -Url $Url -DestinationPath $cnizip
+    Expand-Archive -path $cnizip -DestinationPath $CNIPath
+    del $cnizip
+}
+
 # TODO: Move the code that creates the wincni configuration file out of windowskubeletfunc.ps1 and put it here
