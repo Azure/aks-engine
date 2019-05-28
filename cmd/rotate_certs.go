77fe023bab34c93564cd8e2258ed1e633d8cbddf
Jack Francis <jack.francis@microsoft.com>
2019-05-28 10:22:30 -0700

    chore: master-FQDN needs to be present to be deprecated

--- cmd/rotate_certs.go
+++ cmd/rotate_certs.go
@@ -74,6 +74,7 @@ func newRotateCertsCmd() *cobra.Command {
 	f.StringVarP(&rcc.resourceGroupName, "resource-group", "g", "", "the resource group where the cluster is deployed (required)")
 	f.StringVarP(&rcc.apiModelPath, "api-model", "m", "", "path to the generated apimodel.json file (required)")
 	f.StringVarP(&rcc.sshFilepath, "ssh", "", "", "the filepath of a valid private ssh key to access the cluster's nodes (required)")
+	f.StringVar(&rcc.masterFQDN, "master-FQDN", "", "FQDN for the master load balancer")
 	f.StringVar(&rcc.masterFQDN, "apiserver", "", "apiserver endpoint (required)")
 	f.StringVarP(&rcc.outputDirectory, "output-directory", "o", "", "output directory where generated TLS artifacts will be saved (derived from DNS prefix if absent)")
 
