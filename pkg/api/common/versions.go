091c61053e5c5bb657405ecc2cc2b0943fb182ce
Matt Boersma <Matt.Boersma@microsoft.com>
2019-06-05 08:58:31 -0600

    feat: add support for Kubernetes 1.12.10
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md#changelog-since-v1129

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -105,6 +105,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.12.7":         true,
 	"1.12.8":         true,
 	"1.12.9":         false, // disabled because of https://github.com/Azure/aks-engine/issues/1421
+	"1.12.10":        true,
 	"1.13.0-alpha.1": false,
 	"1.13.0-alpha.2": false,
 	"1.13.1":         false,
