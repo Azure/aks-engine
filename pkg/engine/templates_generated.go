80289d1e9d2fa41c1e3a8d4ed45d41fdd6074d51
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-06-05 00:15:23 -0700

    update script after tests

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -11818,14 +11818,14 @@ set -u
 DHCLIENT6_CONF_FILE=/etc/dhcp/dhclient6.conf
 CLOUD_INIT_CFG=/etc/network/interfaces.d/50-cloud-init.cfg
 
-read -r -d '' NETWORK_CONFIGURATION << EOC 
+read -r -d '' NETWORK_CONFIGURATION << EOC || true
 iface eth0 inet6 auto 
     up sleep 5 
     up dhclient -1 -6 -cf /etc/dhcp/dhclient6.conf -lf /var/lib/dhcp/dhclient6.eth0.leases -v eth0 || true
 EOC
 
 add_if_not_exists() {
-    grep -qxF "${1}" ${2} || echo "${1}" >> ${2}
+    grep -qxF "${1}" "${2}" || echo "${1}" >> "${2}"
 }
 
 echo "Configuring dhcpv6 ..."
