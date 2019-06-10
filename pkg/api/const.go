149b40693d091762defdf0e9ec42fbcced422e25
Jack Francis <jackfrancis@gmail.com>
2019-06-10 12:44:12 -0700

    chore: update etcd default to 3.2.26 (#1451)

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -385,7 +385,7 @@ const (
 	//DefaultKubernetesGCLowThreshold specifies the value for the image-gc-low-threshold kubelet flag
 	DefaultKubernetesGCLowThreshold = 80
 	// DefaultEtcdVersion specifies the default etcd version to install
-	DefaultEtcdVersion = "3.2.25"
+	DefaultEtcdVersion = "3.2.26"
 	// DefaultEtcdDiskSize specifies the default size for Kubernetes master etcd disk volumes in GB
 	DefaultEtcdDiskSize = "256"
 	// DefaultEtcdDiskSizeGT3Nodes = size for Kubernetes master etcd disk volumes in GB if > 3 nodes
