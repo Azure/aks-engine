d0bbdf72a268728dac0fafe42477ac87391b76a6
Jack Francis <jack.francis@microsoft.com>
2019-06-12 17:46:09 -0700

    test: re-enable parallelism

--- pkg/api/defaults_test.go
+++ pkg/api/defaults_test.go
@@ -335,7 +335,7 @@ func TestAssignDefaultImages(t *testing.T) {
 	for _, c := range cases {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
-			//t.Parallel()
+			t.Parallel()
 			mockCS := getMockBaseContainerService("1.14.1")
 			mockCS.Properties.OrchestratorProfile.OrchestratorType = Kubernetes
 			if c.myKubernetesImagesConfig != nil {
