628c048bb46c737db517bc0aa3c1a0e779051cd9
Matt Boersma <Matt.Boersma@microsoft.com>
2019-06-17 13:47:50 -0600

    chore: remove support for Kubernetes 1.9.x (#1486)

--- pkg/api/convertertoagentpoolonlyapi_test.go
+++ pkg/api/convertertoagentpoolonlyapi_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestConvertV20180331AgentPoolOnlyOrchestratorProfile(t *testing.T) {
-	kubernetesVersion := "1.9.11"
+	kubernetesVersion := "1.10.13"
 	networkPlugin := v20180331.Azure
 	networkPolicy := v20180331.NetworkPolicyCalico
 	networkPluginKubenet := v20180331.Kubenet
