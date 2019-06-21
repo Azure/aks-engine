23494b8cf5d1e72c08187fbfd41101b14f983474
Matt Boersma <Matt.Boersma@microsoft.com>
2019-06-21 13:32:30 -0600

    chore: remove redundant import

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -26,8 +26,6 @@ import (
 	"github.com/Azure/aks-engine/pkg/api/common"
 	"github.com/Azure/aks-engine/pkg/helpers"
 	"github.com/pkg/errors"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth/azure" // register azure (AD) authentication plugin
 )
 
 var commonTemplateFiles = []string{agentOutputs, agentParams, masterOutputs, iaasOutputs, masterParams, windowsParams}
