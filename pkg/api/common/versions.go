ce245393e5214fc1dafb115736d8fa8117c364ae
Patrick Lang <patrick.lang@hotmail.com>
2019-07-16 22:03:17 -0700

    feat: add support for Kubernetes 1.16.0-alpha.1

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -133,6 +133,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.15.0-rc.1":    false,
 	"1.15.0":         true,
 	"1.15.1":         true,
+	"1.16.0-alpha.1": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
