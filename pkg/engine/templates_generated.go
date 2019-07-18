07f8090c052fbabcbeafdbf5f7bb135330067966
Mohammed Abu Taleb <mohatb@gmail.com>
2019-07-18 14:29:22 +0300

    fix: fix restart command in setup-custom-search-domains.sh fix: fix sourcing kubelet.sh which is required to update the variables searchDomainRealmPassword, searchDomainRealmUser, and searchDomainName

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -14684,11 +14684,27 @@ set -x
 source /opt/azure/containers/provision_source.sh
 
 echo "  dns-search <searchDomainName>" | tee -a /etc/network/interfaces.d/50-cloud-init.cfg
-systemctl_restart 20 5 10 restart networking
+systemctl_restart 20 5 10 networking
 wait_for_apt_locks
 retrycmd_if_failure 10 5 120 apt-get -y install realmd sssd sssd-tools samba-common samba samba-common python2.7 samba-libs packagekit
 wait_for_apt_locks
-echo "<searchDomainRealmPassword>" | realm join -U <searchDomainRealmUser>@$(echo "<searchDomainName>" | tr /a-z/ /A-Z/) $(echo "<searchDomainName>" | tr /a-z/ /A-Z/)
+
+function updatevars1() {
+  source /opt/azure/containers/kubelet.sh
+}
+
+function updatevars2() {
+  echo "<searchDomainRealmPassword>" | realm join -U <searchDomainRealmUser>@$(echo "<searchDomainName>" | tr /a-z/ /A-Z/) $(echo "<searchDomainName>" | tr /a-z/ /A-Z/)
+}
+
+function updatevars3() {
+  cat /opt/azure/containers/setup-custom-search-domains.sh | grep 'realm join' > /opt/azure/containers/realmjoin.sh
+  chmod +x /opt/azure/containers/realmjoin.sh
+  bash -x /opt/azure/containers/realmjoin.sh
+}
+
+updatevars1
+updatevars3
 `)
 
 func k8sCloudInitArtifactsSetupCustomSearchDomainsShBytes() ([]byte, error) {
