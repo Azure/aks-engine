130d18b38dbf5e0d89e608a004de418989e78394
Matt Boersma <Matt.Boersma@microsoft.com>
2019-07-18 09:21:40 -0600

    refactor: simplify label-nodes.sh script

--- parts/k8s/cloud-init/artifacts/label-nodes.sh
+++ parts/k8s/cloud-init/artifacts/label-nodes.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-# Scans master and agent nodes and applies missing labels.
-# Kubernetes 1.16+ rejects the kubernetes.io/role and node-role.kubernetes.io
-# labels in the "--node-labels" argument, but they need to be present for
-# backward compatibility with Azure clusters.
+# Applies missing master and agent labels to Kubernetes nodes.
+#
+# Kubelet 1.16+ rejects the `kubernetes.io/role` and `node-role.kubernetes.io`
+# labels in its `--node-labels` argument, but they need to be present for
+# backward compatibility.
 
 set -euo pipefail
 
-MASTER_LABELS="kubernetes.azure.com/role=master kubernetes.io/role=master node-role.kubernetes.io/master="
-AGENT_LABELS="kubernetes.azure.com/role=agent kubernetes.io/role=agent node-role.kubernetes.io/agent="
 MASTER_SELECTOR="kubernetes.azure.com/role!=agent,kubernetes.io/role!=agent"
+MASTER_LABELS="kubernetes.azure.com/role=master kubernetes.io/role=master node-role.kubernetes.io/master="
 AGENT_SELECTOR="kubernetes.azure.com/role!=master,kubernetes.io/role!=master"
+AGENT_LABELS="kubernetes.azure.com/role=agent kubernetes.io/role=agent node-role.kubernetes.io/agent="
 
-# Find master nodes and label them
-for node in $(kubectl get nodes -l $MASTER_SELECTOR -o name); do
-  kubectl label --overwrite $node $MASTER_LABELS || echo "Error labeling master nodes"
-done
-
-# Find agent nodes and label them
-for node in $(kubectl get nodes -l $AGENT_SELECTOR -o name); do
-  kubectl label --overwrite $node $AGENT_LABELS || echo "Error labeling agent nodes"
-done
+kubectl label nodes --overwrite -l $MASTER_SELECTOR $MASTER_LABELS
+kubectl label nodes --overwrite -l $AGENT_SELECTOR $AGENT_LABELS
