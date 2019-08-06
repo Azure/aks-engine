c1ed8019c566c295a56a3fcc35d42df0f9ea4059
Matt Boersma <Matt.Boersma@microsoft.com>
2019-08-06 13:42:21 -0600

    feat: add support for Kubernetes 1.15.2 (#1730)
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.15.md#v1152

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -131,8 +131,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.15.0-beta.1":  false,
 	"1.15.0-beta.2":  false,
 	"1.15.0-rc.1":    false,
-	"1.15.0":         true,
+	"1.15.0":         false,
 	"1.15.1":         true,
+	"1.15.2":         true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
