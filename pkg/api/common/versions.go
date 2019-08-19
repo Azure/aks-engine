c327465f8ae37f2e45446993871e52a395262f92
Matt Boersma <Matt.Boersma@microsoft.com>
2019-08-19 14:54:22 -0600

    feat: add support for Kubernetes 1.15.3 (#1805)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -136,8 +136,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.15.0-beta.2":  false,
 	"1.15.0-rc.1":    false,
 	"1.15.0":         false,
-	"1.15.1":         true,
+	"1.15.1":         false,
 	"1.15.2":         true,
+	"1.15.3":         true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
