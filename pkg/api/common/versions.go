2e0c58fd94830469ccfd68b96080b00fd152e7e8
Matt Boersma <Matt.Boersma@microsoft.com>
2019-08-19 14:12:21 -0600

    feat: add support for Kubernetes 1.13.10 (#1803)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -114,8 +114,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.13.5":         true,  // TODO: disable when Azure Stack supports 1.13.8
 	"1.13.6":         false, // disabled because of https://github.com/kubernetes/kubernetes/issues/78308
 	"1.13.7":         false,
-	"1.13.8":         true,
+	"1.13.8":         false,
 	"1.13.9":         true,
+	"1.13.10":        true,
 	"1.14.0-alpha.1": false,
 	"1.14.0-alpha.2": false,
 	"1.14.0-beta.1":  false,
