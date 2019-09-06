851df07118b0bedafd779e4a92362a99175d5430
Andy Liu <andyliuliming@outlook.com>
2019-09-06 22:04:18 +0800

    fix

--- pkg/helpers/pki_test.go
+++ pkg/helpers/pki_test.go
@@ -20,7 +20,7 @@ func TestCreateCertificateWithOrganisation(t *testing.T) {
 		caPrivateKey    *rsa.PrivateKey
 		testCertificate *x509.Certificate
 	)
-	certOption := certOptions{
+	certParam := certParams{
 		commonName:    "ca",
 		caCertificate: nil,
 		caPrivateKey:  nil,
@@ -31,7 +31,7 @@ func TestCreateCertificateWithOrganisation(t *testing.T) {
 		organization:  nil,
 		keySize:       DefaultPkiKeySize,
 	}
-	caCertificate, caPrivateKey, err = createCertificate(certOption)
+	caCertificate, caPrivateKey, err = createCertificate(certParam)
 	if err != nil {
 		t.Fatalf("failed to generate certificate: %s", err)
 	}
@@ -48,7 +48,7 @@ func TestCreateCertificateWithOrganisation(t *testing.T) {
 
 	organization := make([]string, 1)
 	organization[0] = "system:masters"
-	certOption = certOptions{
+	certParam = certParams{
 		commonName:    "client",
 		caCertificate: caCertificate,
 		caPrivateKey:  caPrivateKey,
@@ -59,7 +59,7 @@ func TestCreateCertificateWithOrganisation(t *testing.T) {
 		organization:  organization,
 		keySize:       DefaultPkiKeySize,
 	}
-	testCertificate, _, err = createCertificate(certOption)
+	testCertificate, _, err = createCertificate(certParam)
 	if err != nil {
 		t.Fatalf("failed to generate certificate: %s", err)
 	}
@@ -80,7 +80,7 @@ func TestCreateCertificateWithoutOrganisation(t *testing.T) {
 		caPrivateKey    *rsa.PrivateKey
 		testCertificate *x509.Certificate
 	)
-	certOption := certOptions{
+	certParam := certParams{
 		commonName:    "ca",
 		caCertificate: nil,
 		caPrivateKey:  nil,
@@ -91,7 +91,7 @@ func TestCreateCertificateWithoutOrganisation(t *testing.T) {
 		organization:  nil,
 		keySize:       DefaultPkiKeySize,
 	}
-	caCertificate, caPrivateKey, err = createCertificate(certOption)
+	caCertificate, caPrivateKey, err = createCertificate(certParam)
 	if err != nil {
 		t.Fatalf("failed to generate certificate: %s", err)
 	}
@@ -105,7 +105,7 @@ func TestCreateCertificateWithoutOrganisation(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to generate certificate: %s", err)
 	}
-	certOption = certOptions{
+	certParam = certParams{
 		commonName:    "client",
 		caCertificate: caCertificate,
 		caPrivateKey:  caPrivateKey,
@@ -116,7 +116,7 @@ func TestCreateCertificateWithoutOrganisation(t *testing.T) {
 		organization:  nil,
 		keySize:       DefaultPkiKeySize,
 	}
-	testCertificate, _, err = createCertificate(certOption)
+	testCertificate, _, err = createCertificate(certParam)
 	if err != nil {
 		t.Fatalf("failed to generate certificate: %s", err)
 	}
@@ -131,7 +131,7 @@ func TestCreateCertificateWithoutOrganisation(t *testing.T) {
 func TestSubjectAltNameInCert(t *testing.T) {
 	extraIPs := []net.IP{net.ParseIP("10.255.255.5"), net.ParseIP("10.255.255.15")}
 	roots := x509.NewCertPool()
-	certOption := certOptions{
+	certParam := certParams{
 		commonName:    "ca",
 		caCertificate: nil,
 		caPrivateKey:  nil,
@@ -143,7 +143,7 @@ func TestSubjectAltNameInCert(t *testing.T) {
 		keySize:       DefaultPkiKeySize,
 	}
 	// Prepare CA and add it to certificate store.
-	caCertificate, caPrivateKey, err := createCertificate(certOption)
+	caCertificate, caPrivateKey, err := createCertificate(certParam)
 	if err != nil {
 		t.Fatalf("failed to generate CA certificates: %s.", err)
 	}
@@ -210,7 +210,7 @@ func TestSubjectAltNameInCert(t *testing.T) {
 	masterExtraFQDNs := append(formattedDNSPrefixes, SubjectAltNames...)
 	expectedDNSNames := []string{"santest.australiaeast.cloudapp.azure.com", "santest.australiasoutheast.cloudapp.azure.com", "santest.brazilsouth.cloudapp.azure.com", "santest.canadacentral.cloudapp.azure.com", "santest.canadaeast.cloudapp.azure.com", "santest.centralindia.cloudapp.azure.com", "santest.centralus.cloudapp.azure.com", "santest.centraluseuap.cloudapp.azure.com", "santest.chinaeast.cloudapp.chinacloudapi.cn", "santest.chinaeast2.cloudapp.chinacloudapi.cn", "santest.chinanorth.cloudapp.chinacloudapi.cn", "santest.chinanorth2.cloudapp.chinacloudapi.cn", "santest.eastasia.cloudapp.azure.com", "santest.eastus.cloudapp.azure.com", "santest.eastus2.cloudapp.azure.com", "santest.eastus2euap.cloudapp.azure.com", "santest.japaneast.cloudapp.azure.com", "santest.japanwest.cloudapp.azure.com", "santest.koreacentral.cloudapp.azure.com", "santest.koreasouth.cloudapp.azure.com", "santest.northcentralus.cloudapp.azure.com", "santest.northeurope.cloudapp.azure.com", "santest.southcentralus.cloudapp.azure.com", "santest.southeastasia.cloudapp.azure.com", "santest.southindia.cloudapp.azure.com", "santest.uksouth.cloudapp.azure.com", "santest.ukwest.cloudapp.azure.com", "santest.westcentralus.cloudapp.azure.com", "santest.westeurope.cloudapp.azure.com", "santest.westindia.cloudapp.azure.com", "santest.westus.cloudapp.azure.com", "santest.westus2.cloudapp.azure.com", "santest.chinaeast.cloudapp.chinacloudapi.cn", "santest.chinanorth.cloudapp.chinacloudapi.cn", "santest.germanycentral.cloudapp.microsoftazure.de", "santest.germanynortheast.cloudapp.microsoftazure.de", "santest.usgovvirginia.cloudapp.usgovcloudapi.net", "santest.usgoviowa.cloudapp.usgovcloudapi.net", "santest.usgovarizona.cloudapp.usgovcloudapi.net", "santest.usgovtexas.cloudapp.usgovcloudapi.net", "santest.francecentral.cloudapp.azure.com", "santest.mydomain.com", "santest2.testdomain.net", "kubernetes", "kubernetes.default", "kubernetes.default.svc", "kubernetes.default.svc.cluster.local", "kubernetes.kube-system", "kubernetes.kube-system.svc", "kubernetes.kube-system.svc.cluster.local"}
 
-	pkiOptions := PkiOptions{
+	pkiParams := PkiParams{
 		ExtraFQDNs:    masterExtraFQDNs,
 		ExtraIPs:      extraIPs,
 		ClusterDomain: "cluster.local",
@@ -218,7 +218,7 @@ func TestSubjectAltNameInCert(t *testing.T) {
 		MasterCount:   1,
 		PkiKeySize:    DefaultPkiKeySize,
 	}
-	apiServerPair, _, _, _, _, _, err := CreatePki(pkiOptions)
+	apiServerPair, _, _, _, _, _, err := CreatePki(pkiParams)
 
 	if err != nil {
 		t.Fatalf("failed to generate certificates: %s.", err)
@@ -254,7 +254,7 @@ func TestSubjectAltNameInCert(t *testing.T) {
 	SubjectAltNames = []string{}
 	masterExtraFQDNs = append(formattedDNSPrefixes, SubjectAltNames...)
 	expectedDNSNames = []string{"santest.australiaeast.cloudapp.azure.com", "santest.australiasoutheast.cloudapp.azure.com", "santest.brazilsouth.cloudapp.azure.com", "santest.canadacentral.cloudapp.azure.com", "santest.canadaeast.cloudapp.azure.com", "santest.centralindia.cloudapp.azure.com", "santest.centralus.cloudapp.azure.com", "santest.centraluseuap.cloudapp.azure.com", "santest.chinaeast.cloudapp.chinacloudapi.cn", "santest.chinaeast2.cloudapp.chinacloudapi.cn", "santest.chinanorth.cloudapp.chinacloudapi.cn", "santest.chinanorth2.cloudapp.chinacloudapi.cn", "santest.eastasia.cloudapp.azure.com", "santest.eastus.cloudapp.azure.com", "santest.eastus2.cloudapp.azure.com", "santest.eastus2euap.cloudapp.azure.com", "santest.japaneast.cloudapp.azure.com", "santest.japanwest.cloudapp.azure.com", "santest.koreacentral.cloudapp.azure.com", "santest.koreasouth.cloudapp.azure.com", "santest.northcentralus.cloudapp.azure.com", "santest.northeurope.cloudapp.azure.com", "santest.southcentralus.cloudapp.azure.com", "santest.southeastasia.cloudapp.azure.com", "santest.southindia.cloudapp.azure.com", "santest.uksouth.cloudapp.azure.com", "santest.ukwest.cloudapp.azure.com", "santest.westcentralus.cloudapp.azure.com", "santest.westeurope.cloudapp.azure.com", "santest.westindia.cloudapp.azure.com", "santest.westus.cloudapp.azure.com", "santest.westus2.cloudapp.azure.com", "santest.chinaeast.cloudapp.chinacloudapi.cn", "santest.chinanorth.cloudapp.chinacloudapi.cn", "santest.germanycentral.cloudapp.microsoftazure.de", "santest.germanynortheast.cloudapp.microsoftazure.de", "santest.usgovvirginia.cloudapp.usgovcloudapi.net", "santest.usgoviowa.cloudapp.usgovcloudapi.net", "santest.usgovarizona.cloudapp.usgovcloudapi.net", "santest.usgovtexas.cloudapp.usgovcloudapi.net", "santest.francecentral.cloudapp.azure.com", "kubernetes", "kubernetes.default", "kubernetes.default.svc", "kubernetes.default.svc.cluster.local", "kubernetes.kube-system", "kubernetes.kube-system.svc", "kubernetes.kube-system.svc.cluster.local"}
-	pkiOptions = PkiOptions{
+	pkiParams = PkiParams{
 		ExtraFQDNs:    masterExtraFQDNs,
 		ExtraIPs:      extraIPs,
 		ClusterDomain: "cluster.local",
@@ -262,7 +262,7 @@ func TestSubjectAltNameInCert(t *testing.T) {
 		MasterCount:   1,
 		PkiKeySize:    DefaultPkiKeySize,
 	}
-	apiServerPair, _, _, _, _, _, err = CreatePki(pkiOptions)
+	apiServerPair, _, _, _, _, _, err = CreatePki(pkiParams)
 
 	if err != nil {
 		t.Fatalf("failed to generate certificates: %s.", err)
