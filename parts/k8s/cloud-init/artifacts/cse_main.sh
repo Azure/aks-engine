0937c1acfcd51d109d63141ba3399c9c523b2aab
Jack Francis <jack.francis@microsoft.com>
2019-09-16 10:32:57 -0700

    fix: actually validate VHD fingerprint

--- parts/k8s/cloud-init/artifacts/cse_main.sh
+++ parts/k8s/cloud-init/artifacts/cse_main.sh
@@ -53,11 +53,10 @@ fi
 
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 if [[ "${IS_VHD}" = true ]]; then
-    # TODO (09/13): uncomment when new VHD is published
-    # if [ -f $VHD_LOGS_FILEPATH ]; then
-    #     echo "Using VHD distro but file $VHD_LOGS_FILEPATH not found"
-    #     exit $ERR_VHD_FILE_NOT_FOUND
-    # fi
+    if [ ! -f $VHD_LOGS_FILEPATH ]; then
+        echo "Using VHD distro but file $VHD_LOGS_FILEPATH not found"
+        exit $ERR_VHD_FILE_NOT_FOUND
+    fi
     echo "detected golden image pre-install"
     FULL_INSTALL_REQUIRED=false
 else
