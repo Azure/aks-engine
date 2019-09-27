d9abdfc7c02d369ec986bc6dc35f1c27632cb0cc
Mark Rossetti <marosset@microsoft.com>
2019-09-27 10:20:26 -0700

    fix: verify LCOW in makedev.ps1 (#2037)

--- makedev.ps1
+++ makedev.ps1
@@ -2,4 +2,12 @@ $REPO_PATH = "github.com/Azure/aks-engine"
 $DEV_ENV_IMAGE = "quay.io/deis/go-dev:v1.23.3"
 $DEV_ENV_WORK_DIR = "/go/src/$REPO_PATH"
 
+# Ensure docker is configured for linux containers
+$json = (docker version --format "{{json .}}" | ConvertFrom-Json)
+if ($json.Server.Os -ne "linux")
+{
+    Write-Error "Please switch Docker use to Linux containers on Windows"
+    exit 1
+}
+
 docker.exe run -it --rm -w $DEV_ENV_WORK_DIR -v `"$($PWD)`":$DEV_ENV_WORK_DIR $DEV_ENV_IMAGE bash
