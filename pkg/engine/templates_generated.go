53d74988749cedd52833bcf95dbfec4632f8e7eb
deaborch <44680211+deaborch@users.noreply.github.com>
2019-09-30 17:13:38 -0700

    fix: fixes cluster preprovisioning script download on azurestack. (#2053)

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -15785,6 +15785,12 @@ coreos:
 runcmd:
 - set -x
 - . /opt/azure/containers/provision_source.sh
+{{if IsAzureStackCloud}}
+- AZURESTACK_ROOT_CERTIFICATE_SOURCE_PATH="/var/lib/waagent/Certificates.pem"
+- AZURESTACK_ROOT_CERTIFICATE__DEST_PATH="/usr/local/share/ca-certificates/azsCertificate.crt"
+- cp $AZURESTACK_ROOT_CERTIFICATE_SOURCE_PATH $AZURESTACK_ROOT_CERTIFICATE__DEST_PATH
+- update-ca-certificates
+{{end}}
 - aptmarkWALinuxAgent hold{{GetKubernetesMasterPreprovisionYaml}}
 {{end}}
 `)
@@ -16141,6 +16147,12 @@ coreos:
 runcmd:
 - set -x
 - . /opt/azure/containers/provision_source.sh
+{{if IsAzureStackCloud}}
+- AZURESTACK_ROOT_CERTIFICATE_SOURCE_PATH="/var/lib/waagent/Certificates.pem"
+- AZURESTACK_ROOT_CERTIFICATE__DEST_PATH="/usr/local/share/ca-certificates/azsCertificate.crt"
+- cp $AZURESTACK_ROOT_CERTIFICATE_SOURCE_PATH $AZURESTACK_ROOT_CERTIFICATE__DEST_PATH
+- update-ca-certificates
+{{end}}
 - aptmarkWALinuxAgent hold{{GetKubernetesAgentPreprovisionYaml .}}
 {{end}}
 `)
