42016063a0531097f438f8b334614bfc0a357c79
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-10-02 16:58:26 -0700

    Update docs for dual-stack phase 2

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -439,7 +439,7 @@ const (
 	// DefaultKubernetesClusterSubnet specifies the default subnet for pods.
 	DefaultKubernetesClusterSubnet = "10.244.0.0/16"
 	// DefaultKubernetesClusterSubnetIPv6 specifies the IPv6 default subnet for pods.
-	DefaultKubernetesClusterSubnetIPv6 = "fd00:101::/8"
+	DefaultKubernetesClusterSubnetIPv6 = "fc00::/8"
 	// DefaultKubernetesServiceCIDR specifies the IP subnet that kubernetes will create Service IPs within.
 	DefaultKubernetesServiceCIDR = "10.0.0.0/16"
 	// DefaultKubernetesDNSServiceIP specifies the IP address that kube-dns listens on by default. must by in the default Service CIDR range.
