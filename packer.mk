a8e2a3f3671bcd437a67046aa2a208ef2316d8d1
Jack Francis <jack.francis@microsoft.com>
2019-10-07 12:47:37 -0700

    test: enable "custom image from VHD" pipeline

--- packer.mk
+++ packer.mk
@@ -4,6 +4,9 @@ build-packer:
 build-packer-windows:
 	@packer build -var-file=packer/settings.json packer/windows-vhd-builder.json
 
+build-packer-custom-image-from-vhd:
+	@packer build -var-file=packer/settings.json packer/custom-image-from-vhd.json
+
 init-packer:
 	@./packer/init-variables.sh
 
@@ -15,6 +18,9 @@ run-packer: az-login
 
 run-packer-windows: az-login
 	@packer version && set -o pipefail && ($(MAKE) init-packer | tee packer-output) && ($(MAKE) build-packer-windows | tee -a packer-output)
+
+run-packer-custom-image-from-vhd: az-login
+	@packer version && set -o pipefail && ($(MAKE) init-packer | tee packer-output) && ($(MAKE) build-packer-custom-image-from-vhd | tee -a packer-output)
 
 az-copy: az-login
 	azcopy-preview copy "${OS_DISK_SAS}" "${CLASSIC_BLOB}${CLASSIC_SAS_TOKEN}"
