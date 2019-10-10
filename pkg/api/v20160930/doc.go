0ed0d1d14264d74e632dd56469095888f11e3209
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-10-10 14:02:21 -0700

    chore: delete rp apis now that they are moved over

--- pkg/api/v20160930/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-// Package v20160930 stores the API model for version "2016-09-30."
-// Deprecated: Use the vlabs package instead. This code remains only to
-// support the legacy ACS service.
-package v20160930
--- pkg/api/v20170131/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-// Package v20170131 stores the API model for version "2017-01-31."
-// Deprecated: Use the vlabs package instead. This code remains only to
-// support the legacy ACS service.
-package v20170131
