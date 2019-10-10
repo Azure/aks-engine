766a9da89b03a7d7aff39de69434ce30978e9a93
Matt Boersma <Matt.Boersma@microsoft.com>
2019-10-10 15:01:38 -0600

    feat: add support for Kubernetes v1.16.2
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.16.md#changelog-since-v1161

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -148,8 +148,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.16.0-beta.1":  false,
 	"1.16.0-beta.2":  false,
 	"1.16.0-rc.1":    false,
-	"1.16.0":         true,
+	"1.16.0":         false,
 	"1.16.1":         true,
+	"1.16.2":         true,
 	"1.17.0-alpha.1": true,
 }
 
