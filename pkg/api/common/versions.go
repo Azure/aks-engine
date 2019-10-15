99dfac6161c47aa43480ada372a84862bbfcdc2d
Matt Boersma <Matt.Boersma@microsoft.com>
2019-10-15 08:52:51 -0600

    feat: add support for Kubernetes v1.17.0-alpha.2
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.17.md#changelog-since-v1170-alpha1

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -156,6 +156,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.16.1":         true,
 	"1.16.2":         true,
 	"1.17.0-alpha.1": true,
+	"1.17.0-alpha.2": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
