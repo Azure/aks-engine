0b4f5948f9e63bac1f4f4343da7918e630b7b399
marosset <marosset@ntdev.microsoft.com>
2019-10-15 14:44:34 -0700

    Add 1.14.8, 1.15.5, and 1.16.2 k8s packages to windows VHD

--- packer/configure-windows-vhd.ps1
+++ packer/configure-windows-vhd.ps1
@@ -67,10 +67,13 @@ function Get-FilesToCacheOnVHD
             "https://acs-mirror.azureedge.net/wink8s/azs-v1.16.1-1int.zip",
             "https://acs-mirror.azureedge.net/wink8s/v1.14.6-1int.zip",
             "https://acs-mirror.azureedge.net/wink8s/v1.14.7-1int.zip",
+            "https://acs-mirror.azureedge.net/wink8s/v1.14.8-1int.zip",
             "https://acs-mirror.azureedge.net/wink8s/v1.15.3-1int.zip",
             "https://acs-mirror.azureedge.net/wink8s/v1.15.4-1int.zip",
+            "https://acs-mirror.azureedge.net/wink8s/v1.15.5-1int.zip",
             "https://acs-mirror.azureedge.net/wink8s/v1.16.0-1int.zip",
-            "https://acs-mirror.azureedge.net/wink8s/v1.16.1-1int.zip"
+            "https://acs-mirror.azureedge.net/wink8s/v1.16.1-1int.zip",
+            "https://acs-mirror.azureedge.net/wink8s/v1.16.2-1int.zip"
         );
         "c:\akse-cache\win-vnet-cni\" = @(
             "https://acs-mirror.azureedge.net/cni/azure-vnet-cni-windows-amd64-v1.0.27.zip"
