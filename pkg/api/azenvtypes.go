26e07fa3a320fedbb138ab89f4f82d405af4c984
Jack Francis <jack.francis@microsoft.com>
2019-10-16 12:57:47 -0700

    chore: rev VHD images to 2019.10.15

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -154,7 +154,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-201910",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2019.10.09",
+		ImageVersion:   "2019.10.15",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
@@ -162,7 +162,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-201910",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2019.10.09",
+		ImageVersion:   "2019.10.15",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
