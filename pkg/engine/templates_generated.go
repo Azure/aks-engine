b532e963dcb81f263665e455b9460afa9133750c
Jack Francis <jack.francis@microsoft.com>
2019-10-22 08:44:48 -0700

    chore: generated code

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -14916,7 +14916,7 @@ retrycmd_get_executable() {
 wait_for_file() {
     retries=$1; wait_sleep=$2; filepath=$3
     paved=/opt/azure/cloud-init-files.paved
-    grep -Fq '${filepath}' $paved && return 0
+    grep -Fq "${filepath}" $paved && return 0
     for i in $(seq 1 $retries); do
         grep -Fq '#EOF' $filepath && break
         if [ $i -eq $retries ]; then
