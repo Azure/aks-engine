7d5ee68e8d708f6a20cf9bc9bb6c59caf8f36132
Jack Francis <jackfrancis@gmail.com>
2019-10-22 13:30:25 -0700

    fix: run all CSE apt-get purge operations in background (#2196)

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -15600,7 +15600,7 @@ ps auxfww > /opt/azure/provision-ps.log &
 
 if [[ "${TARGET_ENVIRONMENT,,}" != "${AZURE_STACK_ENV}"  ]]; then
     # TODO: remove once ACR is available on Azure Stack
-    apt_get_purge 20 30 120 apache2-utils || exit $ERR_APT_PURGE_FAIL
+    apt_get_purge 20 30 120 apache2-utils &
 fi
 
 if $REBOOTREQUIRED; then
