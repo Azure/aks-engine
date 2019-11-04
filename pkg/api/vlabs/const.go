86547ee369268f692180b85eb07594b15f5985b6
guwe <guwe@microsoft.com>
2019-11-04 06:57:06 +0000

    unit test

--- pkg/api/vlabs/const.go
+++ pkg/api/vlabs/const.go
@@ -103,6 +103,9 @@ var (
 
 	// DependenciesLocationValues holds the valid values for dependencies location
 	DependenciesLocationValues = []DependenciesLocation{"", AzureStackDependenciesLocationPublic, AzureStackDependenciesLocationChina, AzureStackDependenciesLocationGerman, AzureStackDependenciesLocationUSGovernment}
+
+	// NetworkModeValues holds the valid values for network mode implementation for cni
+	NetworkModeValues = [...]string{"", NetworkModeBridge, NetworkModeTransparent}
 )
 
 // Kubernetes configuration
@@ -123,6 +126,10 @@ const (
 	NetworkPolicyCilium = "cilium"
 	// NetworkPluginCilium is the string expression for cilium network policy config option
 	NetworkPluginCilium = NetworkPolicyCilium
+	// NetworkModeBridge is the string expression for bridge network mode config option
+	NetworkModeBridge = "bridge"
+	// NetworkModeTransparent is the string expression for transparent network mode config option
+	NetworkModeTransparent = "transparent"
 )
 
 const (
