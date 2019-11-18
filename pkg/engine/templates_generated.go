35de6273609744f11d1920cc477b899d8e8bac7e
Xiaofang Zhang <xizha@microsoft.com>
2019-11-18 10:12:22 -0800

    fix: userAssignedIdentityId in windows azure.json missing quotes (#2327)

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -35071,7 +35071,7 @@ Write-AzureConfig {
     "primaryAvailabilitySetName": "$PrimaryAvailabilitySetName",
     "primaryScaleSetName": "$PrimaryScaleSetName",
     "useManagedIdentityExtension": $UseManagedIdentityExtension,
-    "userAssignedIdentityID": $UserAssignedClientID,
+    "userAssignedIdentityID": "$UserAssignedClientID",
     "useInstanceMetadata": $UseInstanceMetadata,
     "loadBalancerSku": "$LoadBalancerSku",
     "excludeMasterFromStandardLB": $ExcludeMasterFromStandardLB
