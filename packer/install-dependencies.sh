510a6f40403e7ef687212c3ab41b012cbc4a6e2a
Matt Boersma <Matt.Boersma@microsoft.com>
2019-11-19 11:13:37 -0700

    chore: add CSI components to VHD (#2332)
    * chore: add CSI components to VHD
    
    * fix: update azuredisk-csi image location
    
    * fix: add azurefile-csi:v0.3.0
    
    * fix: use correct livenessprobe image name

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -460,6 +460,69 @@ for CLOUD_MANAGER_VERSION in ${CLOUD_MANAGER_VERSIONS}; do
   done
 done
 
+AZUREDISK_CSI_VERSIONS="
+0.4.0
+"
+for AZUREDISK_CSI_VERSION in ${AZUREDISK_CSI_VERSIONS}; do
+  CONTAINER_IMAGE="mcr.microsoft.com/k8s/csi/azuredisk-csi:v${AZUREDISK_CSI_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+AZUREFILE_CSI_VERSIONS="
+0.3.0
+"
+for AZUREFILE_CSI_VERSION in ${AZUREFILE_CSI_VERSIONS}; do
+  CONTAINER_IMAGE="mcr.microsoft.com/k8s/csi/azurefile-csi:v${AZUREFILE_CSI_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+CSI_ATTACHER_VERSIONS="
+1.0.1
+"
+for CSI_ATTACHER_VERSION in ${CSI_ATTACHER_VERSIONS}; do
+  CONTAINER_IMAGE="quay.io/k8scsi/csi-attacher:v${CSI_ATTACHER_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+CSI_CLUSTER_DRIVER_REGISTRAR_VERSIONS="
+1.0.1
+"
+for CSI_CLUSTER_DRIVER_REGISTRAR_VERSION in ${CSI_CLUSTER_DRIVER_REGISTRAR_VERSIONS}; do
+  CONTAINER_IMAGE="quay.io/k8scsi/csi-cluster-driver-registrar:v${CSI_CLUSTER_DRIVER_REGISTRAR_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+CSI_NODE_DRIVER_REGISTRAR_VERSIONS="
+1.1.0
+"
+for CSI_NODE_DRIVER_REGISTRAR_VERSION in ${CSI_NODE_DRIVER_REGISTRAR_VERSIONS}; do
+  CONTAINER_IMAGE="quay.io/k8scsi/csi-node-driver-registrar:v${CSI_NODE_DRIVER_REGISTRAR_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+CSI_PROVISIONER_VERSIONS="
+1.0.1
+"
+for CSI_PROVISIONER_VERSION in ${CSI_PROVISIONER_VERSIONS}; do
+  CONTAINER_IMAGE="quay.io/k8scsi/csi-provisioner:v${CSI_PROVISIONER_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
+LIVENESSPROBE_VERSIONS="
+1.1.0
+"
+for LIVENESSPROBE_VERSION in ${LIVENESSPROBE_VERSIONS}; do
+  CONTAINER_IMAGE="quay.io/k8scsi/livenessprobe:v${LIVENESSPROBE_VERSION}"
+  pullContainerImage "docker" ${CONTAINER_IMAGE}
+  echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
+done
+
 # TODO: remove once ACR is available on Azure Stack
 CONTAINER_IMAGE="registry:2.7.1"
 pullContainerImage "docker" ${CONTAINER_IMAGE}
