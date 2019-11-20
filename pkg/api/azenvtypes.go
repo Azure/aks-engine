d9ecf3e4007c799ddbd734ce2af1f4e845ac39be
Jack Francis <jackfrancis@gmail.com>
2019-11-19 16:13:09 -0800

    chore: rev VHD images to 2019.11.18 (#2340)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -152,17 +152,17 @@ var (
 	// AKSUbuntu1604OSImageConfig is the AKS image based on Ubuntu 16.04-LTS.
 	AKSUbuntu1604OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1604-201910",
+		ImageSku:       "aks-ubuntu-1604-201911",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2019.10.24",
+		ImageVersion:   "2019.11.18",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
 	AKSUbuntu1804OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1804-201910",
+		ImageSku:       "aks-ubuntu-1804-201911",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2019.10.24",
+		ImageVersion:   "2019.11.18",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
