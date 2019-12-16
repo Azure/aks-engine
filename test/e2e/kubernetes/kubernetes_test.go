76212b1c6941fd0ca9d2c78ad558678a7a54b01f
Matt Boersma <Matt.Boersma@microsoft.com>
2019-12-16 13:59:10 -0700

    fix: apply new master node labels for k8s v1.18+ compatibility

--- test/e2e/kubernetes/kubernetes_test.go
+++ test/e2e/kubernetes/kubernetes_test.go
@@ -684,6 +684,10 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				labels := node.Metadata.Labels
 				Expect(labels).To(HaveKeyWithValue("kubernetes.io/role", role))
 				Expect(labels).To(HaveKey(fmt.Sprintf("node-role.kubernetes.io/%s", role)))
+				if role == "master" {
+					Expect(labels).To(HaveKeyWithValue("node.kubernetes.io/exclude-from-external-load-balancers", "true"))
+					Expect(labels).To(HaveKeyWithValue("node.kubernetes.io/exclude-disruption", "true"))
+				}
 			}
 		})
 
