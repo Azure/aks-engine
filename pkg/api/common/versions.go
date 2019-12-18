979e6fb2b430e20d9822b930d848f4c6b8a5f2a2
Matt Boersma <Matt.Boersma@microsoft.com>
2019-12-17 17:55:18 -0700

    feat: add support for Kubernetes 1.15.8
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.15.md#changelog-since-v1157

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -145,9 +145,10 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.15.1":         false,
 	"1.15.2":         false,
 	"1.15.3":         false,
-	"1.15.4":         true,
+	"1.15.4":         false,
 	"1.15.5":         false, // disabled because of https://github.com/Azure/aks-engine/issues/2312
 	"1.15.7":         true,
+	"1.15.8":         true,
 	"1.16.0-alpha.1": false,
 	"1.16.0-alpha.2": false,
 	"1.16.0-alpha.3": false,
