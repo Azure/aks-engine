666053e0b8407ce5f8603a3036e269d47ddfbd7d
Mark Rossetti <marosset@microsoft.com>
2019-12-19 08:49:52 -0800

    fix: fix ARM dependency issues with vm user-specified extensions on node pools (#2398)
    * fix: fix ARM dependency issues if many extensions are specified for a node profile

--- pkg/engine/vmextensions_test.go
+++ pkg/engine/vmextensions_test.go
@@ -481,6 +481,11 @@ func TestCreateCustomExtensions(t *testing.T) {
 				Version: "v1",
 				RootURL: "https://raw.githubusercontent.com/Azure/aks-engine/master/",
 			},
+			{
+				Name:    "hello-world-k8s",
+				Version: "v1",
+				RootURL: "https://raw.githubusercontent.com/Azure/aks-engine/master/",
+			},
 		},
 		AgentPoolProfiles: []*api.AgentPoolProfile{
 			{
@@ -501,6 +506,9 @@ func TestCreateCustomExtensions(t *testing.T) {
 					{
 						Name: "winrm",
 					},
+					{
+						Name: "hello-world-k8s",
+					},
 				},
 			},
 		},
@@ -516,7 +524,7 @@ func TestCreateCustomExtensions(t *testing.T) {
 					"count": "[sub(variables('windowspool1Count'), variables('windowspool1Offset'))]",
 					"name":  "winrmExtensionLoop",
 				},
-				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('windowspool1VMNamePrefix'), copyIndex(variables('windowspool1Offset')), '/extensions/cse', '-agent-', copyIndex(variables('windowspool1Offset')))]"},
+				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('windowspool1VMNamePrefix'), copyIndex(variables('windowspool1Offset')), '/extensions/cse-agent-', copyIndex(variables('windowspool1Offset')))]"},
 			},
 			DeploymentExtended: resources.DeploymentExtended{
 				Name: to.StringPtr("[concat(variables('windowspool1VMNamePrefix'), copyIndex(variables('windowspool1Offset')), 'winrm')]"),
@@ -545,7 +553,7 @@ func TestCreateCustomExtensions(t *testing.T) {
 					"count": "[sub(variables('windowspool2Count'), variables('windowspool2Offset'))]",
 					"name":  "winrmExtensionLoop",
 				},
-				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')), '/extensions/cse', '-agent-', copyIndex(variables('windowspool2Offset')))]"},
+				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')), '/extensions/cse-agent-', copyIndex(variables('windowspool2Offset')))]"},
 			},
 			DeploymentExtended: resources.DeploymentExtended{
 				Name: to.StringPtr("[concat(variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')), 'winrm')]"),
@@ -567,6 +575,35 @@ func TestCreateCustomExtensions(t *testing.T) {
 				Type: to.StringPtr("Microsoft.Resources/deployments"),
 			},
 		},
+		{
+			DeploymentARMResource: DeploymentARMResource{
+				APIVersion: "[variables('apiVersionDeployments')]",
+				Copy: map[string]string{
+					"count": "[sub(variables('windowspool2Count'), variables('windowspool2Offset'))]",
+					"name":  "helloWorldExtensionLoop",
+				},
+				DependsOn: []string{"[concat(variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')), 'winrm')]"},
+			},
+			DeploymentExtended: resources.DeploymentExtended{
+				Name: to.StringPtr("[concat(variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')), 'HelloWorldK8s')]"),
+				Properties: &resources.DeploymentPropertiesExtended{
+					TemplateLink: &resources.TemplateLink{
+						URI:            to.StringPtr("https://raw.githubusercontent.com/Azure/aks-engine/master/extensions/hello-world-k8s/v1/template.json"),
+						ContentVersion: to.StringPtr("1.0.0.0"),
+					},
+					Parameters: map[string]interface{}{
+						"apiVersionDeployments": map[string]interface{}{"value": "[variables('apiVersionDeployments')]"},
+						"artifactsLocation":     map[string]interface{}{"value": "https://raw.githubusercontent.com/Azure/aks-engine/master/"},
+						"extensionParameters":   map[string]interface{}{"value": "[parameters('hello-world-k8sParameters')]"},
+						"targetVMName":          map[string]interface{}{"value": "[concat(variables('windowspool2VMNamePrefix'), copyIndex(variables('windowspool2Offset')))]"},
+						"targetVMType":          map[string]interface{}{"value": "agent"},
+						"vmIndex":               map[string]interface{}{"value": "[copyIndex(variables('windowspool2Offset'))]"},
+					},
+					Mode: resources.DeploymentMode("Incremental"),
+				},
+				Type: to.StringPtr("Microsoft.Resources/deployments"),
+			},
+		},
 	}
 
 	diff := cmp.Diff(extensions, expectedDeployments)
@@ -574,6 +611,7 @@ func TestCreateCustomExtensions(t *testing.T) {
 	if diff != "" {
 		t.Errorf("unexpected diff while expecting equal structs: %s", diff)
 	}
+
 	properties = &api.Properties{
 		OrchestratorProfile: &api.OrchestratorProfile{
 			OrchestratorType: Kubernetes,
@@ -607,7 +645,7 @@ func TestCreateCustomExtensions(t *testing.T) {
 					"count": "[sub(variables('masterCount'), variables('masterOffset'))]",
 					"name":  "helloWorldExtensionLoop",
 				},
-				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('masterVMNamePrefix'), copyIndex(variables('masterOffset')), '/extensions/cse', '-master-', copyIndex(variables('masterOffset')))]"},
+				DependsOn: []string{"[concat('Microsoft.Compute/virtualMachines/', variables('masterVMNamePrefix'), copyIndex(variables('masterOffset')), '/extensions/cse-master-', copyIndex(variables('masterOffset')))]"},
 			},
 			DeploymentExtended: resources.DeploymentExtended{
 				Name: to.StringPtr("[concat(variables('masterVMNamePrefix'), copyIndex(variables('masterOffset')), 'HelloWorldK8s')]"),
