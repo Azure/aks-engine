d035f252da2fd746e12722ab899d87d0ceb99283
Jack Francis <jack.francis@microsoft.com>
2020-01-16 16:07:32 -0800

    chore: lint

--- examples/addpool/addpool.sh
+++ examples/addpool/addpool.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -x 
+set -x
 
 group=$1
 subscription=$2
@@ -9,20 +9,22 @@ az group create --name $group --location $3 --subscription $2
 
 # Create role
 az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscription/resourceGroups/$group" >~/.kube/$group-sp.json
+# shellcheck disable=SC2002,SC2155
 export appId=$(cat ~/.kube/$group-sp.json | jq -r .appId)
+# shellcheck disable=SC2002,SC2155
 export password=$(cat ~/.kube/$group-sp.json | jq -r .password)
 
 # give the sp time to propagate
 sleep 180
 
 # Deploy Cluster
-aks-engine deploy --subscription-id $subscription --resource-group $group --location $location us --api-model apimodel.json --dns-prefix $group --client-id $appId --client-secret $password --set servicePrincipalProfile.clientId=$appId --set servicePrincipalProfile.secret=$password 
+aks-engine deploy --subscription-id $subscription --resource-group $group --location $location us --api-model apimodel.json --dns-prefix $group --client-id $appId --client-secret $password --set servicePrincipalProfile.clientId=$appId --set servicePrincipalProfile.secret=$password
 
 cp $(pwd)/_output/$group/kubeconfig/kubeconfig.eastus.json ~/.kube/$group.json
 
 sleep 180
 
-aks-engine addpool --subscription-id $subscription --resource-group $group --location $location us --api-model _output/$group/apimodel.json --agent-pool agentpool.json --client-id $appId --client-secret $password 
+aks-engine addpool --subscription-id $subscription --resource-group $group --location $location us --api-model _output/$group/apimodel.json --agent-pool agentpool.json --client-id $appId --client-secret $password
 
 az vmss list -g $group --subscription $subscription -o table
 
