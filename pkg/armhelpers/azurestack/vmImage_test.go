ddf0535f820b18da4a1d2f00801ff8ec240acf12
deaborch <44680211+deaborch@users.noreply.github.com>
2020-01-22 12:01:20 -0800

    Improved code coverage for failure cases

--- pkg/armhelpers/azurestack/vmImage_test.go
+++ pkg/armhelpers/azurestack/vmImage_test.go
@@ -8,6 +8,13 @@ import (
 	"testing"
 )
 
+const (
+	badlocation  = "badlocation"
+	badPublisher = "badPublisher"
+	badOffer     = "badOffer"
+	badSku       = "badSku"
+)
+
 func TestVMImageFetcherInterface(t *testing.T) {
 	mc, err := NewHTTPMockClient()
 	if err != nil {
@@ -36,5 +43,36 @@ func TestVMImageFetcherInterface(t *testing.T) {
 	_, err = azureClient.ListVirtualMachineImages(context.Background(), location, publisher, offer, sku)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestVMImageFetcherInterfaceBadInput(t *testing.T) {
+	mc, err := NewHTTPMockClient()
+	if err != nil {
+		t.Fatalf("failed to create HttpMockClient - %s", err)
+	}
+	mc.RegisterLogin()
+	mc.RegisterVMImageFetcherInterface()
+
+	err = mc.Activate()
+	if err != nil {
+		t.Fatalf("failed to activate HttpMockClient - %s", err)
+	}
+	defer mc.DeactivateAndReset()
+
+	env := mc.GetEnvironment()
+	azureClient, err := NewAzureClientWithClientSecret(env, subscriptionID, "clientID", "secret")
+	if err != nil {
+		t.Fatalf("can not get client %s", err)
+	}
+
+	_, err = azureClient.GetVirtualMachineImage(context.Background(), badlocation, badPublisher, badOffer, badSku, version)
+	if err == nil {
+		t.Fatal("GetVirtualMachineImage did not fail with bad input")
+	}
+
+	_, err = azureClient.ListVirtualMachineImages(context.Background(), badlocation, badPublisher, badOffer, badSku)
+	if err == nil {
+		t.Fatal("ListVirtualMachineImages did not fail with bad input")
 	}
 }
--- pkg/armhelpers/support_validator_test.go
+++ pkg/armhelpers/support_validator_test.go
@@ -55,3 +55,47 @@ func TestValidateRequiredImages(t *testing.T) {
 		t.Fatalf("can not validate required images %s", err)
 	}
 }
+
+func TestValidateRequiredImagesMissingImageCase(t *testing.T) {
+
+	mc, err := NewHTTPMockClient()
+	if err != nil {
+		t.Fatalf("failed to create HttpMockClient - %s", err)
+	}
+
+	mc.RegisterLogin()
+	mc.RegisterVMImageFetcherInterface()
+
+	err = mc.Activate()
+	if err != nil {
+		t.Fatalf("failed to activate HttpMockClient - %s", err)
+	}
+	defer mc.DeactivateAndReset()
+
+	env := mc.GetEnvironment()
+	azureClient, err := NewAzureClientWithClientSecret(env, subscriptionID, "clientID", "secret")
+	if err != nil {
+		t.Fatalf("can not get client %s", err)
+	}
+
+	testProperties := api.Properties{}
+	masterProfile := api.MasterProfile{}
+
+	masterProfile.Distro = api.AKSUbuntu1804
+
+	profile := api.AgentPoolProfile{
+		OSType: api.Linux,
+		Distro: api.AKSUbuntu1804,
+	}
+
+	agentProfiles := []*api.AgentPoolProfile{}
+	agentProfiles = append(agentProfiles, &profile)
+
+	testProperties.AgentPoolProfiles = agentProfiles
+	testProperties.MasterProfile = &masterProfile
+
+	if err := ValidateRequiredImages(context.Background(), location, &testProperties, azureClient); err == nil {
+		t.Fatal("could not fail fast for missing images")
+	}
+
+}
