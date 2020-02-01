432a008ab733ef0e643ea222bde819d2655ab44a
Jack Francis <jack.francis@microsoft.com>
2020-02-01 09:55:26 -0800

    delete remote before adding

--- test/e2e/cluster.sh
+++ test/e2e/cluster.sh
@@ -111,6 +111,7 @@ if [ "${UPGRADE_CLUSTER}" = "true" ] || [ "${SCALE_CLUSTER}" = "true" ] || [ -n 
     done
   fi
   git reset --hard
+  git remote rm $UPGRADE_FORK
   git remote add $UPGRADE_FORK https://github.com/$UPGRADE_FORK/aks-engine.git
   git fetch --prune $UPGRADE_FORK
   git branch -D $UPGRADE_FORK/$UPGRADE_BRANCH
