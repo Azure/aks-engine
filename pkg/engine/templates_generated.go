e7c59ffee382f23bb9a1b789ba214213f7cdef9d
marosset <marosset@microsoft.com>
2020-02-03 21:44:44 +0000

    Testing updates to use pauseloop.exe in windows pause image

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -42822,7 +42822,7 @@ New-InfraContainer {
     # Reference for these tags: curl -L https://mcr.microsoft.com/v2/k8s/core/pause/tags/list
     # Then docker run --rm mplatform/manifest-tool inspect mcr.microsoft.com/k8s/core/pause:<tag>
 
-    $defaultPauseImage = "upstream.azurecr.io/oss/kubernetes/pause:1.3.0-alpha2"
+    $defaultPauseImage = "upstream.azurecr.io/oss/kubernetes/pause:1.3.0-alpha3"
 
     switch ($computerInfo.WindowsVersion) {
         "1803" { 
