da85cda826d54769d959b0506bb23f944d88969e
marosset <marosset@microsoft.com>
2020-02-04 19:10:22 +0000

    upate win pause image

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -42822,7 +42822,7 @@ New-InfraContainer {
     # Reference for these tags: curl -L https://mcr.microsoft.com/v2/k8s/core/pause/tags/list
     # Then docker run --rm mplatform/manifest-tool inspect mcr.microsoft.com/k8s/core/pause:<tag>
 
-    $defaultPauseImage = "upstream.azurecr.io/oss/kubernetes/pause:1.3.0-alpha4"
+    $defaultPauseImage = "mcr.microsoft.com/oss/kubernetes/pause:1.3.0"
 
     switch ($computerInfo.WindowsVersion) {
         "1803" { 
