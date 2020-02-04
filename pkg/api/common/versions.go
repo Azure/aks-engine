98522bec0d9aac2f38c25378f1e2611d91e50502
Matt Boersma <Matt.Boersma@microsoft.com>
2020-02-04 14:13:15 -0700

    feat: add support for Kubernetes 1.18.0-alpha.3
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.18.md#changelog-since-v1180-alpha2

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -177,6 +177,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.17.3":         true,
 	"1.18.0-alpha.1": true,
 	"1.18.0-alpha.2": true,
+	"1.18.0-alpha.3": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
