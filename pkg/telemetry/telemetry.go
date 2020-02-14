451b9d438df600bcdfd7d052613df386f050e042
David Justice <david@devigned.com>
2020-02-13 17:26:44 -0800

    fix: remove requirement for ldflags or make test

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -5,5 +5,5 @@ package telemetry
 
 var (
 	// AKSEngineAppInsightsKey is the Application Insights Key used by AKS Engine when telemetry is enabled
-	AKSEngineAppInsightsKey string
+	AKSEngineAppInsightsKey = "c92d8284-b550-4b06-b7ba-e80fd7178faa" // this will be replaced by ldflags upon release
 )
