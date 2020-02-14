8c65e0ec7e933d021f3e1b2250cd18812506623b
Jack Francis <jack.francis@microsoft.com>
2020-02-14 15:11:27 -0800

    fix: cse and tests

--- test/e2e/kubernetes/scripts/time-sync-validate.sh
+++ test/e2e/kubernetes/scripts/time-sync-validate.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-
-RELEASE=$(grep -oP 'DISTRIB_RELEASE=\K(\S+)' /etc/lsb-release)
-# verify that timesyncd configuration is healthy
 set -x
-if [[ $RELEASE == "16.04" ]]; then
-  sudo timedatectl status | grep 'Network time on: yes' || exit 1
-  sudo timedatectl status | grep 'NTP synchronized: yes' || exit 1
-elif [[ $RELEASE == "18.04" ]]; then
-  sudo timedatectl status | grep 'systemd-timesyncd.service active: yes' || exit 1
-  sudo timedatectl status | grep 'System clock synchronized: yes' || exit 1
-
+OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
+UBUNTU_OS_NAME="UBUNTU"
+RHEL_OS_NAME="RHEL"
+COREOS_OS_NAME="COREOS"
+DEBIAN_OS_NAME="DEBIAN"
+if ! echo "${UBUNTU_OS_NAME} ${RHEL_OS_NAME} ${COREOS_OS_NAME} ${DEBIAN_OS_NAME}" | grep -q "${OS}"; then
+    OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 fi
+
+if [[ $OS == $UBUNTU_OS_NAME ]]; then
+  RELEASE=$(grep -oP 'DISTRIB_RELEASE=\K(\S+)' /etc/lsb-release)
+  # verify that timesyncd configuration is healthy
+  if [[ $RELEASE == "16.04" ]]; then
+    sudo timedatectl status | grep 'Network time on: yes' || exit 1
+    sudo timedatectl status | grep 'NTP synchronized: yes' || exit 1
+  elif [[ $RELEASE == "18.04" ]]; then
+    sudo timedatectl status | grep 'systemd-timesyncd.service active: yes' || exit 1
+    sudo timedatectl status | grep 'System clock synchronized: yes' || exit 1
+  fi
+fi
+if [[ $OS == $DEBIAN_OS_NAME ]]; then
+  sudo timedatectl status | grep 'NTP service: active' || exit 1
+  sudo timedatectl status | grep 'System clock synchronized: yes' || exit 1
+fi
+
 sudo timedatectl status | grep 'RTC in local TZ: no' || exit 1
 sudo systemctl status systemd-timesyncd | grep 'Active: active' || exit 1
 sudo systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || exit 1
--- test/e2e/kubernetes/scripts/ubuntu-installed-packages-validate.sh
+++ test/e2e/kubernetes/scripts/ubuntu-installed-packages-validate.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
-
-OS=$(cat /etc/*-release | grep ^ID= | tr -d 'ID="' | awk '{print toupper($0)}')
+set -x
+OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 UBUNTU_OS_NAME="UBUNTU"
+RHEL_OS_NAME="RHEL"
+COREOS_OS_NAME="COREOS"
+DEBIAN_OS_NAME="DEBIAN"
+if ! echo "${UBUNTU_OS_NAME} ${RHEL_OS_NAME} ${COREOS_OS_NAME} ${DEBIAN_OS_NAME}" | grep -q "${OS}"; then
+    OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
+fi
+
+ENSURE_NOT_INSTALLED="
+postfix
+"
+for PACKAGE in ${ENSURE_NOT_INSTALLED}; do
+    apt list --installed | grep -E "^${PACKAGE}" && exit 1
+done
 
 if [[ $OS == $UBUNTU_OS_NAME ]]; then
-    ENSURE_NOT_INSTALLED="
-    postfix
-    "
-    for PACKAGE in ${ENSURE_NOT_INSTALLED}; do
-        apt list --installed | grep -E "^${PACKAGE}" && exit 1
-    done
-
-    ENSURE_INSTALLED="
-    apt-transport-https
-    blobfuse
-    ca-certificates
-    ceph-common
-    cgroup-lite
-    cifs-utils
-    conntrack
-    ebtables
-    ethtool
-    fuse
-    git
-    glusterfs-client
-    init-system-helpers
-    iproute2
-    ipset
-    iptables
-    jq
-    mount
-    nfs-common
-    pigz
-    socat
-    util-linux
-    xz-utils
-    zip
-    htop
-    iotop
-    iftop
-    sysstat
-    "
-    for PACKAGE in ${ENSURE_INSTALLED}; do
+    ENSURE_INSTALLED_UBUNTU="
+ceph-common
+cgroup-lite
+glusterfs-client
+"
+    for PACKAGE in ${ENSURE_INSTALLED_UBUNTU}; do
         apt list --installed | grep -E "^${PACKAGE}" || exit 1
     done
-else
-    exit 1
 fi
+
+ENSURE_INSTALLED="
+apt-transport-https
+blobfuse
+ca-certificates
+cifs-utils
+conntrack
+dbus
+ebtables
+ethtool
+fuse
+git
+init-system-helpers
+iproute2
+ipset
+iptables
+jq
+mount
+nfs-common
+pigz
+socat
+util-linux
+xz-utils
+zip
+htop
+iotop
+iftop
+sysstat
+"
+for PACKAGE in ${ENSURE_INSTALLED}; do
+    apt list --installed | grep -E "^${PACKAGE}" || exit 1
+done
