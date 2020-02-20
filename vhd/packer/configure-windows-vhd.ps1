ddd195c90c2d4c082567ebe10cf562f462b8c59a
Mark Rossetti <marosset@microsoft.com>
2020-02-20 11:44:05 -0800

    chore: installing Docker EE 19.03.5 by default in Windows VHD (#2751)

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -107,7 +107,7 @@ function Get-FilesToCacheOnVHD
 
 function Install-Docker
 {
-    $defaultDockerVersion = "19.03.2"
+    $defaultDockerVersion = "19.03.5"
 
     Write-Log "Attempting to install Docker version $defaultDockerVersion"
     Install-PackageProvider -Name DockerMsftProvider -Force -ForceBootstrap | Out-null
