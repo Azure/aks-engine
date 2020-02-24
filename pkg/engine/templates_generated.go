9938de15975edc0812f7850490a1e0c179d3d8ad
andyzhangx <xiazhang@microsoft.com>
2020-02-24 05:37:35 +0000

    fix: apiserver broken due to incorrect kms cachesize

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -37877,7 +37877,7 @@ write_files:
         - kms:
             name: azurekmsprovider
             endpoint: unix:///opt/azurekms.socket
-            cachesize: 0
+            cachesize: 1000
         - identity: {}
 {{end}}
 
