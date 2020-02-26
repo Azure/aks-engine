34706cf9871a96b704fb5f4c866c29b89f369875
Matt Boersma <Matt.Boersma@microsoft.com>
2020-02-26 16:38:27 -0700

    feat: add support for Kubernetes 1.18.0-beta.1 (#2791)
    * feat: add support for Kubernetes 1.18.0-beta.1
    
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.18.md/#v1180-beta1
    
    * fix: windows support .zip URL

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -179,6 +179,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.18.0-alpha.2": true,
 	"1.18.0-alpha.3": true,
 	"1.18.0-alpha.5": true,
+	"1.18.0-beta.1":  true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
