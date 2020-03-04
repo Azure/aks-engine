a64743e302fa44483dcef156fcda7c7582004676
Matt Boersma <Matt.Boersma@microsoft.com>
2020-03-04 14:32:13 -0700

    fix: same mistake, again

--- vhd/packer/install-dependencies.sh
+++ vhd/packer/install-dependencies.sh
@@ -90,7 +90,7 @@ CNI_PLUGIN_VERSIONS="
 0.8.5
 "
 for CNI_PLUGIN_VERSION in $CNI_PLUGIN_VERSIONS; do
-    CNI_PLUGINS_URL="https://kubernetesartifacts.azureedge.net/cni-plugins/v${CNI_PLUGIN_VERSION}/binaries/cni-plugins-amd64-linux-v${CNI_PLUGIN_VERSION}.tgz"
+    CNI_PLUGINS_URL="https://kubernetesartifacts.azureedge.net/cni-plugins/v${CNI_PLUGIN_VERSION}/binaries/cni-plugins-linux-amd64-v${CNI_PLUGIN_VERSION}.tgz"
     downloadCNI
     echo "  - CNI plugin version ${CNI_PLUGIN_VERSION}" >> ${VHD_LOGS_FILEPATH}
 done
