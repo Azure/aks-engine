83c3a1b6dd34d00626cc10b51515d29a7cfe9983
Shufang <shchen@microsoft.com>
2020-03-13 17:15:15 -0700

    Create functions in network policy specific file and let tests call it.

--- test/e2e/kubernetes/networkpolicy/networkpolicy.go
+++ test/e2e/kubernetes/networkpolicy/networkpolicy.go
@@ -7,8 +7,15 @@ package networkpolicy
 import (
 	"log"
 	"os/exec"
+	"path/filepath"
+	"time"
 
+	"github.com/Azure/aks-engine/pkg/api"
+	"github.com/Azure/aks-engine/test/e2e/config"
+	"github.com/Azure/aks-engine/test/e2e/kubernetes/deployment"
+	"github.com/Azure/aks-engine/test/e2e/kubernetes/pod"
 	"github.com/Azure/aks-engine/test/e2e/kubernetes/util"
+	. "github.com/onsi/gomega"
 )
 
 // CreateNetworkPolicyFromFile will create a NetworkPolicy from file with a name
@@ -33,4 +40,49 @@ func DeleteNetworkPolicy(name, namespace string) error {
 		return err
 	}
 	return nil
+}
+
+func EnsureRunningPodExists(deploymentName string, namespace string, successesNeeded int, sleepTime time.Duration, timeout time.Duration) {
+	running, err := pod.WaitOnSuccesses(deploymentName, namespace, 4, sleepTime, timeout)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(running).To(Equal(true))
+}
+
+func GetRunningPodsFromDeployment(dep *deployment.Deployment) []pod.Pod {
+	podsRunning, err := dep.PodsRunning()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(len(podsRunning)).ToNot(BeZero())
+	return podsRunning
+}
+
+func EnsureOutboundInternetAccess(pods []pod.Pod, cfg config.Config) {
+	pl := pod.List{Pods: pods}
+	pass, err := pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(pass).To(BeTrue())
+}
+
+func EnsureConnectivityResultBetweenPods(fromPods []pod.Pod, toPods []pod.Pod, cfg config.Config, true bool) {
+	pl := pod.List{Pods: fromPods}
+	for _, toPod := range toPods {
+		pass, err := pl.ValidateCurlConnection(toPod.Status.PodIP, 30*time.Second, cfg.Timeout)
+		if err != nil {
+			e := toPod.Describe()
+			if e != nil {
+				log.Printf("Unable to describe pod %s\n: %s", toPod.Metadata.Name, e)
+			}
+		}
+		Expect(err).NotTo(HaveOccurred())
+		if true {
+			Expect(pass).To(BeTrue())
+		} else {
+			Expect(pass).To(BeFalse())
+		}
+
+	}
+}
+
+func ApplyNetworkPolicy(nwpolicyName string, namespace string, policyName string, policyDir string) {
+	err := CreateNetworkPolicyFromFile(filepath.Join(policyDir, "backend-policy-deny-egress.yaml"), nwpolicyName, namespace)
+	Expect(err).NotTo(HaveOccurred())
 }
