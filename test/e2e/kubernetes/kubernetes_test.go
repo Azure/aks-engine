6dc8522bf995d198cfaf266bb8ade5a69bdb3c94
shchen <shchen@microsoft.com>
2020-03-17 17:18:22 -0700

    test: create functions in network policy specific file and let tests call it. (#2904)

--- test/e2e/kubernetes/kubernetes_test.go
+++ test/e2e/kubernetes/kubernetes_test.go
@@ -1492,10 +1492,10 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				eng.HasNetworkPolicy("cilium") || eng.HasNetworkPolicy("antrea") {
 				nsDev, nsProd := "development", "production"
 				By("Creating development namespace")
-				namespaceDev, err := namespace.CreateIfNotExist(nsDev)
+				namespaceDev, err := namespace.CreateNamespaceDeleteIfExist(nsDev)
 				Expect(err).NotTo(HaveOccurred())
 				By("Creating production namespace")
-				namespaceProd, err := namespace.CreateIfNotExist(nsProd)
+				namespaceProd, err := namespace.CreateNamespaceDeleteIfExist(nsProd)
 				Expect(err).NotTo(HaveOccurred())
 				By("Labelling development namespace")
 				err = namespaceDev.Label("purpose=development")
@@ -1520,199 +1520,83 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				Expect(err).NotTo(HaveOccurred())
 
 				By("Ensure there is a running frontend-prod pod")
-				running, err := pod.WaitOnSuccesses(frontendProdDeploymentName, nsProd, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(running).To(Equal(true))
+				networkpolicy.EnsureRunningPodExists(frontendProdDeploymentName, nsProd, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
 
 				By("Ensure there is a running frontend-dev pod")
-				running, err = pod.WaitOnSuccesses(frontendDevDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(running).To(Equal(true))
+				networkpolicy.EnsureRunningPodExists(frontendDevDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
 
 				By("Ensure there is a running backend pod")
-				running, err = pod.WaitOnSuccesses(backendDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(running).To(Equal(true))
+				networkpolicy.EnsureRunningPodExists(backendDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
 
 				By("Ensure there is a running network-policy pod")
-				running, err = pod.WaitOnSuccesses(nwpolicyDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(running).To(Equal(true))
+				networkpolicy.EnsureRunningPodExists(nwpolicyDeploymentName, nsDev, 4, sleepBetweenRetriesWhenWaitingForPodReady, cfg.Timeout)
 
 				By("Ensuring we have outbound internet access from the frontend-prod pods")
-				frontendProdPods, err := frontendProdDeployment.PodsRunning()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(frontendProdPods)).ToNot(BeZero())
-				pl := pod.List{Pods: frontendProdPods}
-				pass, err := pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(pass).To(BeTrue())
+				frontendProdPods := networkpolicy.GetRunningPodsFromDeployment(frontendProdDeployment)
+				networkpolicy.EnsureOutboundInternetAccess(frontendProdPods, cfg)
 
 				By("Ensuring we have outbound internet access from the frontend-dev pods")
-				frontendDevPods, err := frontendDevDeployment.PodsRunning()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(frontendDevPods)).ToNot(BeZero())
-				pl = pod.List{Pods: frontendDevPods}
-				pass, err = pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(pass).To(BeTrue())
+				frontendDevPods := networkpolicy.GetRunningPodsFromDeployment(frontendDevDeployment)
+				networkpolicy.EnsureOutboundInternetAccess(frontendDevPods, cfg)
 
 				By("Ensuring we have outbound internet access from the backend pods")
-				backendPods, err := backendDeployment.PodsRunning()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(backendPods)).ToNot(BeZero())
-				pl = pod.List{Pods: backendPods}
-				pass, err = pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(pass).To(BeTrue())
+				backendPods := networkpolicy.GetRunningPodsFromDeployment(backendDeployment)
+				networkpolicy.EnsureOutboundInternetAccess(backendPods, cfg)
 
 				By("Ensuring we have outbound internet access from the network-policy pods")
-				nwpolicyPods, err := nwpolicyDeployment.PodsRunning()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(nwpolicyPods)).ToNot(BeZero())
-				pl = pod.List{Pods: nwpolicyPods}
-				pass, err = pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(pass).To(BeTrue())
+				nwpolicyPods := networkpolicy.GetRunningPodsFromDeployment(nwpolicyDeployment)
+				networkpolicy.EnsureOutboundInternetAccess(nwpolicyPods, cfg)
 
 				By("Ensuring we have connectivity from network-policy pods to frontend-prod pods")
-				pl = pod.List{Pods: nwpolicyPods}
-				for _, frontendProdPod := range frontendProdPods {
-					pass, err = pl.ValidateCurlConnection(frontendProdPod.Status.PodIP, 30*time.Second, cfg.Timeout)
-					if err != nil {
-						e := frontendProdPod.Describe()
-						if e != nil {
-							log.Printf("Unable to describe pod %s\n: %s", frontendProdPod.Metadata.Name, e)
-						}
-					}
-					Expect(err).NotTo(HaveOccurred())
-					Expect(pass).To(BeTrue())
-				}
+				networkpolicy.EnsureConnectivityResultBetweenPods(nwpolicyPods, frontendProdPods, cfg, true)
 
 				By("Ensuring we have connectivity from network-policy pods to backend pods")
-				for _, backendPod := range backendPods {
-					pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, cfg.Timeout)
-					if err != nil {
-						e := backendPod.Describe()
-						if e != nil {
-							log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-						}
-					}
-					Expect(err).NotTo(HaveOccurred())
-					Expect(pass).To(BeTrue())
-				}
+				networkpolicy.EnsureConnectivityResultBetweenPods(nwpolicyPods, backendPods, cfg, true)
 
 				By("Applying a network policy to deny ingress access to app: webapp, role: backend pods in development namespace")
-				nwpolicyName, namespace := "backend-deny-ingress", nsDev
-				err = networkpolicy.CreateNetworkPolicyFromFile(filepath.Join(PolicyDir, "backend-policy-deny-ingress.yaml"), nwpolicyName, namespace)
-				Expect(err).NotTo(HaveOccurred())
+				nwpolicyName, namespace, nwpolicyFileName := "backend-deny-ingress", nsDev, "backend-policy-deny-ingress.yaml"
+				networkpolicy.ApplyNetworkPolicy(nwpolicyName, namespace, nwpolicyFileName, PolicyDir)
 
 				By("Ensuring we no longer have ingress access from the network-policy pods to backend pods")
-				for _, backendPod := range backendPods {
-					pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, validateNetworkPolicyTimeout)
-					if err != nil {
-						e := backendPod.Describe()
-						if e != nil {
-							log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-						}
-					}
-					Expect(err).Should(HaveOccurred())
-					Expect(pass).To(BeFalse())
-				}
+				networkpolicy.EnsureConnectivityResultBetweenPods(nwpolicyPods, backendPods, cfg, false)
 
 				By("Cleaning up after ourselves")
 				networkpolicy.DeleteNetworkPolicy(nwpolicyName, namespace)
 
 				By("Applying a network policy to deny egress access in development namespace")
-				nwpolicyName, namespace = "backend-deny-egress", nsDev
-				err = networkpolicy.CreateNetworkPolicyFromFile(filepath.Join(PolicyDir, "backend-policy-deny-egress.yaml"), nwpolicyName, namespace)
-				Expect(err).NotTo(HaveOccurred())
+				nwpolicyName, namespace, nwpolicyFileName = "backend-deny-egress", nsDev, "backend-policy-deny-egress.yaml"
+				networkpolicy.ApplyNetworkPolicy(nwpolicyName, nsDev, nwpolicyFileName, PolicyDir)
 
 				By("Ensuring we no longer have egress access from the network-policy pods to backend pods")
-				for _, backendPod := range backendPods {
-					pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, validateNetworkPolicyTimeout)
-					if err != nil {
-						e := backendPod.Describe()
-						if e != nil {
-							log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-						}
-					}
-					Expect(err).Should(HaveOccurred())
-					Expect(pass).To(BeFalse())
-				}
+				networkpolicy.EnsureConnectivityResultBetweenPods(nwpolicyPods, backendPods, cfg, false)
+				networkpolicy.EnsureConnectivityResultBetweenPods(frontendDevPods, backendPods, cfg, false)
 
 				By("Cleaning up after ourselves")
 				networkpolicy.DeleteNetworkPolicy(nwpolicyName, namespace)
 
 				if common.IsKubernetesVersionGe(eng.ExpandedDefinition.Properties.OrchestratorProfile.OrchestratorVersion, "1.11.0") {
 					By("Applying a network policy to only allow ingress access to app: webapp, role: backend pods in development namespace from pods in any namespace with the same labels")
-					nwpolicyName, namespace = "backend-allow-ingress-pod-label", nsDev
-					err = networkpolicy.CreateNetworkPolicyFromFile(filepath.Join(PolicyDir, "backend-policy-allow-ingress-pod-label.yaml"), nwpolicyName, namespace)
-					Expect(err).NotTo(HaveOccurred())
+					nwpolicyName, namespace, nwpolicyFileName = "backend-allow-ingress-pod-label", nsDev, "backend-policy-allow-ingress-pod-label.yaml"
+					networkpolicy.ApplyNetworkPolicy(nwpolicyName, namespace, nwpolicyFileName, PolicyDir)
 
 					By("Ensuring we have ingress access from pods with matching labels")
-					pl = pod.List{Pods: backendPods}
-					for _, backendDstPod := range backendPods {
-						pass, err = pl.ValidateCurlConnection(backendDstPod.Status.PodIP, 30*time.Second, cfg.Timeout)
-						if err != nil {
-							e := backendDstPod.Describe()
-							if e != nil {
-								log.Printf("Unable to describe pod %s\n: %s", backendDstPod.Metadata.Name, e)
-							}
-						}
-						Expect(err).NotTo(HaveOccurred())
-						Expect(pass).To(BeTrue())
-					}
+					networkpolicy.EnsureConnectivityResultBetweenPods(backendPods, backendPods, cfg, true)
 
 					By("Ensuring we don't have ingress access from pods without matching labels")
-					pl = pod.List{Pods: nwpolicyPods}
-					for _, backendPod := range backendPods {
-						pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, validateNetworkPolicyTimeout)
-						if err != nil {
-							e := backendPod.Describe()
-							if e != nil {
-								log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-							}
-						}
-						Expect(err).Should(HaveOccurred())
-						Expect(pass).To(BeFalse())
-					}
+					networkpolicy.EnsureConnectivityResultBetweenPods(nwpolicyPods, backendPods, cfg, false)
 
 					By("Cleaning up after ourselves")
 					networkpolicy.DeleteNetworkPolicy(nwpolicyName, namespace)
 
 					By("Applying a network policy to only allow ingress access to app: webapp role:backends in development namespace from pods with label app:webapp, role: frontendProd within namespace with label purpose: development")
-					nwpolicyName, namespace = "backend-policy-allow-ingress-pod-namespace-label", nsDev
-					err = networkpolicy.CreateNetworkPolicyFromFile(filepath.Join(PolicyDir, "backend-policy-allow-ingress-pod-namespace-label.yaml"), nwpolicyName, namespace)
-					Expect(err).NotTo(HaveOccurred())
+					nwpolicyName, namespace, nwpolicyFileName = "backend-policy-allow-ingress-pod-namespace-label", nsDev, "backend-policy-allow-ingress-pod-namespace-label.yaml"
+					networkpolicy.ApplyNetworkPolicy(nwpolicyName, namespace, nwpolicyFileName, PolicyDir)
 
 					By("Ensuring we don't have ingress access from role:frontend pods in production namespace")
-					pl = pod.List{Pods: frontendProdPods}
-					for _, backendPod := range backendPods {
-						pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, validateNetworkPolicyTimeout)
-						if err != nil {
-							e := backendPod.Describe()
-							if e != nil {
-								log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-							}
-						}
-						Expect(err).Should(HaveOccurred())
-						Expect(pass).To(BeFalse())
-					}
+					networkpolicy.EnsureConnectivityResultBetweenPods(frontendProdPods, backendPods, cfg, false)
 
 					By("Ensuring we have ingress access from role:frontend pods in development namespace")
-					pl = pod.List{Pods: frontendDevPods}
-					for _, backendPod := range backendPods {
-						pass, err = pl.ValidateCurlConnection(backendPod.Status.PodIP, 30*time.Second, cfg.Timeout)
-						if err != nil {
-							e := backendPod.Describe()
-							if e != nil {
-								log.Printf("Unable to describe pod %s\n: %s", backendPod.Metadata.Name, e)
-							}
-						}
-						Expect(err).NotTo(HaveOccurred())
-						Expect(pass).To(BeTrue())
-					}
+					networkpolicy.EnsureConnectivityResultBetweenPods(frontendDevPods, backendPods, cfg, true)
 
 					By("Cleaning up after ourselves")
 					networkpolicy.DeleteNetworkPolicy(nwpolicyName, namespace)
--- test/e2e/kubernetes/networkpolicy/networkpolicy.go
+++ test/e2e/kubernetes/networkpolicy/networkpolicy.go
@@ -7,8 +7,15 @@ package networkpolicy
 import (
 	"log"
 	"os/exec"
+	"path/filepath"
+	"time"
 
+	"github.com/Azure/aks-engine/pkg/api"
+	"github.com/Azure/aks-engine/test/e2e/config"
+	"github.com/Azure/aks-engine/test/e2e/kubernetes/deployment"
+	"github.com/Azure/aks-engine/test/e2e/kubernetes/pod"
 	"github.com/Azure/aks-engine/test/e2e/kubernetes/util"
+	. "github.com/onsi/gomega"
 )
 
 // CreateNetworkPolicyFromFile will create a NetworkPolicy from file with a name
@@ -24,13 +31,57 @@ func CreateNetworkPolicyFromFile(filename, name, namespace string) error {
 }
 
 // DeleteNetworkPolicy will create a NetworkPolicy from file with a name
-func DeleteNetworkPolicy(name, namespace string) error {
+func DeleteNetworkPolicy(name, namespace string) {
 	cmd := exec.Command("k", "delete", "networkpolicy", "-n", namespace, name)
 	util.PrintCommand(cmd)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Printf("Error while trying to delete NetworkPolicy %s in namespace %s:%s\n", name, namespace, string(out))
-		return err
 	}
-	return nil
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func EnsureRunningPodExists(deploymentName string, namespace string, successesNeeded int, sleepTime time.Duration, timeout time.Duration) {
+	running, err := pod.WaitOnSuccesses(deploymentName, namespace, 4, sleepTime, timeout)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(running).To(Equal(true))
+}
+
+func GetRunningPodsFromDeployment(dep *deployment.Deployment) []pod.Pod {
+	podsRunning, err := dep.PodsRunning()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(len(podsRunning)).ToNot(BeZero())
+	return podsRunning
+}
+
+func EnsureOutboundInternetAccess(pods []pod.Pod, cfg config.Config) {
+	pl := pod.List{Pods: pods}
+	pass, err := pl.CheckOutboundConnection(5*time.Second, cfg.Timeout, api.Linux)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(pass).To(BeTrue())
+}
+
+func EnsureConnectivityResultBetweenPods(fromPods []pod.Pod, toPods []pod.Pod, cfg config.Config, true bool) {
+	pl := pod.List{Pods: fromPods}
+	for _, toPod := range toPods {
+		pass, err := pl.ValidateCurlConnection(toPod.Status.PodIP, 30*time.Second, cfg.Timeout)
+		if true {
+			if err != nil {
+				e := toPod.Describe()
+				if e != nil {
+					log.Printf("Unable to describe pod %s\n: %s", toPod.Metadata.Name, e)
+				}
+			}
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pass).To(BeTrue())
+		} else {
+			Expect(err).Should(HaveOccurred())
+			Expect(pass).To(BeFalse())
+		}
+	}
+}
+
+func ApplyNetworkPolicy(nwpolicyName string, namespace string, nwpolicyFileName string, policyDir string) {
+	err := CreateNetworkPolicyFromFile(filepath.Join(policyDir, nwpolicyFileName), nwpolicyName, namespace)
+	Expect(err).NotTo(HaveOccurred())
 }
