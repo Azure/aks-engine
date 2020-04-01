64cada8087be90dce30a13db9c05f82f5d8d86e7
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-04-01 16:05:08 -0700

    cherry-pick: check dns and network connection in cse (#3008)
    * fix: check network from node to k8s api server in CSE (#2919)
    
    * fix: check network to k8s api for vmss (#2938)
    
    * fix: check dns status before connection (#3002)
    
    Co-authored-by: Bo Wang <bowa@microsoft.com>
    
    Co-authored-by: Bo Wang <bowa@microsoft.com>

--- parts/k8s/cloud-init/artifacts/cse_main.sh
+++ parts/k8s/cloud-init/artifacts/cse_main.sh
@@ -223,6 +223,19 @@ if [[ $OS == $UBUNTU_OS_NAME ]]; then
 fi
 {{end}}
 
+{{- if IsHostedMaster }}
+RES=$(retrycmd_if_failure 20 1 3 nslookup ${API_SERVER_NAME})
+STS=$?
+if [[ $STS != 0 ]]; then
+    if [[ $RES == *"168.63.129.16"*  ]]; then
+        exit $ERR_K8S_API_SERVER_AZURE_DNS_LOOKUP_FAIL
+    else
+        exit $ERR_K8S_API_SERVER_DNS_LOOKUP_FAIL
+    fi
+fi
+retrycmd_if_failure 50 1 3 nc -vz ${API_SERVER_NAME} 443 || exit $ERR_K8S_API_SERVER_CONN_FAIL
+{{end}}
+
 if $REBOOTREQUIRED; then
     echo 'reboot required, rebooting node in 1 minute'
     /bin/bash -c "shutdown -r 1 &"
--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -37009,6 +37009,9 @@ ERR_MS_PROD_DEB_PKG_ADD_FAIL=43 {{/* Failed to add repo pkg file */}}
 ERR_SYSTEMD_INSTALL_FAIL=48 {{/* Unable to install required systemd version */}}
 ERR_MODPROBE_FAIL=49 {{/* Unable to load a kernel module using modprobe */}}
 ERR_OUTBOUND_CONN_FAIL=50 {{/* Unable to establish outbound connection */}}
+ERR_K8S_API_SERVER_CONN_FAIL=51 {{/* Unable to establish connection to k8s api server*/}}
+ERR_K8S_API_SERVER_DNS_LOOKUP_FAIL=52 {{/* Unable to resolve k8s api server name */}}
+ERR_K8S_API_SERVER_AZURE_DNS_LOOKUP_FAIL=53 {{/* Unable to resolve k8s api server name due to Azure DNS issue */}}
 ERR_KATA_KEY_DOWNLOAD_TIMEOUT=60 {{/* Timeout waiting to download kata repo key */}}
 ERR_KATA_APT_KEY_TIMEOUT=61 {{/* Timeout waiting for kata apt-key */}}
 ERR_KATA_INSTALL_TIMEOUT=62 {{/* Timeout waiting for kata install */}}
@@ -37819,6 +37822,19 @@ fi
 if [[ $OS == $UBUNTU_OS_NAME ]]; then
     apt_get_purge 20 30 120 apache2-utils &
 fi
+{{end}}
+
+{{- if IsHostedMaster }}
+RES=$(retrycmd_if_failure 20 1 3 nslookup ${API_SERVER_NAME})
+STS=$?
+if [[ $STS != 0 ]]; then
+    if [[ $RES == *"168.63.129.16"*  ]]; then
+        exit $ERR_K8S_API_SERVER_AZURE_DNS_LOOKUP_FAIL
+    else
+        exit $ERR_K8S_API_SERVER_DNS_LOOKUP_FAIL
+    fi
+fi
+retrycmd_if_failure 50 1 3 nc -vz ${API_SERVER_NAME} 443 || exit $ERR_K8S_API_SERVER_CONN_FAIL
 {{end}}
 
 if $REBOOTREQUIRED; then
