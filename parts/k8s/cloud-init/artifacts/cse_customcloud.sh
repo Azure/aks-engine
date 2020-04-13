59190195d8b66e5b133420ecba09a5a94563ac26
Eric Hong <Eric.Hong@microsoft.com>
2020-04-13 16:31:26 -0700

    Added if not IsAzureStackCloud

--- parts/k8s/cloud-init/artifacts/cse_customcloud.sh
+++ parts/k8s/cloud-init/artifacts/cse_customcloud.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 {{- if IsCustomCloudProfile}}
+{{- if not IsAzureStackCloud}}
 ensureCustomCloudRootCertificates() {
     CUSTOM_CLOUD_ROOT_CERTIFICATES="{{GetCustomCloudRootCertificates}}"
 
@@ -37,6 +38,7 @@ ensureCustomCloudSourcesList() {
         echo $CUSTOM_CLOUD_SOURCES_LIST | base64 -d > /etc/apt/sources.list
     fi
 }
+{{end}}
 
 configureK8sCustomCloud() {
   KUBE_CONTROLLER_MANAGER_FILE=/etc/kubernetes/manifests/kube-controller-manager.yaml
