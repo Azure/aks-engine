3b794ae3779dc57fa01af91a30269ef3f8a9c0a0
Kalya Subramanian <42158129+ksubrmnn@users.noreply.github.com>
2020-04-17 13:13:59 -0700

    refactor: Create signable PS scripts (#3015)
    * feat: Refactor PS scripts to be static

---
+++ parts/k8s/kubeproxystart.ps1
@@ -0,0 +1,27 @@
+$Global:ClusterConfiguration = ConvertFrom-Json ((Get-Content "c:\k\kubeclusterconfig.json" -ErrorAction Stop) | out-string)
+
+$KubeNetwork = "azure"
+if ($Global:ClusterConfiguration.Cni.Name -eq "kubenet") {
+    $KubeNetwork = "l2bridge"
+}
+
+$env:KUBE_NETWORK = $KubeNetwork
+$global:HNSModule = "c:\k\hns.psm1"
+$KubeDir = $Global:ClusterConfiguration.Install.Destination
+
+$hnsNetwork = Get-HnsNetwork | ? Name -EQ $KubeNetwork
+while (!$hnsNetwork) {
+    Write-Host "$(Get-Date -Format o) Waiting for Network [$KubeNetwork] to be created . . ."
+    Start-Sleep 10
+    $hnsNetwork = Get-HnsNetwork | ? Name -EQ $KubeNetwork
+}
+
+#
+# cleanup the persisted policy lists
+#
+Import-Module $global:HNSModule
+# Workaround for https://github.com/kubernetes/kubernetes/pull/68923 in < 1.14,
+# and https://github.com/kubernetes/kubernetes/pull/78612 for <= 1.15
+Get-HnsPolicyList | Remove-HnsPolicyList
+
+.$KubeDir\kube-proxy.exe --v=3 --proxy-mode=kernelspace --hostname-override=$env:computername --kubeconfig=$KubeDir\config
