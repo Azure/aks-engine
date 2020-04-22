3f4a8749b4163b642a394e8a791d3807e60fb5b6
Matt Boersma <Matt.Boersma@microsoft.com>
2020-04-22 12:52:16 -0600

    fix: add to supported versions list

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -189,6 +189,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.18.1":         true,
 	"1.18.2":         true,
 	"1.19.0-alpha.1": true,
+	"1.19.0-alpha.2": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
