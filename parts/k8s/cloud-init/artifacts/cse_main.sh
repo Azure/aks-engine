9ff3d9055ae2a2a4a0e9b8babc15d44c593dfee3
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-04 15:23:23 +0200

    Fix conditional for installing etcd
    If OS is different than UBUNTU_OS_NAME, then UBUNTU_RELEASE is empty,
    which breaks the if condition.
    
    UBUNTU_RELEASE should only be checked if OS == UBUNTU_OS_NAME.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- parts/k8s/cloud-init/artifacts/cse_main.sh
+++ parts/k8s/cloud-init/artifacts/cse_main.sh
@@ -240,7 +240,7 @@ if [[ -n ${MASTER_NODE} ]]; then
   time_metric "EnsureTaints" ensureTaints
 {{end}}
   if [[ -z ${COSMOS_URI} ]]; then
-    if ! { [ "$FULL_INSTALL_REQUIRED" = "true" ] && [ ${UBUNTU_RELEASE} == "18.04" ]; }; then
+    if ! { [ "$FULL_INSTALL_REQUIRED" = "true" ] && [ ${OS} == ${UBUNTU_OS_NAME} ] && [ ${UBUNTU_RELEASE} == "18.04" ]; }; then
       time_metric "EnsureEtcd" ensureEtcd
     fi
   fi
