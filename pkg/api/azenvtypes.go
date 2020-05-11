a487b28954a2244941decfbb843de4037e86c2d2
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-05-11 11:17:52 -0700

    update: Update AKS VHD version (#3224)
    * Update aks vhd version
    
    * update sku
    
    Co-authored-by: Bo Wang <bowa@microsoft.com>

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -136,9 +136,9 @@ var (
 	//Ubuntu1804Gen2OSImageConfig is Gen2 flavor the Ubunutu 18.04-LTS Linux distribution.
 	Ubuntu1804Gen2OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1804-gen2-202004",
+		ImageSku:       "aks-ubuntu-1804-gen2-202005",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.04.16",
+		ImageVersion:   "2020.05.06",
 	}
 
 	//RHELOSImageConfig is the RHEL Linux distribution.
@@ -160,17 +160,17 @@ var (
 	// AKSUbuntu1604OSImageConfig is the AKS image based on Ubuntu 16.04-LTS.
 	AKSUbuntu1604OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1604-202004",
+		ImageSku:       "aks-ubuntu-1604-202005",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.04.16",
+		ImageVersion:   "2020.05.06",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
 	AKSUbuntu1804OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1804-202004",
+		ImageSku:       "aks-ubuntu-1804-202005",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.04.16",
+		ImageVersion:   "2020.05.06",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
