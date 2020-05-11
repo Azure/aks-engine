c0765b8901d83484ee1abe0543714125c3868f56
Bo Wang <bowa@microsoft.com>
2020-05-11 10:27:14 -0700

    Update vhd version

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -158,7 +158,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-engine-ubuntu-1604-202004",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.04.21",
+		ImageVersion:   "2020.05.06",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
@@ -166,7 +166,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-engine-ubuntu-1804-202004",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.04.21",
+		ImageVersion:   "2020.05.06",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
