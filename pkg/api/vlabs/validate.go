498c7d98ac850c10da775d163b47a4fd32af067f
Jack Francis <jackfrancis@gmail.com>
2020-05-11 20:12:42 -0700

    fix: don't validate Windows version in update scenario (#3226)

--- pkg/api/vlabs/validate.go
+++ pkg/api/vlabs/validate.go
@@ -171,7 +171,7 @@ func (a *Properties) validate(isUpdate bool) error {
 		return e
 	}
 
-	if e := a.validateWindowsProfile(); e != nil {
+	if e := a.validateWindowsProfile(isUpdate); e != nil {
 		return e
 	}
 	return nil
@@ -1114,7 +1114,7 @@ func validateVMSS(o *OrchestratorProfile, isUpdate bool, storageProfile string) 
 	return nil
 }
 
-func (a *Properties) validateWindowsProfile() error {
+func (a *Properties) validateWindowsProfile(isUpdate bool) error {
 	hasWindowsAgentPools := false
 	for _, profile := range a.AgentPoolProfiles {
 		if profile.OSType == Windows {
@@ -1141,7 +1141,7 @@ func (a *Properties) validateWindowsProfile() error {
 			o.OrchestratorType,
 			o.OrchestratorRelease,
 			o.OrchestratorVersion,
-			false,
+			isUpdate,
 			true)
 
 		if version == "" {
