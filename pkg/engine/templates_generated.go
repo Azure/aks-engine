5b4d54a9e012356973cad518cf0e24fc13c5dbcc
Jack Francis <jack.francis@microsoft.com>
2020-05-14 12:19:00 -0700

    fix: resilient etcd data disk mounting

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -35797,17 +35797,23 @@ var _k8sCloudInitArtifactsMountetcdSh = []byte(`#!/bin/bash
 # Mounting is done here instead of etcd because of bug https://bugs.launchpad.net/cloud-init/+bug/1692093
 # Once the bug is fixed, replace the below with the cloud init changes replaced in https://github.com/Azure/aks-engine/pull/661.
 set -x
-DISK=/dev/sdc
-PARTITION=${DISK}1
+PARTITION=""
+for DISK in $(cat /proc/partitions | grep -o sd[a-z] | uniq); do
+  if ! cat /proc/partitions | grep "$DISK"1; then
+    if [[ -n $PARTITION ]]; then
+      exit 1
+    fi
+    PARTITION=/dev/${DISK}1
+  fi;
+done
 MOUNTPOINT=/var/lib/etcddisk
 udevadm settle
 mkdir -p $MOUNTPOINT
 if mount | grep $MOUNTPOINT; then
-  echo "disk is already mounted"
-  umount /dev/sdc1
+  umount $MOUNTPOINT
 fi
-if ! grep "/dev/sdc1" /etc/fstab; then
-  echo "$PARTITION       $MOUNTPOINT       auto    defaults,nofail       0       2" >>/etc/fstab
+if ! grep "$MOUNTPOINT" /etc/fstab; then
+  echo "LABEL=etcd_disk       $MOUNTPOINT       auto    defaults,nofail       0       2" >>/etc/fstab
 fi
 if ! ls $PARTITION; then
   /sbin/sgdisk --new 1 $DISK
