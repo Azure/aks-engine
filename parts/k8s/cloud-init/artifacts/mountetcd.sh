b9c27c1baeb49f505d9589f271192a0c53e53f8e
Jack Francis <jack.francis@microsoft.com>
2020-05-14 12:33:24 -0700

    we need to store the disk var

--- parts/k8s/cloud-init/artifacts/mountetcd.sh
+++ parts/k8s/cloud-init/artifacts/mountetcd.sh
@@ -2,13 +2,15 @@
 # Mounting is done here instead of etcd because of bug https://bugs.launchpad.net/cloud-init/+bug/1692093
 # Once the bug is fixed, replace the below with the cloud init changes replaced in https://github.com/Azure/aks-engine/pull/661.
 set -x
+ETCDDISK=""
 PARTITION=""
 for DISK in $(cat /proc/partitions | grep -o sd[a-z] | uniq); do
   if ! cat /proc/partitions | grep "$DISK"1; then
     if [[ -n $PARTITION ]]; then
       exit 1
     fi
-    PARTITION=/dev/${DISK}1
+    ETCDDISK=/dev/${DISK}
+    PARTITION=${ETCDDISK}1
   fi;
 done
 MOUNTPOINT=/var/lib/etcddisk
@@ -21,7 +23,7 @@ if ! grep "$MOUNTPOINT" /etc/fstab; then
   echo "LABEL=etcd_disk       $MOUNTPOINT       auto    defaults,nofail       0       2" >>/etc/fstab
 fi
 if ! ls $PARTITION; then
-  /sbin/sgdisk --new 1 $DISK
+  /sbin/sgdisk --new 1 $ETCDDISK
   /sbin/mkfs.ext4 $PARTITION -L etcd_disk -F -E lazy_itable_init=1,lazy_journal_init=1
 fi
 mount $MOUNTPOINT
