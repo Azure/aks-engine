55ce90846cf959dd1d55b87fe75bfe9e11d0d573
Jack Francis <jackfrancis@gmail.com>
2020-05-15 11:17:26 -0700

    fix: resilient etcd data disk mounting (#3259)

--- parts/k8s/cloud-init/artifacts/mountetcd.sh
+++ parts/k8s/cloud-init/artifacts/mountetcd.sh
@@ -2,22 +2,22 @@
 # Mounting is done here instead of etcd because of bug https://bugs.launchpad.net/cloud-init/+bug/1692093
 # Once the bug is fixed, replace the below with the cloud init changes replaced in https://github.com/Azure/aks-engine/pull/661.
 set -x
-DISK=/dev/sdc
-PARTITION=${DISK}1
-MOUNTPOINT=/var/lib/etcddisk
 udevadm settle
-mkdir -p $MOUNTPOINT
-if mount | grep $MOUNTPOINT; then
-  echo "disk is already mounted"
-  umount /dev/sdc1
-fi
-if ! grep "/dev/sdc1" /etc/fstab; then
-  echo "$PARTITION       $MOUNTPOINT       auto    defaults,nofail       0       2" >>/etc/fstab
-fi
+MOUNTPOINT=/var/lib/etcddisk
+LABEL=etcd_disk
+ETCDDISK=$(readlink -f /dev/disk/azure/scsi1/lun0)
+PARTITION=${ETCDDISK}1
 if ! ls $PARTITION; then
-  /sbin/sgdisk --new 1 $DISK
-  /sbin/mkfs.ext4 $PARTITION -L etcd_disk -F -E lazy_itable_init=1,lazy_journal_init=1
+  /sbin/sgdisk --new 1 $ETCDDISK
 fi
+if ! blkid $PARTITION | grep "LABEL=.${LABEL}"; then
+  /sbin/mkfs.ext4 $PARTITION -L $LABEL -F -E lazy_itable_init=1,lazy_journal_init=1
+fi
+mkdir -p $MOUNTPOINT
+if ! grep "$MOUNTPOINT" /etc/fstab; then
+  echo "LABEL=${LABEL}       $MOUNTPOINT       auto    defaults,nofail       0       2" >>/etc/fstab
+fi
+umount $MOUNTPOINT
 mount $MOUNTPOINT
 /bin/chown -R etcd:etcd /var/lib/etcddisk
 #EOF
