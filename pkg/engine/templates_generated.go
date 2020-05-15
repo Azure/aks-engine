d52a6c31ad6e8beae2dc29d9ae9041ba901cdd30
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-15 13:56:15 +0200

    Fix installing kubectl and kubelet binaries on Flatcar
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -18340,7 +18340,8 @@ ensureJournal() {
   systemctlEnableAndStart systemd-journald || exit {{GetCSEErrorCode "ERR_SYSTEMCTL_START_FAIL"}}
 }
 installKubeletAndKubectl() {
-  if [[ ! -f "/usr/local/bin/kubectl-${KUBERNETES_VERSION}" ]]; then
+  path=/usr/local/bin
+  if [[ ! -f "${path}/kubectl-${KUBERNETES_VERSION}" ]]; then
     if version_gte ${KUBERNETES_VERSION} 1.17; then
       extractKubeBinaries
     else
@@ -18351,10 +18352,13 @@ installKubeletAndKubectl() {
       fi
     fi
   fi
-  mv "/usr/local/bin/kubelet-${KUBERNETES_VERSION}" "/usr/local/bin/kubelet"
-  mv "/usr/local/bin/kubectl-${KUBERNETES_VERSION}" "/usr/local/bin/kubectl"
-  chmod a+x /usr/local/bin/kubelet /usr/local/bin/kubectl
-  rm -rf /usr/local/bin/kubelet-* /usr/local/bin/kubectl-* /home/hyperkube-downloads &
+  if [[ $OS == $FLATCAR_OS_NAME ]]; then
+    path=/opt
+  fi
+  mv "${path}/kubelet-${KUBERNETES_VERSION}" "${path}/kubelet"
+  mv "${path}/kubectl-${KUBERNETES_VERSION}" "${path}/kubectl"
+  chmod a+x ${path}/kubelet ${path}/kubectl
+  rm -rf ${path}/kubelet-* ${path}/kubectl-* /home/hyperkube-downloads &
 }
 ensureK8sControlPlane() {
   if $REBOOTREQUIRED || [ "$NO_OUTBOUND" = "true" ]; then
@@ -19284,12 +19288,16 @@ installImg() {
 extractHyperkube() {
   CLI_TOOL=$1
   path="/home/hyperkube-downloads/${KUBERNETES_VERSION}"
+  targetpath="/usr/local/bin"
+  if [[ $OS == $FLATCAR_OS_NAME ]]; then
+    targetpath="/opt"
+  fi
   pullContainerImage $CLI_TOOL ${HYPERKUBE_URL}
   if [[ $CLI_TOOL == "docker" ]]; then
     mkdir -p "$path"
     if docker run --rm --entrypoint "" -v $path:$path ${HYPERKUBE_URL} /bin/bash -c "cp /usr/local/bin/{kubelet,kubectl} $path"; then
-      mv "$path/kubelet" "/usr/local/bin/kubelet-${KUBERNETES_VERSION}"
-      mv "$path/kubectl" "/usr/local/bin/kubectl-${KUBERNETES_VERSION}"
+      mv "$path/kubelet" "${targetpath}/kubelet-${KUBERNETES_VERSION}"
+      mv "$path/kubectl" "${targetpath}/kubectl-${KUBERNETES_VERSION}"
       return
     else
       docker run --rm -v $path:$path ${HYPERKUBE_URL} /bin/bash -c "cp /hyperkube $path"
