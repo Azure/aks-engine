6956fa15908773bad6a9cb47a620b872394f236a
Jack Francis <jack.francis@microsoft.com>
2020-05-26 11:22:58 -0700

    contains works

--- vhd/packer/vhd-rg-cleanup.sh
+++ vhd/packer/vhd-rg-cleanup.sh
@@ -48,7 +48,7 @@ az account set -s $SUBSCRIPTION_ID_TO_CLEANUP
 
 # find packer resource groups created before our deadline
 echo "Looking for resource groups created over ${EXPIRATION_IN_HOURS} hours ago..."
-for resourceGroup in $( az group list --query "[?contains(name, 'packer-Resource-Group')]" | jq --arg dl $deadline '.[] | select(.tags.now < $dl).name' | tr -d '\"' || ""); do
+for resourceGroup in $( az group list | jq --arg dl $deadline '.[] | select(.name | contains("packer-Resource-Group") or contains("pkr-Resource-Group")) | select(.tags.now < $dl).name' | tr -d '\"' || ""); do
     for deployment in $(az deployment group list -g $resourceGroup | jq '.[] | .name' | tr -d '\"' || ""); do
         echo "Will delete deployment ${deployment} from resource group ${resourceGroup}..."
         if [[ "${DRY_RUN}" = false ]]; then
