664bd2654045fea93ac33831ac8ad51dacdce96c
Jack Francis <jackfrancis@gmail.com>
2020-05-29 13:02:11 -0700

    chore: rev pause image to 1.3.1 (#3370)

--- vhd/packer/install-dependencies.sh
+++ vhd/packer/install-dependencies.sh
@@ -192,10 +192,10 @@ for KUBE_DNS_MASQ_VERSION in ${KUBE_DNS_MASQ_VERSIONS}; do
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
-MCR_PAUSE_VERSIONS="1.2.0"
+MCR_PAUSE_VERSIONS="1.3.1"
 for PAUSE_VERSION in ${MCR_PAUSE_VERSIONS}; do
     # Pull the arch independent MCR pause image which is built for Linux and Windows
-    CONTAINER_IMAGE="mcr.microsoft.com/k8s/core/pause:${PAUSE_VERSION}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/kubernetes/pause:${PAUSE_VERSION}"
     pullContainerImage "docker" "${CONTAINER_IMAGE}"
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
