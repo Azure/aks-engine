bc684f301c9f21b51c0b19be2a8c50aa22cebd0f
marosset <marosset@microsoft.com>
2020-06-10 18:05:45 +0000

    Adding build-windows-provisioning-scripts.sh

---
+++ scripts/build-windows-provisioning-scripts.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+required_env_vars=(
+    "CLIENT_ID"
+    "CLIENT_SECRET"
+    "RESOURCE_GROUP_NAME"
+    "STORAGE_ACCOUNT_NAME"
+    "STORAGE_CONTAINER_NAME"
+    "SUBSCRIPTION_ID"
+    "TENANT_ID"
+)
+
+for v in "${required_env_vars[@]}"
+do
+    if [ -z "${!v}" ]; then
+        echo "$v was not set!"
+        exit 1
+    fi
+done
+
+CREATE_TIME="$(date +%s)"
+az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
+
+avail=$(az storage account check-name -n ${STORAGE_ACCOUNT_NAME} -o json | jq -r .nameAvailable)
+if $avail ; then
+    echo "creating new storage account: ${STORAGE_ACCOUNT_NAME}..."
+    az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME --sku "Standard_RAGRS" --tags "now=${CREATE_TIME}"
+else
+    echo "storage account '${STORAGE_ACCOUNT_NAME}' already exists."
+fi
+
+exists=$(az storage container exists -n ${STORAGE_CONTAINER_NAME} --account-name ${STORAGE_ACCOUNT_NAME} -o json | jq -r .exists)
+if ! $exists ; then
+    echo "creating new storage container: ${STORAGE_CONTAINER_NAME}..."
+    key=$(az storage account keys list -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -o json | jq -r '.[0].value')
+    az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key=$key --public-access=blob
+else
+    echo "storage container '${STORAGE_CONTAINER_NAME}' already exists."
+fi
+
+temp_dir=$(mktemp -d -t aks-engine-XXXXXXXX)
+zip_name=windows-provisioning-scripts-${CREATE_TIME}.zip
+zip_path="${temp_dir}/$zip_name"
+echo "creating provisioning ${zip_path}..."
+zip -j $zip_path ./staging/provisioning/windows/*
+
+echo "uploading zip to storage account..."
+az storage copy --source-local-path $zip_path --destination-account-name $STORAGE_ACCOUNT_NAME --destination-container $STORAGE_CONTAINER_NAME
+
+rm -r -f $temp_dir
+echo ""
+echo "https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${STORAGE_CONTAINER_NAME}/${zip_name}"
