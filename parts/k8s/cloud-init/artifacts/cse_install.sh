d5a0ab34bcb86d7e44e0b859b10514641ce20770
Andy Liu <andyliuliming@outlook.com>
2020-06-12 00:01:56 +0800

    fix

--- parts/k8s/cloud-init/artifacts/cse_install.sh
+++ parts/k8s/cloud-init/artifacts/cse_install.sh
@@ -288,19 +288,19 @@ cleanUpContainerImages() {
     function cleanUpHyperkubeImagesRun() {
       images_to_delete=$(docker images --format '{{OpenBraces}}.Repository{{CloseBraces}}:{{OpenBraces}}.Tag{{CloseBraces}}' | grep -vE "${KUBERNETES_VERSION}$|${KUBERNETES_VERSION}.[0-9]+$|${KUBERNETES_VERSION}-|${KUBERNETES_VERSION}_" | grep 'hyperkube')
       if [[ "${images_to_delete}" != "" ]]; then
-        docker rmi "${images_to_delete[@]}"
+        docker rmi ${images_to_delete[@]}
       fi
     }
     function cleanUpControllerManagerImagesRun() {
       images_to_delete=$(docker images --format '{{OpenBraces}}.Repository{{CloseBraces}}:{{OpenBraces}}.Tag{{CloseBraces}}' | grep -vE "${KUBERNETES_VERSION}$|${KUBERNETES_VERSION}.[0-9]+$|${KUBERNETES_VERSION}-|${KUBERNETES_VERSION}_" | grep 'cloud-controller-manager')
       if [[ "${images_to_delete}" != "" ]]; then
-        docker rmi "${images_to_delete[@]}"
+        docker rmi ${images_to_delete[@]}
       fi
     }
     function cleanUpEtcdImagesRun() {
       images_to_delete=$(docker images --format '{{OpenBraces}}.Repository{{CloseBraces}}:{{OpenBraces}}.Tag{{CloseBraces}}' | grep -vE "${ETCD_VERSION}$|${ETCD_VERSION}-|${ETCD_VERSION}_" | grep 'etcd')
       if [[ "${images_to_delete}" != "" ]]; then
-        docker rmi "${images_to_delete[@]}"
+        docker rmi ${images_to_delete[@]}
       fi
     }
     retrycmd_if_failure 10 5 120 bash -c cleanUpHyperkubeImagesRun
