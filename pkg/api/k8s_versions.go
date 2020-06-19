cffa6e00623eceda3f9aa80a80a0f53f6f506649
Jack Francis <jack.francis@microsoft.com>
2020-06-19 09:52:28 -0700

    fix: use hotfix Windows builds for 1.18.4, 1.17.7, and 1.16.11

--- pkg/api/k8s_versions.go
+++ pkg/api/k8s_versions.go
@@ -348,11 +348,26 @@ func GetK8sComponentsByVersionMap(k *KubernetesConfig) map[string]map[string]str
 }
 
 func getVersionOverridesMCR(v string) map[string]string {
-	return nil
+	switch v {
+	case "1.18.4":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.18.4-hotfix.20200617/windowszip/v1.18.4-hotfix.20200617-1int.zip"}
+	case "1.17.7":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.17.7-hotfix.20200617/windowszip/v1.17.7-hotfix.20200617-1int.zip"}
+	case "1.16.11":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.16.11-hotfix.20200617/windowszip/v1.16.11-hotfix.20200617-1int.zip"}
+	default:
+		return nil
+	}
 }
 
 func getVersionOverridesGCR(v string) map[string]string {
 	switch v {
+	case "1.18.4":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.18.4-hotfix.20200617/windowszip/v1.18.4-hotfix.20200617-1int.zip"}
+	case "1.17.7":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.17.7-hotfix.20200617/windowszip/v1.17.7-hotfix.20200617-1int.zip"}
+	case "1.16.11":
+		return map[string]string{common.WindowsArtifactComponentName: "v1.16.11-hotfix.20200617/windowszip/v1.16.11-hotfix.20200617-1int.zip"}
 	case "1.8.11":
 		return map[string]string{common.KubeDNSAddonName: "k8s-dns-kube-dns-amd64:1.14.9"}
 	case "1.8.9":
