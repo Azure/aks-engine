34fe69a9cabbeb57c06e3e4b10f636eb611cad4a
Liming Liu <andyliuliming@outlook.com>
2020-07-12 13:01:24 +0800

    switch to use the new 07 vhd. (#3592)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -136,9 +136,9 @@ var (
 	//Ubuntu1804Gen2OSImageConfig is Gen2 flavor the Ubunutu 18.04-LTS Linux distribution.
 	Ubuntu1804Gen2OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1804-gen2-202006",
+		ImageSku:       "aks-ubuntu-1804-gen2-202007",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.06.30",
+		ImageVersion:   "2020.07.11",
 	}
 
 	//RHELOSImageConfig is the RHEL Linux distribution.
@@ -160,17 +160,17 @@ var (
 	// AKSUbuntu1604OSImageConfig is the AKS image based on Ubuntu 16.04-LTS.
 	AKSUbuntu1604OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1604-202006",
+		ImageSku:       "aks-ubuntu-1604-202007",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.06.30",
+		ImageVersion:   "2020.07.11",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
 	AKSUbuntu1804OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1804-202006",
+		ImageSku:       "aks-ubuntu-1804-202007",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.06.30",
+		ImageVersion:   "2020.07.11",
 	}
 
 	// AKSUbuntuGPU1804OSImageConfig is the AKS image with GPU drivers and device plugin based on Ubuntu 18.04-LTS.
