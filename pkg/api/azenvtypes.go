c47bc73db7afbf3ffdcf72b0501b2f8a815139b1
Tony Xu <tonyxu@microsoft.com>
2020-08-07 16:50:04 -0700

    Set AKS VHD to 2020.08.06

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -146,7 +146,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-gen2-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.07.16",
+		ImageVersion:   "2020.08.06",
 	}
 
 	//RHELOSImageConfig is the RHEL Linux distribution.
@@ -170,7 +170,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.07.16",
+		ImageVersion:   "2020.08.06",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
@@ -178,7 +178,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.07.16",
+		ImageVersion:   "2020.08.06",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
