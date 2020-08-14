8f71212ef8a37c02eab3bfbca94783e262146407
James Sturtevant <jstur@microsoft.com>
2020-08-14 16:20:18 -0700

    fix: update to latest image for this 0.52 release

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -174,7 +174,7 @@ var (
 		ImageOffer:     "aks-windows",
 		ImageSku:       "2019-datacenter-core-smalldisk-2006",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "17763.1217.200603",
+		ImageVersion:   "17763.1282.200626",
 	}
 
 	// WindowsServer2019OSImageConfig is the 'vanilla' Windows Server 2019 image
