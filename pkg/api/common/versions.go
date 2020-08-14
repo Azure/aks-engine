f15f186bab02bc516239f282707ca9100d130078
jadarsie <jadarsie@microsoft.com>
2020-08-14 09:10:20 -0700

    feat: add support for K8s v1.17.11 on Azure Stack

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -235,6 +235,7 @@ var AllKubernetesSupportedVersionsAzureStack = map[string]bool{
 	"1.17.6":  false,
 	"1.17.7":  false,
 	"1.17.9":  true,
+	"1.17.11": true,
 }
 
 // AllKubernetesWindowsSupportedVersionsAzureStack maintain a set of available k8s Windows versions in aks-engine on Azure Stack
@@ -251,6 +252,7 @@ var AllKubernetesWindowsSupportedVersionsAzureStack = map[string]bool{
 	"1.17.6":  false,
 	"1.17.7":  false,
 	"1.17.9":  true,
+	"1.17.11": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
