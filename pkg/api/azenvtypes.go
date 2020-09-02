9a86005b5dd0bc1bee973962325acf73f051d5e5
Ace Eldeib <alexeldeib@gmail.com>
2020-09-02 12:04:56 -0700

    chore: update aks vhd to v20200902

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -146,7 +146,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-gen2-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.08.19",
+		ImageVersion:   "2020.09.02",
 	}
 
 	//RHELOSImageConfig is the RHEL Linux distribution.
@@ -170,7 +170,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.08.19",
+		ImageVersion:   "2020.09.02",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
@@ -178,7 +178,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-2020-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2020.08.19",
+		ImageVersion:   "2020.09.02",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
