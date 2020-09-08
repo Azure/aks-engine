2ec680656313ca6a0ead3c582a68189bf6b8cd50
JesusAlvarezTorres <jesusa@microsoft.com>
2020-09-08 16:19:43 -0700

    fix: add Azure Stack CNI jsons to the log collection

--- scripts/collect-logs.sh
+++ scripts/collect-logs.sh
@@ -18,6 +18,12 @@ collectCloudProviderJson() {
     if [ -f /etc/kubernetes/azurestackcloud.json ]; then
         jq . /etc/kubernetes/azurestackcloud.json > ${DIR}/azurestackcloud.json
     fi
+    if [ -f /etc/kubernetes/network_interfaces.json ]; then
+        jq . /etc/kubernetes/network_interfaces.json > ${DIR}/network_interfaces.json
+    fi
+    if [ -f /etc/kubernetes/interfaces.json ]; then
+        jq . /etc/kubernetes/interfaces.json > ${DIR}/interfaces.json
+    fi
     if [ -f /opt/azure/vhd-install.complete ]; then
         mkdir -p ${OUTDIR}/opt/azure
         cp /opt/azure/vhd-install.complete ${OUTDIR}/opt/azure
