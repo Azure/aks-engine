3a91897614e63ca9267b800775297c4e53865586
Jack Francis <jack.francis@microsoft.com>
2020-09-09 09:55:11 -0700

    chore: remove apiserver /etc/kubernetes/certs mount

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -24218,8 +24218,6 @@ spec:
       volumeMounts:
         - name: etc-kubernetes
           mountPath: /etc/kubernetes
-        - name: etc-kubernetes-certs
-          mountPath: /etc/kubernetes/certs
         - name: etc-ssl-certs
           mountPath: /etc/ssl/certs
         - name: var-lib-kubelet
@@ -24235,9 +24233,6 @@ spec:
     - name: etc-kubernetes
       hostPath:
         path: /etc/kubernetes
-    - name: etc-kubernetes-certs
-      hostPath:
-        path: /etc/kubernetes/certs
     - name: etc-ssl-certs
       hostPath:
         path: /etc/ssl/certs
