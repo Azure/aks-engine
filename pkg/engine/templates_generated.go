afda41f6d8239f1c00652ecd8eeeed2b387a3e7a
Jack Francis <jack.francis@microsoft.com>
2020-09-11 14:24:14 -0700

    optimized ip retrieval

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -19313,11 +19313,8 @@ NVIDIA_DOCKER_VERSION=2.0.3
 DOCKER_VERSION=1.13.1-1
 NVIDIA_CONTAINER_RUNTIME_VER=2.0.0
 NVIDIA_DOCKER_SUFFIX=docker18.09.2-1
-PRIVATE_IP=$(ip -br -4 addr show eth0 | grep -Po '\d+\.\d+\.\d+\.\d+')
-if [[ $PRIVATE_IP == "" ]]; then
-  PRIVATE_IP=$(ip -br -4 addr show azure0 | grep -Po '\d+\.\d+\.\d+\.\d+')
-fi
-if [[ $PRIVATE_IP == "" ]]; then
+PRIVATE_IP=$( (ip -br -4 addr show eth0 || ip -br -4 addr show azure0) | grep -Po '\d+\.\d+\.\d+\.\d+')
+if ! [[ $(echo -n "$PRIVATE_IP" | grep -c '^') == 1 ]]; then
   PRIVATE_IP=$(hostname -i)
 fi
 export PRIVATE_IP
