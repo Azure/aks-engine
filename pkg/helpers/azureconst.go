49e88913ab6640c3c69e294280174c01101c225c
Tony Xu <tonyxu@microsoft.com>
2020-09-11 08:51:39 -0700

    feat: Add T4 skus for samsung (#3818)
    * Add T4 skus
    
    * Fix tabbing
    
    * Revert changes to pkg/engine/templates_generated.go

--- pkg/helpers/azureconst.go
+++ pkg/helpers/azureconst.go
@@ -710,7 +710,11 @@ func GetKubernetesAllowedVMSKUs() string {
         "Standard_NV6s_v2",
         "Standard_PB12s",
         "Standard_PB24s",
-        "Standard_PB6s"
+        "Standard_PB6s",
+        "Standard_NC4as_T4_v3",
+        "Standard_NC8as_T4_v3",
+        "Standard_NC16as_T4_v3",
+        "Standard_NC64as_T4_v3"
     ],
 `
 }
@@ -1691,6 +1695,18 @@ func GetSizeMap() string {
       "storageAccountType": "Premium_LRS"
     },
     "Standard_PB6s": {
+      "storageAccountType": "Premium_LRS"
+    },
+    "Standard_NC4as_T4_v3": {
+      "storageAccountType": "Premium_LRS"
+    },
+    "Standard_NC8as_T4_v3": {
+      "storageAccountType": "Premium_LRS"
+    },
+    "Standard_NC16as_T4_v3": {
+      "storageAccountType": "Premium_LRS"
+    },
+    "Standard_NC64as_T4_v3": {
       "storageAccountType": "Premium_LRS"
     }
    }
