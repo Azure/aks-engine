5cc9092f7f7c7eba278b14f9800d1e03c7482f3b
Jack Francis <jack.francis@microsoft.com>
2020-09-23 11:25:45 -0700

    add deployment if control plane is in unique vnet

--- pkg/engine/masterarmresources.go
+++ pkg/engine/masterarmresources.go
@@ -4,6 +4,8 @@
 package engine
 
 import (
+	"strings"
+
 	"github.com/Azure/aks-engine/pkg/api"
 	"github.com/Azure/go-autorest/autorest/to"
 )
@@ -120,19 +122,40 @@ func createKubernetesMasterResourcesVMAS(cs *api.ContainerService) []interface{}
 		masterCSE.ARMResource.DependsOn = append(masterCSE.ARMResource.DependsOn, "[concat('Microsoft.KeyVault/vaults/', variables('clusterKeyVaultName'))]")
 	}
 
-	// TODO: This is only necessary if the resource group of the masters is different from the RG of the node pool
-	// subnet. But when we generate the template we don't know to which RG it will be deployed to. To solve this we
-	// would have to add the necessary condition into the template. For the resources we can use the `condition` field
-	// but how can we conditionally declare the dependencies? Perhaps by creating a variable for the dependency array
-	// and conditionally adding more dependencies.
-	if kubernetesConfig.SystemAssignedIDEnabled() &&
-		// The fix for ticket 2373 is only available for individual VMs / AvailabilitySet.
-		cs.Properties.MasterProfile.IsAvailabilitySet() {
-		masterRoleAssignmentForAgentPools := createKubernetesMasterRoleAssignmentForAgentPools(cs.Properties.MasterProfile, cs.Properties.AgentPoolProfiles)
+	var vnetInCluster = struct{}{}
+	vnets := make(map[string]struct{})
+	for _, agentPool := range cs.Properties.AgentPoolProfiles {
+		subnetElements := strings.Split(agentPool.VnetSubnetID, "/")
+		if len(subnetElements) < 9 {
+			continue
+		}
+		vnetResourceURI := strings.Join(subnetElements[:9], "/")
+		if _, ok := vnets[vnetResourceURI]; ok {
+			continue
+		}
+		vnets[vnetResourceURI] = vnetInCluster
+	}
+	var controlPlaneVNETResourceURI string
+	controlPlaneSubnetElements := strings.Split(cs.Properties.MasterProfile.VnetSubnetID, "/")
+	if len(controlPlaneSubnetElements) >= 9 {
+		controlPlaneVNETResourceURI = strings.Join(controlPlaneSubnetElements[:9], "/")
+	}
+	// If the control plane is in a discrete VNET
+	if _, ok := vnets[controlPlaneVNETResourceURI]; !ok {
+		// TODO: This is only necessary if the resource group of the masters is different from the RG of the node pool
+		// subnet. But when we generate the template we don't know to which RG it will be deployed to. To solve this we
+		// would have to add the necessary condition into the template. For the resources we can use the `condition` field
+		// but how can we conditionally declare the dependencies? Perhaps by creating a variable for the dependency array
+		// and conditionally adding more dependencies.
+		if kubernetesConfig.SystemAssignedIDEnabled() &&
+			// The fix for ticket 2373 is only available for individual VMs / AvailabilitySet.
+			cs.Properties.MasterProfile.IsAvailabilitySet() {
+			masterRoleAssignmentForAgentPools := createKubernetesMasterRoleAssignmentForAgentPools(cs.Properties.MasterProfile, cs.Properties.AgentPoolProfiles)
 
-		for _, assignmentForAgentPool := range masterRoleAssignmentForAgentPools {
-			masterResources = append(masterResources, assignmentForAgentPool)
-			masterCSE.ARMResource.DependsOn = append(masterCSE.ARMResource.DependsOn, *assignmentForAgentPool.Name)
+			for _, assignmentForAgentPool := range masterRoleAssignmentForAgentPools {
+				masterResources = append(masterResources, assignmentForAgentPool)
+				masterCSE.ARMResource.DependsOn = append(masterCSE.ARMResource.DependsOn, *assignmentForAgentPool.Name)
+			}
 		}
 	}
 
