442453861791f066f84e1fc2bfa07c3b2157d41e
Jack Francis <jack.francis@microsoft.com>
2020-10-02 09:17:08 -0700

    don't retry E2E if PrincipalNotFound

--- test/e2e/azure/cli.go
+++ test/e2e/azure/cli.go
@@ -5,7 +5,6 @@
 package azure
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -290,7 +289,7 @@ func (a *Account) CreateDeployment(name string, e *engine.Engine) error {
 	if err != nil {
 		log.Printf("\nError from deployment for %s in resource group %s:%s\n", d.Name, a.ResourceGroup.Name, err)
 		log.Printf("Command Output: %s\n", out)
-		if bytes.Contains(out, []byte("PrincipalNotFound")) {
+		/*if bytes.Contains(out, []byte("PrincipalNotFound")) {
 			for err != nil {
 				cmd = exec.Command("az", azArgsStringSlice...)
 				util.PrintCommand(cmd)
@@ -299,7 +298,7 @@ func (a *Account) CreateDeployment(name string, e *engine.Engine) error {
 					log.Printf("Command Output: %s\n", out)
 				}
 			}
-		}
+		}*/
 		return err
 	}
 	quit <- true
