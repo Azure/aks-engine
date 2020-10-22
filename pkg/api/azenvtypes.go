5387290ab15b2fcc21e7fa934e3eeb6022929261
Qingchuan Hao <qinhao@microsoft.com>
2020-10-22 02:46:01 +0000

    chore: Update Windows image version

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -186,7 +186,7 @@ var (
 		ImageOffer:     "aks-windows",
 		ImageSku:       "aks-2019-datacenter-core-smalldisk-2010",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "17763.1397.201014",
+		ImageVersion:   "17763.1554.201021",
 	}
 
 	// WindowsServer2019OSImageConfig is the 'vanilla' Windows Server 2019 image
