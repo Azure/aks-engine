79de20bd76fe7c7277805a982b36aa417df71766
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2020-10-23 13:54:51 -0700

    refactor: delete Azure Stack's KubernetesClient implementation (#3957)

--- pkg/armhelpers/interfaces.go
+++ pkg/armhelpers/interfaces.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/Azure/aks-engine/pkg/kubernetes"
 	"github.com/Azure/azure-sdk-for-go/services/authorization/mgmt/2015-07-01/authorization"
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-12-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
@@ -16,10 +17,6 @@ import (
 
 	azStorage "github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/Azure/go-autorest/autorest"
-	log "github.com/sirupsen/logrus"
-	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 )
 
 // ResourceSkusResultPage
@@ -201,7 +198,7 @@ type AKSEngineClient interface {
 	DeleteManagedDisk(ctx context.Context, resourceGroupName string, diskName string) error
 	ListManagedDisksByResourceGroup(ctx context.Context, resourceGroupName string) (result DiskListPage, err error)
 
-	GetKubernetesClient(apiserverURL, kubeConfig string, interval, timeout time.Duration) (KubernetesClient, error)
+	GetKubernetesClient(apiserverURL, kubeConfig string, interval, timeout time.Duration) (kubernetes.Client, error)
 
 	ListProviders(ctx context.Context) (ProviderListResultPage, error)
 
@@ -230,44 +227,4 @@ type AKSStorageClient interface {
 	CreateContainer(containerName string, options *azStorage.CreateContainerOptions) (bool, error)
 	// SaveBlockBlob initializes a block blob by taking the byte
 	SaveBlockBlob(containerName, blobName string, b []byte, options *azStorage.PutBlobOptions) error
-}
-
-// KubernetesClient interface models client for interacting with kubernetes api server
-type KubernetesClient interface {
-	// ListPods returns Pods running on the passed in node.
-	ListPods(node *v1.Node) (*v1.PodList, error)
-	// ListPods returns all Pods running
-	ListAllPods() (*v1.PodList, error)
-	// ListNodes returns a list of Nodes registered in the api server.
-	ListNodes() (*v1.NodeList, error)
-	// ListServiceAccounts returns a list of Service Accounts in a namespace
-	ListServiceAccounts(namespace string) (*v1.ServiceAccountList, error)
-	// GetDaemonSet returns details about DaemonSet with passed in name.
-	GetDaemonSet(namespace, name string) (*appsv1.DaemonSet, error)
-	// GetDeployment returns a given deployment in a namespace.
-	GetDeployment(namespace, name string) (*appsv1.Deployment, error)
-	// GetNode returns details about node with passed in name.
-	GetNode(name string) (*v1.Node, error)
-	// UpdateNode updates the node in the api server with the passed in info.
-	UpdateNode(node *v1.Node) (*v1.Node, error)
-	// DeleteNode deregisters node in the api server.
-	DeleteNode(name string) error
-	// SupportEviction queries the api server to discover if it supports eviction, and returns supported type if it is supported.
-	SupportEviction() (string, error)
-	// DeleteClusterRole deletes the passed in ClusterRole.
-	DeleteClusterRole(role *rbacv1.ClusterRole) error
-	// DeleteDaemonSet deletes the passed in DaemonSet.
-	DeleteDaemonSet(ds *appsv1.DaemonSet) error
-	// DeleteDeployment deletes the passed in Deployment.
-	DeleteDeployment(ds *appsv1.Deployment) error
-	// DeletePod deletes the passed in pod.
-	DeletePod(pod *v1.Pod) error
-	// DeleteServiceAccount deletes the passed in service account.
-	DeleteServiceAccount(sa *v1.ServiceAccount) error
-	// EvictPod evicts the passed in pod using the passed in api version.
-	EvictPod(pod *v1.Pod, policyGroupVersion string) error
-	// WaitForDelete waits until all pods are deleted. Returns all pods not deleted and an error on failure.
-	WaitForDelete(logger *log.Entry, pods []v1.Pod, usingEviction bool) ([]v1.Pod, error)
-	// UpdateDeployment updates a deployment to match the given specification.
-	UpdateDeployment(namespace string, deployment *appsv1.Deployment) (*appsv1.Deployment, error)
 }
