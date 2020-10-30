87338f2f384c8ddecfeb57206f300b24e6fc5147
JesusAlvarezTorres <jesusa@microsoft.com>
2020-10-30 14:57:32 -0700

    chore: Set go sdk log level for Azure Stack clusters

--- parts/k8s/cloud-init/artifacts/cse_customcloud.sh
+++ parts/k8s/cloud-init/artifacts/cse_customcloud.sh
@@ -28,6 +28,9 @@ ensureCustomCloudRootCertificates() {
             sed -i "/<volumeMountssl>/d" $KUBE_CONTROLLER_MANAGER_FILE
         fi
     fi
+
+    # remove the placeholder go sdk log level as it should be set for azure stack cloud only.
+    sed -i "/<gosdkloglevel>/d" $KUBE_CONTROLLER_MANAGER_FILE
 }
 
 ensureCustomCloudSourcesList() {
@@ -124,6 +127,9 @@ ensureAzureStackCertificates() {
       sed -i "/<volumeMountssl>/d" $KUBE_CONTROLLER_MANAGER_FILE
     fi
   fi
+
+  # set the go sdk log level
+  sed -i "s|<gosdkloglevel>|- name: AZURE_GO_SDK_LOG_LEVEL\n        value: INFO|g" $KUBE_CONTROLLER_MANAGER_FILE
 
   # ensureAzureStackCertificates will be retried if the exit code is not 0
   curl $AZURESTACK_RESOURCE_METADATA_ENDPOINT
