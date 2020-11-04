2e2dfd7b03772f1f8a37c7656d2f32032a658510
Matt Boersma <Matt.Boersma@microsoft.com>
2020-11-04 10:56:55 -0700

    feat: add support for Kubernetes v1.20.0-beta.1 (#3999)
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.20.md#changelog-since-v1200-beta0

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -227,6 +227,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.20.0-alpha.2": false,
 	"1.20.0-alpha.3": true,
 	"1.20.0-beta.0":  true,
+	"1.20.0-beta.1":  true,
 }
 
 // AllKubernetesSupportedVersionsAzureStack is a hash table of all supported Kubernetes version strings on Azure Stack
--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -117,7 +117,7 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.1/windowszip/v1.19.1-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.2/windowszip/v1.19.2-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.3/windowszip/v1.19.3-1int.zip"
-            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.0-beta.0/windowszip/v1.20.0-beta.0-1int.zip"
+            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.0-beta.1/windowszip/v1.20.0-beta.1-1int.zip"
         );
         "c:\akse-cache\win-vnet-cni\" = @(
             "https://kubernetesartifacts.azureedge.net/azure-cni/v1.1.3/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.1.3.zip",
