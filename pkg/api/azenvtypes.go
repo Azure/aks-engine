3efa1e26f2660853a6d23a4ff213d857a07c25fc
Yi Zhang <yizhang5@microsoft.com>
2020-11-13 16:52:59 -0800

    Update Windows image version to the latest. (#4032)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -184,9 +184,9 @@ var (
 	// AKSWindowsServer2019OSImageConfig is the AKS image based on Windows Server 2019
 	AKSWindowsServer2019OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks-windows",
-		ImageSku:       "aks-2019-datacenter-core-smalldisk-2010",
+		ImageSku:       "aks-2019-datacenter-core-smalldisk-2011",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "17763.1577.201110",
+		ImageVersion:   "17763.1577.201111",
 	}
 
 	// WindowsServer2019OSImageConfig is the 'vanilla' Windows Server 2019 image
