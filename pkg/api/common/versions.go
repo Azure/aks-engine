55cf5e37264f5c09ed3cdc0dc4223970b07b9bec
Matt Boersma <Matt.Boersma@microsoft.com>
2020-11-20 11:30:18 -0700

    chore: mark Kubernetes 1.17.14 as disabled
    See https://github.com/kubernetes/kubernetes/pull/96623 for the future fix.

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -195,6 +195,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.17.11":        false,
 	"1.17.12":        true,
 	"1.17.13":        true,
+	"1.17.14":        true, // disabled, see https://github.com/kubernetes/kubernetes/pull/96623
 	"1.18.0-alpha.1": false,
 	"1.18.0-alpha.2": false,
 	"1.18.0-alpha.3": false,
