11d66092bdf35e9b64c4312fa29401b7e1ccbd4f
Matt Boersma <Matt.Boersma@microsoft.com>
2020-12-01 22:56:13 -0700

    feat: add support for Kubernetes v1.20.0-rc.0 (#4076)

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -120,7 +120,7 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.2/windowszip/v1.19.2-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.3/windowszip/v1.19.3-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.4/windowszip/v1.19.4-1int.zip",
-            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.0-beta.1/windowszip/v1.20.0-beta.1-1int.zip"
+            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.0-rc.0/windowszip/v1.20.0-rc.0-1int.zip"
         );
         "c:\akse-cache\win-vnet-cni\" = @(
             "https://kubernetesartifacts.azureedge.net/azure-cni/v1.1.6/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.1.6.zip",
