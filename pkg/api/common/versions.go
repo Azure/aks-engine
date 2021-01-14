b66bd36640dd0d80602ce238452a5f42fa588d8a
Abel Hu <abelch@microsoft.com>
2021-01-14 15:05:59 +0800

    chore: Add k8s 1.20.0 support

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -205,6 +205,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.19.1":         true,
 	"1.19.3":         true,
 	"1.19.6":         true,
+	"1.20.0":         true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
