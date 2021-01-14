4504356d38125e4a0a420dba2f3220bfbe5e720e
Qingchuan Hao <qinhao@microsoft.com>
2021-01-14 10:03:04 +0800

    Read node label from kubeclusterconfig.json

--- staging/provisioning/windows/windowsnodelabelsync.ps1
+++ staging/provisioning/windows/windowsnodelabelsync.ps1
@@ -3,9 +3,10 @@
     This script is intended to update Kubelet node labels.
 #>
 
+$Global:ClusterConfiguration = ConvertFrom-Json ((Get-Content "c:\k\kubeclusterconfig.json" -ErrorAction Stop) | out-string)
 $global:KubeletNodeLabels = $Global:ClusterConfiguration.Kubernetes.Kubelet.NodeLabels
 
 $NodeLabels = $KubeletNodeLabels -split ","
 ForEach ($NodeLabel in $NodeLabels) {
-  c:\k\kubectl.exe --kubeconfig='c:\\k\\config' --overwrite $env:computername.ToLower() $NodeLabel
+  c:\k\kubectl.exe --kubeconfig=c:\k\config --overwrite $env:computername.ToLower() $NodeLabel
 }
