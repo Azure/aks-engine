ffb76b4476a83b0375342fe47e6a7d0f3cb6f5b3
JesusAlvarezTorres <jesusa@microsoft.com>
2021-01-20 17:33:37 -0800

    feat: add support for Kubernetes v1.18.15 on Azure Stack

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -271,6 +271,7 @@ var AllKubernetesSupportedVersionsAzureStack = map[string]bool{
 	"1.17.9":  true,
 	"1.17.11": true,
 	"1.18.10": true,
+	"1.18.15": true,
 }
 
 // AllKubernetesWindowsSupportedVersionsAzureStack maintain a set of available k8s Windows versions in aks-engine on Azure Stack
@@ -290,6 +291,7 @@ var AllKubernetesWindowsSupportedVersionsAzureStack = map[string]bool{
 	"1.17.9":  true,
 	"1.17.11": true,
 	"1.18.10": true,
+	"1.18.15": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
