870d7f74ce9aa9a10ee2e6bb233921104c7e20be
Abel Hu <abelch@microsoft.com>
2021-01-22 09:07:47 +0800

    chore: Update Azure CNI to v1.2.2

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -314,11 +314,11 @@ const (
 	// AzureCniPluginVerLinux specifies version of Azure CNI plugin, which has been mirrored from
 	// https://github.com/Azure/azure-container-networking/releases/download/${AZURE_PLUGIN_VER}/azure-vnet-cni-linux-amd64-${AZURE_PLUGIN_VER}.tgz
 	// to https://kubernetesartifacts.azureedge.net/azure-cni
-	AzureCniPluginVerLinux = "v1.2.0_hotfix"
+	AzureCniPluginVerLinux = "v1.2.2"
 	// AzureCniPluginVerWindows specifies version of Azure CNI plugin, which has been mirrored from
 	// https://github.com/Azure/azure-container-networking/releases/download/${AZURE_PLUGIN_VER}/azure-vnet-cni-windows-amd64-${AZURE_PLUGIN_VER}.zip
 	// to https://kubernetesartifacts.azureedge.net/azure-cni
-	AzureCniPluginVerWindows = "v1.2.0_hotfix"
+	AzureCniPluginVerWindows = "v1.2.2"
 	// CNIPluginVer specifies the version of CNI implementation
 	// https://github.com/containernetworking/plugins
 	CNIPluginVer = "v0.8.7"
