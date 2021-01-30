e13dbe41c5c57da781d7752996ed6aec8fceaf87
Bo Wang <bowa@microsoft.com>
2021-01-29 17:52:36 -0800

    Update vhd version and add 1.19.7

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -205,6 +205,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.19.1":         true,
 	"1.19.3":         true,
 	"1.19.6":         true,
+	"1.19.7":         true,
 	"1.20.0":         true,
 	"1.20.2":         true,
 }
