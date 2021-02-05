038b4f26628d8a9e9da54a11a2ec72ce259d8a7a
Jack Francis <jackfrancis@gmail.com>
2021-02-05 12:12:55 -0800

    feat: run unattended upgrades by default (#4231)

--- pkg/api/defaults.go
+++ pkg/api/defaults.go
@@ -54,6 +54,11 @@ func (cs *ContainerService) SetPropertiesDefaults(params PropertiesDefaultsParam
 		properties.setMasterProfileDefaults()
 	}
 
+	// Set Linux profile defaults if this cluster configuration includes Linux nodes
+	if cs.Properties.LinuxProfile != nil {
+		properties.setLinuxProfileDefaults()
+	}
+
 	properties.setAgentProfileDefaults(params.IsUpgrade, params.IsScale)
 
 	properties.setStorageDefaults()
@@ -694,6 +699,12 @@ func (p *Properties) setMasterProfileDefaults() {
 
 	if p.MasterProfile.OSDiskCachingType == "" {
 		p.MasterProfile.OSDiskCachingType = string(compute.CachingTypesReadWrite)
+	}
+}
+
+func (p *Properties) setLinuxProfileDefaults() {
+	if !p.IsAzureStackCloud() && p.LinuxProfile.RunUnattendedUpgradesOnBootstrap == nil {
+		p.LinuxProfile.RunUnattendedUpgradesOnBootstrap = to.BoolPtr(DefaultRunUnattendedUpgradesOnBootstrap)
 	}
 }
 
