4899b6fdbe1f98005623c11c22cd03c22b824b52
James Sturtevant <jstur@microsoft.com>
2021-03-12 11:28:09 -0800

    chore: update azurecni to 1.2.7 (#4319)
    * chore: update azurecni to 1.2.7
    
    * Update vhd/packer/configure-windows-vhd.ps1
    
    Co-authored-by: Mark Rossetti <marosset@microsoft.com>
    
    Co-authored-by: Mark Rossetti <marosset@microsoft.com>

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -115,8 +115,10 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.21.0-beta.0/windowszip/v1.21.0-beta.0-1int.zip"
         );
         "c:\akse-cache\win-vnet-cni\" = @(
-            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.2.0_hotfix/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.2.0_hotfix.zip",
-            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.2.2/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.2.2.zip"
+            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.2.2/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.2.2.zip",
+            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.2.7/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.2.7.zip"
+
+
         )
     }
 
