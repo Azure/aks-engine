6ddeae927e5e8427a49623ea3fc044bf45fd2daf
Jack Francis <jack.francis@microsoft.com>
2021-04-19 09:23:04 -0700

    chore: update Linux VHDs to 2021.04.13 (#4366)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -122,7 +122,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-engine-ubuntu-1604-202007",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2021.02.22",
+		ImageVersion:   "2021.04.13",
 	}
 
 	// AKSUbuntu1804OSImageConfig is the AKS image based on Ubuntu 18.04-LTS.
@@ -130,7 +130,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-engine-ubuntu-1804-202007",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2021.02.22",
+		ImageVersion:   "2021.04.13",
 	}
 
 	// AKSWindowsServer2019OSImageConfig is the aks-engine image based on Windows Server 2019
