6874a53f650d8e708cd46b4e3e7cc81f2dcd5d21
Giulio D'Ippolito <giulio@wayve.ai>
2021-04-21 19:49:32 +0100

    chore: Upgrade nvidia drivers to 450.80.02

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -12703,7 +12703,7 @@ if [[ ${OS} == "${UBUNTU_OS_NAME}" ]]; then
 fi
 DOCKER=/usr/bin/docker
 if [[ $UBUNTU_RELEASE == "18.04" ]]; then
-  export GPU_DV=450.51.06
+  export GPU_DV=450.80.02
 else
   export GPU_DV=418.40.04
 fi
