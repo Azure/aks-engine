dada4be7de8d854f8b913e26eebf623ad1fd5c4d
Ernest Wong <chuwon@microsoft.com>
2021-05-14 15:45:36 -0700

    chore: bump kube-addon-manager to v9.1.5 (#4412)
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- pkg/api/k8s_versions.go
+++ pkg/api/k8s_versions.go
@@ -117,7 +117,7 @@ var kubernetesImageBaseVersionedImages = map[string]map[string]map[string]string
 		"1.22": {
 			common.AddonResizerComponentName:  "addon-resizer:1.8.7",
 			common.MetricsServerAddonName:     "metrics-server/metrics-server:v0.4.1",
-			common.AddonManagerComponentName:  "kube-addon-manager-amd64:v9.1.3",
+			common.AddonManagerComponentName:  "kube-addon-manager-amd64:v9.1.5",
 			common.ClusterAutoscalerAddonName: "cluster-autoscaler:v1.18.0",
 		},
 		"1.21": {
@@ -241,7 +241,7 @@ var kubernetesImageBaseVersionedImages = map[string]map[string]map[string]string
 		"1.22": {
 			common.AddonResizerComponentName:  "oss/kubernetes/autoscaler/addon-resizer:1.8.7",
 			common.MetricsServerAddonName:     "oss/kubernetes/metrics-server:v0.3.7",
-			common.AddonManagerComponentName:  "oss/kubernetes/kube-addon-manager:v9.1.3",
+			common.AddonManagerComponentName:  "oss/kubernetes/kube-addon-manager:v9.1.5",
 			common.ClusterAutoscalerAddonName: "oss/kubernetes/autoscaler/cluster-autoscaler:v1.20.0",
 		},
 		"1.21": {
