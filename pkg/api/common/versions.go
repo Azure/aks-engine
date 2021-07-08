4a86fe58b01e1d865951a965738cad33dd4c4edf
Matt Boersma <Matt.Boersma@microsoft.com>
2021-07-08 14:49:37 -0600

    feat: add support for Kubernetes 1.22.0-beta.1 (#4570)
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.22.md#changelog-since-v1220-beta0

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -275,6 +275,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.22.0-alpha.2": false,
 	"1.22.0-alpha.3": false,
 	"1.22.0-beta.0":  true,
+	"1.22.0-beta.1":  true,
 }
 
 // AllKubernetesSupportedVersionsAzureStack is a hash table of all supported Kubernetes version strings on Azure Stack
