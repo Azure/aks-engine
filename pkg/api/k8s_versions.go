a7d7edb1b7915b3f9d5ca211b882dc4b4bb62086
Matt Boersma <Matt.Boersma@microsoft.com>
2021-09-15 17:06:36 -0600

    Use c-a 1.22.0 for k8s 1.23.x as well

--- pkg/api/k8s_versions.go
+++ pkg/api/k8s_versions.go
@@ -248,7 +248,7 @@ var kubernetesImageBaseVersionedImages = map[string]map[string]map[string]string
 			common.AddonResizerComponentName:  "oss/kubernetes/autoscaler/addon-resizer:1.8.7",
 			common.MetricsServerAddonName:     "oss/kubernetes/metrics-server:v0.5.0",
 			common.AddonManagerComponentName:  "oss/kubernetes/kube-addon-manager:v9.1.5",
-			common.ClusterAutoscalerAddonName: "oss/kubernetes/autoscaler/cluster-autoscaler:v1.21.0",
+			common.ClusterAutoscalerAddonName: "oss/kubernetes/autoscaler/cluster-autoscaler:v1.22.0",
 		},
 		"1.22": {
 			common.AddonResizerComponentName:  "oss/kubernetes/autoscaler/addon-resizer:1.8.7",
