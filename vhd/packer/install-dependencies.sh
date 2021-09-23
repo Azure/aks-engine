0688d32f1f89ea0fd51d6ef9f7f90af911fac66b
haofan-ms <hafa@microsoft.com>
2021-09-23 09:40:50 -0700

    feat: add support for Kubernetes v1.19.15 and v1.20.11 on Azure Stack

--- vhd/packer/install-dependencies.sh
+++ vhd/packer/install-dependencies.sh
@@ -230,11 +230,10 @@ K8S_VERSIONS="
 1.22.1
 1.21.4
 1.20.10
-1.20.6-azs
+1.20.11-azs
 1.19.14
-1.19.10-azs
+1.19.15-azs
 1.18.20
-1.18.18-azs
 "
 for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
   for component in kube-apiserver kube-controller-manager kube-proxy kube-scheduler; do
