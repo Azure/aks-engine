f8fefaf62ef962a7d6d6591dabf1c5ad85f833ca
Jack Francis <jack.francis@microsoft.com>
2021-09-22 18:54:01 -0700

    test: delete failed vmss extensions, wait longer (#4669)

--- test/e2e/kubernetes/scripts/vmss-health-check.sh
+++ test/e2e/kubernetes/scripts/vmss-health-check.sh
@@ -20,44 +20,56 @@ while true; do
   for VMSS in $(az vmss list -g $RESOURCE_GROUP | jq -r '.[] | .name'); do
     ((NUM_VMSS++))
     NUM_DELETED_INSTANCES=0
+    VMSS_PROVISIONING_STATE=$(az vmss show -g $RESOURCE_GROUP -n $VMSS | jq -r '.provisioningState')
+    echo $(date)    VMSS $VMSS has a ProvisioningState of $VMSS_PROVISIONING_STATE
     VMSS_CAPACITY=$(az vmss list -g $RESOURCE_GROUP | jq -r --arg VMSS "$VMSS" '.[] | select(.name == $VMSS) | .sku.capacity')
     echo $(date)    VMSS $VMSS has a current capacity of $VMSS_CAPACITY
-    for TERMINAL_VMSS in $(az vmss show -g $RESOURCE_GROUP -n $VMSS | jq -r '. | select(.provisioningState == "Succeeded" or .provisioningState == "Failed") | .name'); do
+    if [ "$VMSS_PROVISIONING_STATE" == "Succeeded" ] || [ "$VMSS_PROVISIONING_STATE" == "Failed" ]; then
       ((NUM_TERMINAL_VMSS++))
-      echo $(date)    VMSS $TERMINAL_VMSS is in a terminal state!
       HAS_FAILED_STATE_INSTANCE="false"
-      for TARGET_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $TERMINAL_VMSS | jq -r '.[] | select(.provisioningState == "Failed") | .name'); do
+      for TARGET_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $VMSS | jq -r '.[] | select(.provisioningState == "Failed") | .name'); do
         HAS_FAILED_STATE_INSTANCE="true"
-        echo $(date)    Deleting VMSS $TERMINAL_VMSS instance $TARGET_VMSS_INSTANCE
-        if ! az vmss delete-instances -n $TERMINAL_VMSS -g $RESOURCE_GROUP --instance-id ${TARGET_VMSS_INSTANCE##*_} --no-wait; then
+        echo $(date)    Deleting VMSS $VMSS instance $TARGET_VMSS_INSTANCE
+        if ! az vmss delete-instances -n $VMSS -g $RESOURCE_GROUP --instance-id ${TARGET_VMSS_INSTANCE##*_} --no-wait; then
           sleep 30
         else
           sleep 1
           ((NUM_DELETED_INSTANCES++))
         fi
       done
-      for TARGET_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $TERMINAL_VMSS | jq -r '.[].resources[] | select(.name == "vmssCSE" and .provisioningState == "Failed") | .id' | awk -F'/' '{print $9}'); do
-        echo $(date)    Deleting VMSS $TERMINAL_VMSS instance $TARGET_VMSS_INSTANCE
-        if ! az vmss delete-instances -n $TERMINAL_VMSS -g $RESOURCE_GROUP --instance-id ${TARGET_VMSS_INSTANCE##*_}; then
+      for TARGET_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $VMSS | jq -r '.[].resources[] | select(.name == "vmssCSE" and .provisioningState == "Failed") | .id' | awk -F'/' '{print $9}'); do
+        HAS_FAILED_STATE_INSTANCE="true"
+        echo $(date)    Deleting VMSS $VMSS instance $TARGET_VMSS_INSTANCE
+        if ! az vmss delete-instances -n $VMSS -g $RESOURCE_GROUP --instance-id ${TARGET_VMSS_INSTANCE##*_}; then
            sleep 30
         else
            sleep 1
            ((NUM_DELETED_INSTANCES++))
         fi
       done
+      for TARGET_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $VMSS | jq -r '.[].resources[] | select(.publisher == "Microsoft.AKS" and .provisioningState != "Succeeded" and .provisioningState != "Creating" and .provisioningState != "Deleting") | .id' | awk -F'/' '{print $9}'); do
+        HAS_FAILED_STATE_INSTANCE="true"
+        echo $(date)    Deleting VMSS $VMSS instance $TARGET_VMSS_INSTANCE
+        if ! az vmss delete-instances -n $VMSS -g $RESOURCE_GROUP --instance-id ${TARGET_VMSS_INSTANCE##*_}; then
+          sleep 30
+        else
+          sleep 1
+          ((NUM_DELETED_INSTANCES++))
+        fi
+      done
       if [ "$HAS_FAILED_STATE_INSTANCE" == "true" ]; then
-        echo $(date)    Waiting for $TERMINAL_VMSS to reach a terminal ProvisioningState after failed instances were deleted...
+        echo $(date)    Waiting for $VMSS to reach a terminal ProvisioningState after failed instances were deleted...
         sleep 30
         until [[ $(az vmss show -g $RESOURCE_GROUP -n $VMSS | jq -r '. | select(.provisioningState == "Succeeded" or .provisioningState == "Failed") | .name') ]]; do
-          for STILL_FAILED_VMSS_INSTANCE in $(az vmss list-instances -g $RESOURCE_GROUP -n $TERMINAL_VMSS | jq -r '.[] | select(.provisioningState == "Failed") | .name'); do
-            echo $(date)    Instance $STILL_FAILED_VMSS_INSTANCE is still in a failed state, will attempt to delete again in the next loop
-          done
+          echo $(date)    Waiting for $VMSS to reach a terminal ProvisioningState after failed instances were deleted...
+          sleep 30
         done
+        echo $(date)    VMSS $VMSS is in a terminal state after failed instances were deleted!
       fi
-    done
+    fi
     if [ "$NUM_DELETED_INSTANCES" -gt "0" ]; then
       echo $(date)    Instances were deleted from VMSS $VMSS, ensuring that capacity is set to $VMSS_CAPACITY
-      if ! az vmss scale --new-capacity $VMSS_CAPACITY -n $VMSS -g $RESOURCE_GROUP; then
+      if ! az vmss scale --new-capacity $VMSS_CAPACITY -n $VMSS -g $RESOURCE_GROUP --no-wait; then
           exit 1
       fi
     fi
