c783bd03221a55eead94a546ab52ab6ce9e54a3a
tamilmani1989 <tamilmani1989@gmail.com>
2021-10-26 08:38:14 -0700

    update cni to v1.4.14 (#4704)

--- vhd/packer/install-dependencies.sh
+++ vhd/packer/install-dependencies.sh
@@ -103,9 +103,9 @@ cat << EOF >> ${VHD_LOGS_FILEPATH}
 EOF
 
 VNET_CNI_VERSIONS="
+1.4.14
 1.4.13
 1.4.12
-1.4.9
 "
 for VNET_CNI_VERSION in $VNET_CNI_VERSIONS; do
     VNET_CNI_PLUGINS_URL="https://kubernetesartifacts.azureedge.net/azure-cni/v${VNET_CNI_VERSION}/binaries/azure-vnet-cni-linux-amd64-v${VNET_CNI_VERSION}.tgz"
