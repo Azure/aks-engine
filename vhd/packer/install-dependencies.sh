61429bc69b5038b874a8e3d6e355efa52ff57d75
Chou Hu <abelch@microsoft.com>
2021-12-03 09:19:34 +0800

    chore: update cni to v1.4.16 (#4769)

--- vhd/packer/install-dependencies.sh
+++ vhd/packer/install-dependencies.sh
@@ -96,9 +96,9 @@ installEtcd "docker"
 echo "  - etcd v${ETCD_VERSION}" >> ${VHD_LOGS_FILEPATH}
 
 VNET_CNI_VERSIONS="
+1.4.16
 1.4.14
 1.4.13
-1.4.12
 "
 for VNET_CNI_VERSION in $VNET_CNI_VERSIONS; do
     VNET_CNI_PLUGINS_URL="https://kubernetesartifacts.azureedge.net/azure-cni/v${VNET_CNI_VERSION}/binaries/azure-vnet-cni-linux-amd64-v${VNET_CNI_VERSION}.tgz"
