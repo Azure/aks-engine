f6a91826903da164c67b5234fc3391bee26fea8f
Matt Boersma <Matt.Boersma@microsoft.com>
2021-12-14 12:25:38 -0700

    feat: add support for Kubernetes v1.20.14
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.20.md#changelog-since-v12013

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -120,7 +120,7 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.15-azs/windowszip/v1.19.15-azs-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.11-azs/windowszip/v1.20.11-azs-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.19.16/windowszip/v1.19.16-1int.zip",
-            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.13/windowszip/v1.20.13-1int.zip",
+            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.20.14/windowszip/v1.20.14-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.21.8/windowszip/v1.21.8-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.22.4/windowszip/v1.22.4-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.23.1/windowszip/v1.23.1-1int.zip"
