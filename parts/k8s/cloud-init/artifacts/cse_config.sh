fd44722074ae4ba31b1b3df94da9c865827d369a
Jack <jackfrancis@gmail.com>
2022-01-05 10:00:19 -0800

    remove unnecessary $(ls)

--- parts/k8s/cloud-init/artifacts/cse_config.sh
+++ parts/k8s/cloud-init/artifacts/cse_config.sh
@@ -623,9 +623,9 @@ configGPUDrivers() {
   retrycmd_no_stats 120 5 25 update-initramfs -u || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
   wait_for_apt_locks
   for apt_package in $NVIDIA_PACKAGES; do
-    dpkg -i $(ls ${PERMANENT_CACHE_DIR}${apt_package}*) || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
+    dpkg -i ${PERMANENT_CACHE_DIR}${apt_package}* || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
   done
-  dpkg -i $(ls ${PERMANENT_CACHE_DIR}nvidia-container-runtime*) || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
+  dpkg -i ${PERMANENT_CACHE_DIR}nvidia-container-runtime* || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
   mkdir -p $GPU_DEST/lib64 $GPU_DEST/overlay-workdir
   retrycmd 120 5 25 mount -t overlay -o lowerdir=/usr/lib/x86_64-linux-gnu,upperdir=${GPU_DEST}/lib64,workdir=${GPU_DEST}/overlay-workdir none /usr/lib/x86_64-linux-gnu || exit {{GetCSEErrorCode "ERR_GPU_DRIVERS_CONFIG"}}
   export -f installNvidiaDrivers
