0772015fb7f713bc730b9d7a4cbb81a1bac57085
Mark Rossetti <marosset@microsoft.com>
2022-01-10 10:43:36 -0800

    chore:update docker EE to 20.10.9 for Windows (#4645)
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -173,7 +173,7 @@ function Install-ContainerD {
 }
 
 function Install-Docker {
-    $defaultDockerVersion = "20.10.6"
+    $defaultDockerVersion = "20.10.9"
 
     Write-Log "Attempting to install Docker version $defaultDockerVersion"
     Install-PackageProvider -Name DockerMsftProvider -Force -ForceBootstrap | Out-Null
