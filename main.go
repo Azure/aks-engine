43f29b2cba93eb68ba9b9d291659062b69f99ea1
Matt Boersma <Matt.Boersma@microsoft.com>
2022-06-17 04:15:00 -0600

    docs: print deprecation message to stderr (#4905)

--- main.go
+++ main.go
@@ -13,6 +13,17 @@ import (
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{ForceColors: true})
+	log.SetOutput(colorable.NewColorableStderr())
+	msg := `
+aks-engine is deprecated for Azure public cloud customers. Learn more at:
+    https://github.com/Azure/aks-engine#project-status
+
+Please consider using Azure Kubernetes Service (AKS) for managed Kubernetes:
+    https://azure.microsoft.com/services/kubernetes-service/
+or Cluster API Provider Azure (CAPZ) for self-managed Kubernetes:
+    https://capz.sigs.k8s.io/
+`
+	log.Warningf("\u001b[33m%s\u001b[0m", msg)
 	log.SetOutput(colorable.NewColorableStdout())
 	if err := cmd.NewRootCmd().Execute(); err != nil {
 		os.Exit(1)
