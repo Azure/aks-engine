9c93c736f12d0a95b4810c1e32a80db52cd3a7da
Matt Boersma <Matt.Boersma@microsoft.com>
2022-06-21 01:34:17 -0600

    feat: add support for Kubernetes v1.21.14 (#4911)
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.21.md#changelog-since-v12113
    
    Co-authored-by: Jack Francis <jackfrancis@gmail.com>

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -121,7 +121,7 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/csi-proxy/v0.2.2/binaries/csi-proxy-v0.2.2.tar.gz"
         );
         "c:\akse-cache\win-k8s\"      = @(
-            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.21.13/windowszip/v1.21.13-1int.zip",
+            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.21.14/windowszip/v1.21.14-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.22.11/windowszip/v1.22.11-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.23.8/windowszip/v1.23.8-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.24.2/windowszip/v1.24.2-1int.zip"
