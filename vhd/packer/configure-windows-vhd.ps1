ed8fd304edf98a331d291e2ff2b0c5435ef486dd
Matt Boersma <Matt.Boersma@microsoft.com>
2022-08-19 14:47:34 -0600

    feat: add support for Kubernetes v1.22.13
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.22.md#changelog-since-v12212

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -124,7 +124,7 @@ function Get-FilesToCacheOnVHD {
         "c:\akse-cache\win-k8s\"      = @(
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.21.14/windowszip/v1.21.14-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.22.7/windowszip/v1.22.7-1int.zip",
-            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.22.12/windowszip/v1.22.12-1int.zip",
+            "https://kubernetesartifacts.azureedge.net/kubernetes/v1.22.13/windowszip/v1.22.13-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.23.6/windowszip/v1.23.6-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.23.9/windowszip/v1.23.9-1int.zip",
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.24.3/windowszip/v1.24.3-1int.zip"
