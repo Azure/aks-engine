f6544ad4a3e0b4c5c73a40e42c665906aa10a03a
Matt Boersma <Matt.Boersma@microsoft.com>
2022-09-22 11:19:51 -0600

    feat: add support for Kubernetes v1.24.6
    See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.24.md#changelog-since-v1245

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -338,7 +338,9 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.24.1":         false,
 	"1.24.2":         false,
 	"1.24.3":         false,
-	"1.24.4":         true,
+	"1.24.4":         false,
+	"1.24.5":         false,
+	"1.24.6":         true,
 }
 
 // AllKubernetesSupportedVersionsAzureStack is a hash table of all supported Kubernetes version strings on Azure Stack
