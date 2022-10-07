220edded4b83e1d7517b5d61b556eed4f8a5466c
vepasupu <79192456+vepasupu@users.noreply.github.com>
2022-10-07 21:32:36 +0530

    Feature: Add hnsremediator for hns crash in Windows nodes (#4975)
    * Feature: Add hnsremediator for hns crash in Windows nodes

--- staging/provisioning/windows/windowsnodereset.ps1
+++ staging/provisioning/windows/windowsnodereset.ps1
@@ -27,6 +27,8 @@ Write-Log "Entering windowsnodereset.ps1"
 
 Import-Module $global:HNSModule
 
+Unregister-HNSRemediatorScriptTask
+
 #
 # Stop services
 #
@@ -44,6 +46,48 @@ if ($global:CsiProxyEnabled) {
 if ($global:EnableHostsConfigAgent) {
     Write-Log "Stopping hosts-config-agent service"
     Stop-Service hosts-config-agent
+}
+
+function Register-HNSRemediatorScriptTask {
+    # Hardcoding RepetitionInterval to 1 Minute
+    # Making it variable would need a new parameter to be added under windowsProfile
+    Write-Log "Creating a scheduled task to run hnsremediator.ps1"
+
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"c:\k\hnsremediator.ps1`""
+    $principal = New-ScheduledTaskPrincipal -UserId SYSTEM -LogonType ServiceAccount -RunLevel Highest
+    $trigger = New-JobTrigger -Once -At (Get-Date).Date -RepeatIndefinitely -RepetitionInterval (New-TimeSpan -Minutes 1)
+    $definition = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Description "hns-remediator-task"
+    Register-ScheduledTask -TaskName "hns-remediator-task" -InputObject $definition
+}
+
+function Unregister-HNSRemediatorScriptTask {
+    if (Get-ScheduledTask -TaskName "hns-remediator-task" -ErrorAction Ignore) {
+        Write-Log "Deleting the scheduled task hns-remediator-task"
+        Unregister-ScheduledTask -TaskName "hns-remediator-task" -Confirm:$false
+    }
+
+    $hnsPIDFile="C:\k\hns.pid"
+    if (Test-Path $hnsPIDFile) {
+        # Remove this file since PID of HNS service may have been changed after node crashes or is rebooted
+        # It should not always fail since hns-remediator-task is unregistered.
+        # We set the max retry count to 20 to avoid dead loop for unknown issues.
+        $maxRetries=20
+        $retryCount=0
+        while ($retryCount -lt $maxRetries) {
+            Write-Log "Deleting $hnsPIDFile"
+            Remove-Item -Path $hnsPIDFile -Force -Confirm:$false -ErrorAction Ignore
+
+            # The file may not be deleted successfully because hnsremediator.ps1 is still writing the logs
+            if (Test-Path $hnsPIDFile) {
+                # Do not log the failure to reduce log
+                Start-Sleep -Milliseconds 500
+                $retryCount=$retryCount+1
+            } else {
+                Write-Log "$hnsPIDFile is deleted"
+                break
+            }
+        }
+    }
 }
 
 # Due to a bug in hns there is a race where it picks up the incorrect IPv6 address from the node in some cases.
@@ -108,5 +152,7 @@ Write-Log "Starting kubelet service"
 Start-Service kubelet
 
 Write-Log "Do not start kubeproxy service since kubelet will restart kubeproxy"
+
+Register-HNSRemediatorScriptTask
 
 Write-Log "Exiting windowsnodereset.ps1"
